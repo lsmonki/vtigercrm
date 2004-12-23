4f6fd56a197b3c082fad4704ac73d239c9826daa
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2004-12-23 15:39:05 +0000

    Initial Version

---
+++ modules/HelpDesk/language/en_us.lang.php
@@ -0,0 +1,39 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Defines the English language pack
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+$mod_strings = Array(
+// Added in release 4.0
+'LBL_MODULE_NAME'=>'HelpDesk',
+'LBL_GROUP'=>'Group',
+'LBL_ACCOUNT_NAME'=>'Account Name',
+'LBL_CONTACT_NAME'=>'Contact Name',
+'LBL_SUBJECT'=>'Subject',
+'LBL_DESCRIPTION'=>'DESCRIPTION',
+'NTC_DELETE_CONFIRMATION'=>'Are you sure you want to delete this record?',
+'LBL_CUSTOM_FIELD_SETTINGS'=>'Custom Field Settings:',
+'LBL_PICKLIST_FIELD_SETTINGS'=>'Picklist Field Settings:',
+'Leads'=>'Lead',
+'Accounts'=>'Account',
+'Contacts'=>'Contact',
+'Opportunities'=>'Opportunity'
+);
+
