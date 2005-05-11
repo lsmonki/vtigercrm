e140ccfb982996409fbe6b5cee9d31025ab345a0
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-05-11 04:54:22 +0000

    * Removed the account as mandatory in quick create

--- modules/Contacts/Forms.php
+++ modules/Contacts/Forms.php
@@ -60,10 +60,6 @@ function verify_data(form) {
 		isError = true;
 		errorMessage += "\\n$lbl_last_name";
 	}
-	if (trim(form.account_id.value) == "") {
-		isError = true;
-		errorMessage += "\\n$lbl_account_name";
-	}
 	if (isError == true) {
 		alert("$err_missing_required_fields" + errorMessage);
 		return false;
@@ -125,7 +121,7 @@ $the_form .= <<<EOQ
 		<input name='firstname' type="text" value=""><br>
 		<FONT class="required">$lbl_required_symbol</FONT>$lbl_last_name<br>
 		<input name='lastname' type="text" value=""><br>
-		<FONT class="required">$lbl_required_symbol</FONT>$lbl_account_name<br>
+		$lbl_account_name<br>
 		<input name='account_name' type="text" value="" readonly>
 		<input name="account_id" type="hidden" value="">&nbsp;<input title="Change" accessKey="Change" type="button" tabindex="3" class="button" value="Change" name="btn1" LANGUAGE=javascript onclick='return window.open("index.php?module=Accounts&action=Popup&popuptype=specific&form=EditView&form_submit=false","test","width=600,height=400,resizable=1,scrollbars=1");'>
 		<br>
