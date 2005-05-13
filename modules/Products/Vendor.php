95127efa7ab04632cc0e268ddca9f2df7cddd9e8
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-05-13 06:21:22 +0000

    Vendor Module Initial Version

---
+++ modules/Products/Vendor.php
@@ -0,0 +1,81 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+include_once('config.php');
+require_once('include/logging.php');
+require_once('include/database/PearDatabase.php');
+require_once('data/SugarBean.php');
+require_once('data/CRMEntity.php');
+require_once('include/utils.php');
+
+
+class Vendor extends CRMEntity {
+	var $log;
+	var $db;
+
+
+	// Stored fields
+	var $id;
+	var $mode;
+
+	var $tab_name = Array('crmentity','vendor');
+	var $tab_name_index = Array('crmentity'=>'crmid','vendor'=>'vendorid');
+	var $column_fields = Array();
+
+//	var $sortby_fields = Array('productname','productcode','commissionrate');		  
+
+        // This is the list of fields that are in the lists.
+	var $list_fields = Array(
+                                'Name'=>Array('vendor'=>'name'),
+                                'Company Name'=>Array('vendor'=>'company_name'),
+                                'Phone'=>Array('vendor'=>'phone'),
+                                'Email'=>Array('vendor'=>'email'),
+                                'Category'=>Array('vendor'=>'category')
+                                );
+        var $list_fields_name = Array(
+                                        'Name'=>'name',
+                                        'Company Name'=>'company_name',
+                                        'Phone'=>'phone',
+                                        'Email'=>'email',
+                                        'Category'=>'category'
+                                     );
+        var $list_link_field= 'name';
+
+
+	var $list_mode;
+	var $popup_type;
+
+	var $search_fields = Array(
+                                'Name'=>Array('vendor'=>'name'),
+                                'Company Name'=>Array('vendor'=>'company_name')
+                                );
+        var $search_fields_name = Array(
+                                        'Name'=>'name',
+                                        'Company Name'=>'company_name'
+                                     );
+/*	
+//	var $combofieldNames = Array('manufacturer'=>'manufacturer_dom'
+                      ,'productcategory'=>'productcategory_dom');
+*/
+
+	function Vendor() {
+		$this->log =LoggerManager::getLogger('vendor');
+		$this->db = new PearDatabase();
+		$this->column_fields = getColumnFields('Vendor');
+	}
+
+  function get_summary_text()
+        {
+                return $this->name;
+        }
+
+}
+?>
---
+++ modules/Products/VendorEditView.php
@@ -0,0 +1,145 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+require_once('include/database/PearDatabase.php');
+require_once('XTemplate/xtpl.php');
+require_once('include/uifromdbutil.php');
+require_once('modules/Products/Vendor.php');
+require_once('include/FormValidationUtil.php');
+
+global $app_strings;
+global $app_list_strings;
+global $mod_strings;
+global $current_user;
+
+$focus = new Vendor();
+
+if(isset($_REQUEST['record'])) 
+{
+    $focus->id = $_REQUEST['record'];
+    $focus->mode = 'edit'; 	
+    $focus->retrieve_entity_info($_REQUEST['record'],"Vendor");
+    //$focus->name=$focus->column_fields['productname'];		
+}
+if(isset($_REQUEST['isDuplicate']) && $_REQUEST['isDuplicate'] == 'true') {
+	$focus->id = "";
+    	$focus->mode = ''; 	
+} 
+
+//get Block 1 Information
+
+$block_1 = getBlockInformation("Vendor",1,$focus->mode,$focus->column_fields);
+
+//get Address Information
+
+$block_2 = getBlockInformation("Vendor",2,$focus->mode,$focus->column_fields);
+
+//get Custom Field Information
+$block_3 = getBlockInformation("Vendor",3,$focus->mode,$focus->column_fields);
+
+global $theme;
+$theme_path="themes/".$theme."/";
+$image_path=$theme_path."images/";
+require_once($theme_path.'layout_utils.php');
+
+$xtpl=new XTemplate ('modules/Products/VendorEditView.html');
+$xtpl->assign("MOD", $mod_strings);
+$xtpl->assign("APP", $app_strings);
+$xtpl->assign("BLOCK1", $block_1);
+$xtpl->assign("BLOCK2", $block_2);
+$xtpl->assign("BLOCK3", $block_3);
+$block_1_header = getBlockTableHeader("LBL_VENDOR_INFORMATION");
+$block_2_header = getBlockTableHeader("LBL_VENDOR_ADDRESS_INFORMATION");
+$block_3_header = getBlockTableHeader("LBL_DESCRIPTION_INFORMATION");
+$xtpl->assign("BLOCK1_HEADER", $block_1_header);
+$xtpl->assign("BLOCK2_HEADER", $block_2_header);
+$xtpl->assign("BLOCK3_HEADER", $block_3_header);
+/*
+if (isset($focus->name)) $xtpl->assign("NAME", $focus->name);
+else $xtpl->assign("NAME", "");
+
+if(isset($cust_fld))
+{
+        $xtpl->assign("CUSTOMFIELD", $cust_fld);
+}
+*/
+$xtpl->assign("ID", $focus->id);
+
+//$xtpl->assign("CALENDAR_LANG", "en");$xtpl->assign("CALENDAR_DATEFORMAT", parse_calendardate($app_strings['NTC_DATE_FORMAT']));
+if($focus->mode == 'edit')
+{
+        $xtpl->assign("MODE", $focus->mode);
+}
+
+if(isset($_REQUEST['return_module'])) $xtpl->assign("RETURN_MODULE", $_REQUEST['return_module']);
+if(isset($_REQUEST['return_action'])) $xtpl->assign("RETURN_ACTION", $_REQUEST['return_action']);
+if(isset($_REQUEST['return_id'])) $xtpl->assign("RETURN_ID", $_REQUEST['return_id']);
+$xtpl->assign("THEME", $theme);
+$xtpl->assign("IMAGE_PATH", $image_path);$xtpl->assign("PRINT_URL", "phprint.php?jt=".session_id().$GLOBALS['request_string']);
+
+
+
+/*
+$vendor_tables = Array('vendor'); 
+
+ $validationData = getDBValidationData($product_tables);
+ $fieldName = '';
+ $fieldLabel = '';
+ $fldDataType = '';
+
+ $rows = count($validationData);
+ foreach($validationData as $fldName => $fldLabel_array)
+ {
+   if($fieldName == '')
+   {
+     $fieldName="'".$fldName."'";
+   }
+   else
+   {
+     $fieldName .= ",'".$fldName ."'";
+   }
+   foreach($fldLabel_array as $fldLabel => $datatype)
+   {
+	if($fieldLabel == '')
+	{
+			
+     		$fieldLabel = "'".$fldLabel ."'";
+	}		
+      else
+       {
+      $fieldLabel .= ",'".$fldLabel ."'";
+        }
+ 	if($fldDataType == '')
+         {
+      		$fldDataType = "'".$datatype ."'";
+    	}
+	 else
+        {
+       		$fldDataType .= ",'".$datatype ."'";
+     	}
+   }
+ }
+
+
+
+$xtpl->assign("VALIDATION_DATA_FIELDNAME",$fieldName);
+$xtpl->assign("VALIDATION_DATA_FIELDDATATYPE",$fldDataType);
+$xtpl->assign("VALIDATION_DATA_FIELDLABEL",$fieldLabel);
+
+*/
+
+
+
+
+$xtpl->parse("main");
+
+$xtpl->out("main");
+
+?>
