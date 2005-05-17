202b14e477f9188cd102137a0860db6587fa7142
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-05-17 11:31:22 +0000

    Initial Version. CRUD Operation file for Orders Module

---
+++ modules/Orders/Delete.php
@@ -0,0 +1,39 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the 
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Deletes an Account record and then redirects the browser to the 
+ * defined return URL.
+ ********************************************************************************/
+
+require_once('modules/Orders/Delete.php');
+require_once('modules/Orders/Order.php');
+global $mod_strings;
+
+require_once('include/logging.php');
+$log = LoggerManager::getLogger('order_delete');
+
+$focus = new Order();
+
+if(!isset($_REQUEST['record']))
+	die($mod_strings['ERR_DELETE_RECORD']);
+
+$sql_recentviewed ='delete from tracker where user_id = '.$current_user->id.' and item_id = '.$_REQUEST['record'];
+$adb->query($sql_recentviewed);
+if($_REQUEST['return_module'] == $_REQUEST['module'])
+	$focus->mark_deleted($_REQUEST['record']);
+
+header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']);
+?>
---
+++ modules/Orders/Order.php
@@ -0,0 +1,229 @@
+<?php
+/*********************************************************************************
+ * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
+ * ("License"); You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is:  SugarCRM Open Source
+ * The Initial Developer of the Original Code is SugarCRM, Inc.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.;
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ ********************************************************************************/
+/*********************************************************************************
+ * $Header$
+ * Description:  Defines the Account SugarBean Account entity with the necessary
+ * methods and variables.
+ * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+ * All Rights Reserved.
+ * Contributor(s): ______________________________________..
+ ********************************************************************************/
+
+include_once('config.php');
+require_once('include/logging.php');
+require_once('include/database/PearDatabase.php');
+require_once('data/SugarBean.php');
+require_once('data/CRMEntity.php');
+require_once('include/utils.php');
+
+// Account is used to store account information.
+class Order extends CRMEntity {
+	var $log;
+	var $db;
+
+
+	// Stored fields
+	var $id;
+	var $mode;
+	
+		
+	var $table_name = "purchaseorder";
+	var $tab_name = Array('crmentity','purchaseorder','pobillads','poshipads','poproductrel');
+	var $tab_name_index = Array('crmentity'=>'crmid','purchaseorder'=>'purchaseorderid','pobillads'=>'pobilladdressid','poshipads'=>'poshipaddressid','poproductrel'=>'purchaseorderid');
+				
+	
+	var $entity_table = "crmentity";
+	
+	var $billadr_table = "pobillads";
+
+	var $object_name = "Order";
+
+	var $new_schema = true;
+	
+	var $module_id = "purchaseorderid";
+
+	var $column_fields = Array();
+
+	//var $sortby_fields = Array('accountname','city','website','phone');		
+
+	// This is used to retrieve related fields from form posts.
+	var $additional_column_fields = Array('assigned_user_name', 'smownerid', 'opportunity_id', 'case_id', 'contact_id', 'task_id', 'note_id', 'meeting_id', 'call_id', 'email_id', 'parent_name', 'member_id' );
+
+	// This is the list of fields that are in the lists.
+	var $list_fields = Array(
+				'Purchase Order Id'=>Array('crmentity'=>'crmid'),
+				'Subject'=>Array('purchaseorder'=>'subject'),
+				'Quote Name'=>Array('purchaseorder'=>'quoteid'),
+				'Vendor Name'=>Array('purchaseorder'=>'vendorid'), 
+				'Type'=>Array('purchaseorder'=> 'type'),
+				'Product Name'=>Array('poproductrel'=>'productid'),
+				'Assigned To'=>Array('crmentity'=>'smownerid')
+				);
+	
+	var $list_fields_name = Array(
+				        'Purchase Order Id'=>'',
+				        'Subject'=>'subject',
+				        'Quote Name'=>'quote_id',
+				        'Vendor Name'=>'vendor_id',
+					'Type'=>'type',
+					'Product Name'=>'product_id',
+				        'Assigned To'=>'assigned_user_id'
+				      );
+	var $list_link_field= 'subject';
+
+	var $record_id;
+	var $list_mode;
+        var $popup_type;
+
+	var $search_fields = Array(
+				'Account Name'=>Array('account'=>'accountname'),
+				'City'=>Array('accountbillads'=>'city'), 
+				);
+	
+	var $search_fields_name = Array(
+				        'Account Name'=>'accountname',
+				        'City'=>'bill_city',
+				      );
+
+	// This is the list of fields that are required.
+	var $required_fields =  array("accountname"=>1);
+
+	function Order() {
+		$this->log =LoggerManager::getLogger('Order');
+		$this->db = new PearDatabase();
+		$this->column_fields = getColumnFields('Orders');
+	}
+
+	function create_tables () {
+          /*
+		$query = 'CREATE TABLE '.$this->table_name.' ( ';
+		$query .='id char(36) NOT NULL';
+		$query .=', date_entered datetime NOT NULL';
+		$query .=', date_modified datetime NOT NULL';
+		$query .=', modified_user_id char(36) NOT NULL';
+		$query .=', assigned_user_id char(36)';
+		$query .=', name char(150)';
+		$query .=', parent_id char(36)';
+		$query .=', account_type char(25)';
+		$query .=', industry char(25)';
+		$query .=', annual_revenue char(25)';
+		$query .=', phone_fax char(25)';
+		$query .=', billing_address_street char(150)';
+		$query .=', billing_address_city char(100)';
+		$query .=', billing_address_state char(100)';
+		$query .=', billing_address_postalcode char(20)';
+		$query .=', billing_address_country char(100)';
+		$query .=', description text';
+		$query .=', rating char(25)';
+		$query .=', phone_office char(25)';
+		$query .=', phone_alternate char(25)';
+		$query .=', email1 char(100)';
+		$query .=', email2 char(100)';
+		$query .=', website char(255)';
+		$query .=', ownership char(100)';
+		$query .=', employees char(10)';
+		$query .=', sic_code char(10)';
+		$query .=', ticker_symbol char(10)';
+		$query .=', shipping_address_street char(150)';
+		$query .=', shipping_address_city char(100)';
+		$query .=', shipping_address_state char(100)';
+		$query .=', shipping_address_postalcode char(20)';
+		$query .=', shipping_address_country char(100)';
+		$query .=', deleted bool NOT NULL default 0';
+		$query .=', PRIMARY KEY ( id ) )';
+
+		
+
+		$this->db->query($query);
+	//TODO Clint 4/27 - add exception handling logic here if the table can't be created.
+        */
+
+	}
+
+	function drop_tables () {
+          /*
+		$query = 'DROP TABLE IF EXISTS '.$this->table_name;
+
+		
+
+		$this->db->query($query);
+
+	//TODO Clint 4/27 - add exception handling logic here if the table can't be dropped.
+        */
+	}
+
+	function get_summary_text()
+	{
+		return $this->name;
+	}
+
+	/** Returns a list of the associated accounts who are member orgs
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated contacts
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated opportunities
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+ 
+
+	/** Returns a list of the associated tasks
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated notes
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated meetings
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated calls
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+	
+
+	/** Returns a list of the associated emails
+	 * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc..
+	 * All Rights Reserved..
+	 * Contributor(s): ______________________________________..
+	*/
+
+}
+
+?>
