8e05b36246c79411653a7005ae2719e6db21f393
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-05-25 17:17:16 +0000

    Added getPriceBookHiddenValues(), Added renderPriceBookRelatedProducts(,)

--- modules/Products/RenderRelatedListUI.php
+++ modules/Products/RenderRelatedListUI.php
@@ -28,6 +28,20 @@ function getHiddenValues($id)
 	return $hidden;
 }
 
+function getPriceBookHiddenValues($id)
+{
+        $hidden .= '<form border="0" action="index.php" method="post" name="form" id="form">';
+        $hidden .= '<input type="hidden" name="module">';
+        $hidden .= '<input type="hidden" name="mode">';
+        $hidden .= '<input type="hidden" name="pricebook_id" value="'.$id.'">';
+        $hidden .= '<input type="hidden" name="return_module" value="Products">';
+        $hidden .= '<input type="hidden" name="return_action" value="PriceBookDetailView">';
+        $hidden .= '<input type="hidden" name="return_id" value="'.$id.'">';
+        $hidden .= '<input type="hidden" name="parent_id" value="'.$id.'">';
+        $hidden .= '<input type="hidden" name="action">';	
+	return $hidden;
+}
+
 function renderRelatedTickets($query,$id)
 {
 	global $mod_strings;
@@ -83,7 +97,31 @@ function renderRelatedAttachments($query,$id)
 
         echo '</form>';
 }
+function renderPriceBookRelatedProducts($query,$id)
+{
+	require_once('modules/Products/Product.php');
+        global $mod_strings;
+        global $app_strings;
 
+        $hidden = getPriceBookHiddenValues($id);
+        echo $hidden;
+
+        $focus = new Product();
+ 
+	$button = '';
+
+
+ 
+		$button .= '<input title="Select Products" accessyKey="F" class="button" onclick="this.form.action.value=\'AddProductsToPriceBook\';this.form.module.value=\'Products\';this.form.return_module.value=\'Products\';this.form.return_action.value=\'PriceBookDetailView\'" type="submit" name="button" value="'.$app_strings['LBL_SELECT_PRODUCT_BUTTON_LABEL'].'">&nbsp;';
+		//$button .= '<input title="Change" accessKey="" tabindex="2" type="button" class="button" value="'.$app_strings['LBL_SELECT_PRODUCT_BUTTON_LABEL'].'" name="Button" LANGUAGE=javascript onclick=\'return window.open("index.php?module=Products&action=Popup&return_module=Products&popuptype=detailview&form=EditView&form_submit=false&recordid='.$_REQUEST["record"].'","test","width=600,height=400,resizable=1,scrollbars=1");\'>&nbsp;';
+	$returnset = '&return_module=Products&return_action=PriceBookDetailView&return_id='.$id;
+
+	//$list = GetRelatedList('PriceBook','Products',$focus,$query,$button,$returnset,'updatePbListPrice','DeletePbProductRel');
+	  $list = getPriceBookRelatedProducts($query,$focus);
+
+		
+	echo '</form>';
+}
 
 echo get_form_footer();
 
