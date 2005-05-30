a83004116da4f00434d27a59f48cc96432368257
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-05-30 12:57:14 +0000

    Ui enhancements done

--- modules/Orders/SalesOrderEditView.php
+++ modules/Orders/SalesOrderEditView.php
@@ -62,6 +62,9 @@ $block_2 = getBlockInformation("SalesOrder",2,$focus->mode,$focus->column_fields
 
 $block_3_header = getBlockTableHeader("LBL_DESCRIPTION_INFORMATION");
 $block_3 = getBlockInformation("SalesOrder",3,$focus->mode,$focus->column_fields);
+
+
+$block_4_header = getBlockTableHeader("LBL_RELATED_PRODUCTS");
 /*
 //get Custom Field Information
 $block_5 = getBlockInformation("Accounts",5,$focus->mode,$focus->column_fields);
@@ -101,6 +104,7 @@ $xtpl->assign("BLOCK2", $block_2);
 $xtpl->assign("BLOCK2_HEADER", $block_2_header);
 $xtpl->assign("BLOCK3", $block_3);
 $xtpl->assign("BLOCK3_HEADER", $block_3_header);
+$xtpl->assign("BLOCK4_HEADER", $block_4_header);
 
 if (isset($focus->name)) $xtpl->assign("NAME", $focus->name);
 else $xtpl->assign("NAME", "");
