e9d3059a54fcd4dca4fce04b69cb959e6e95185a
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-09 06:52:57 +0000

    * Modified the contact_id as parent_id and send details about ticket to the contact

--- modules/HelpDesk/Save.php
+++ modules/HelpDesk/Save.php
@@ -64,21 +64,52 @@ else
 $_REQUEST['name'] = '[ Ticket ID : '.$focus->id.' ] '.$reply.$_REQUEST['ticket_title'];
 $bodysubject = ' Subject : '.$focus->id.' : '.$_REQUEST['ticket_title'].'<br><br>';
 
-if($focus->column_fields['contact_id'] != '')
+if($focus->column_fields['parent_id'] != '')
 {
-	$sql = "select * from contactdetails where contactid=".$focus->column_fields['contact_id'];
-	$result = $adb->query($sql);
-	$contactname = $adb->query_result($result,0,'firstname').' '.$adb->query_result($result,0,'lastname');
+	$query = "select * from crmentity where crmid=".$focus->column_fields['parent_id'];
+	$parent_module = $adb->query_result($adb->query($query),0,'setype');
+	if($parent_module == 'Contacts')
+	{
+		$sql = "select * from contactdetails where contactid=".$focus->column_fields['parent_id'];
+		$result = $adb->query($sql);
+		$contactname = $adb->query_result($result,0,'firstname').' '.$adb->query_result($result,0,'lastname');
+		$contact_mailid = $adb->query_result($result,0,'email');
+	}
 }
+if($contact_mailid != '')
+{
+	$sql = "select * from PortalInfo where user_name='".$contact_mailid."'";
+	$isactive = $adb->query_result($adb->query($sql),0,'isactive');
+}
+if($isactive == 1)
+{
+	$bodydetails = "Dear ".$contactname.",<br><br>";
+	$bodydetails .= 'There is a reply to <b>'.$_REQUEST['ticket_title'].'</b> in the "Customer Portal" at VTiger.';
+	$bodydetails .= "You can use the following link to view the replies made:<br>";
 
-$bodydetails = "Dear ".$contactname.",<br><br>";
-$bodydetails .= 'There is a reply to <b>'.$_REQUEST['ticket_title'].'</b> in the "Customer Portal" at VTiger.';
-$bodydetails .= "You can use the following link to view the replies made:<br>";
-$bodydetails .= "<a href='http://rajeshkannan/customerportal/general.php?action=UserTickets&ticketid=".$focus->id."&fun=detail'>Ticket Details</a>";
-$bodydetails .= "<br><br>Thanks,<br><br> Vtiger Support Team ";
+	//Provide your customer portal url
+	$PORTAL_URL = "<customerportal-url:port>";//e.g : vtigercrm:90/customerportal
 
-$_REQUEST['description'] = $bodysubject.$bodydetails;
-$_REQUEST['parent_id'] = $_REQUEST['contact_id'];
+	$bodydetails .= "<a href='http://".$PORTAL_URL."/general.php?action=UserTickets&ticketid=".$focus->id."&fun=detail'>Ticket Details</a>";
+	$bodydetails .= "<br><br>Thanks,<br><br> Vtiger Support Team ";
+
+	$_REQUEST['description'] = $bodysubject.$bodydetails;
+}
+else
+{
+	$desc = 'Ticket ID : '.$focus->id.'<br> Ticket Title : '.$reply.$_REQUEST['ticket_title'];
+	$desc .= "<br><br>Dear ".$contactname.",<br><br>The Ticket is replied and the details are : <br>";
+	$desc .= "<br> Status : ".$focus->column_fields['ticketstatus'];
+	$desc .= "<br> Category : ".$focus->column_fields['ticketcategories'];
+	$desc .= "<br> Severity : ".$focus->column_fields['ticketseverities'];
+	$desc .= "<br> Priority : ".$focus->column_fields['ticketpriorities'];
+	$desc .= '<br><br>Description : <br>'.$focus->column_fields['description'];
+	$desc .= '<br><br>Solution : <br>'.$focus->column_fields['solution'];
+	$desc .= getTicketComments($focus->id);
+
+	$_REQUEST['description'] = $desc;
+}
+//$_REQUEST['parent_id'] = $_REQUEST['contact_id'];
 $_REQUEST['return_id'] = $return_id;
 
 if($_REQUEST['product_id'] != '' && $focus->id != '' && $_REQUEST['mode'] != 'edit')
@@ -91,4 +122,24 @@ if($_REQUEST['product_id'] != '' && $focus->id != '' && $_REQUEST['mode'] != 'ed
 require_once('modules/Emails/send_mail.php');
 //header("Location: index.php?action=$return_action&module=$return_module&record=$return_id");
 
+function getTicketComments($ticketid)
+{
+	global $adb;
+
+	$commentlist = '';
+	$sql = "select * from ticketcomments where ticketid=".$ticketid;
+	$result = $adb->query($sql);
+	for($i=0;$i<$adb->num_rows($result);$i++)
+	{
+		$comment = $adb->query_result($result,$i,'comments');
+		if($comment != '')
+		{
+			$commentlist .= '<br><br>'.$comment;
+		}
+	}
+	if($commentlist != '')
+		$commentlist = '<br><br> The comments are : '.$commentlist;
+
+	return $commentlist;
+}
 ?>
