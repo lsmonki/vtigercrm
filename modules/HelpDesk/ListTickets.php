6562917f1141dee15f907c8fadc5b554bf543925
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-09 06:45:49 +0000

    * Modifie the Contact name as Related to and give more link

--- modules/HelpDesk/ListTickets.php
+++ modules/HelpDesk/ListTickets.php
@@ -70,8 +70,9 @@ else
 */
 
 //$search_query="select troubletickets.ticketid,contact_id,priority,troubletickets.status,category,troubletickets.title,troubletickets.description,update_log,version_id,crmentity.createdtime,crmentity.modifiedtime, contactdetails.firstname,contactdetails.lastname,users.user_name from troubletickets inner join users on users.id=crmentity.smownerid left join contactdetails on troubletickets.contact_id=contactdetails.contactid left join seticketsrel on seticketsrel.ticketid=troubletickets.ticketid inner join crmentity on crmentity.crmid=troubletickets.ticketid and crmentity.smownerid=".$current_user->id." and crmentity.deleted=0";
-$search_query="select troubletickets.ticketid,contact_id,priority,troubletickets.status,category,troubletickets.title,troubletickets.description,update_log,version_id,crmentity.createdtime,crmentity.modifiedtime, contactdetails.firstname,contactdetails.lastname,users.user_name from troubletickets inner join crmentity on crmentity.crmid= troubletickets.ticketid inner join users on users.id=crmentity.smownerid left join contactdetails on troubletickets.contact_id=contactdetails.contactid left join seticketsrel on seticketsrel.ticketid=troubletickets.ticketid where crmentity.smownerid=".$current_user->id." and crmentity.deleted=0 and troubletickets.status <> 'Closed'  ORDER BY createdtime DESC";
+$search_query="select troubletickets.ticketid,parent_id,priority,troubletickets.status,category,troubletickets.title,troubletickets.description,update_log,version_id,crmentity.createdtime,crmentity.modifiedtime, contactdetails.firstname,contactdetails.lastname, account.accountid, account.accountname, users.user_name from troubletickets inner join crmentity on crmentity.crmid= troubletickets.ticketid inner join users on users.id=crmentity.smownerid left join contactdetails on troubletickets.parent_id=contactdetails.contactid left join account on account.accountid=troubletickets.parent_id left join seticketsrel on seticketsrel.ticketid=troubletickets.ticketid where crmentity.smownerid=".$current_user->id." and crmentity.deleted=0 and troubletickets.status <> 'Closed'  ORDER BY createdtime DESC";
 
+$resultcount = $adb->num_rows($adb->query($search_query));
 $tktresult = $adb->limitquery($search_query,0,5);
 $ticketListheader = get_form_header($current_module_strings['LBL_MY_TICKETS'], "", false );
 echo "<br>";
@@ -92,7 +93,7 @@ $list.='<td class="moduleListTitle" height="21" style="padding:0px 3px 0px 3px;"
 $list.='<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
 $list.='<td class="moduleListTitle" height="21" style="padding:0px 3px 0px 3px;">'.$current_module_strings['LBL_SUBJECT'].'</td>';
 $list.='<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
-$list.='<td class="moduleListTitle" height="21" style="padding:0px 3px 0px 3px;">'.$current_module_strings['LBL_CONTACT'].'</td>';
+$list.='<td class="moduleListTitle" height="21" style="padding:0px 3px 0px 3px;">'.$current_module_strings['Related to'].'</td>';
 $list.='<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
 $list.='<td class="moduleListTitle" height="21" style="padding:0px 3px 0px 3px;">'.$current_module_strings['LBL_STATUS'].'</td>';
 $list.='<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
@@ -117,8 +118,15 @@ for ($i=0; $i<$adb->num_rows($tktresult); $i++)
         $subject = '<a href="index.php?action=DetailView&module=HelpDesk&record='.$adb->query_result($tktresult,$i,"ticketid").'">'.$adb->query_result($tktresult,$i,"title").'</a>';
        $list .= '<td style="padding:0px 3px 0px 3px;">'.$subject.'</td>';
                 $list .= '<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
-        $contact_name = '<a href="index.php?action=DetailView&module=Contacts&record='.$adb->query_result($tktresult,$i,"contact_id").'">'.$adb->query_result($tktresult,$i,"firstname").' '.$adb->query_result($tktresult,$i,"lastname").'</a>';
-        $list .= '<td style="padding:0px 3px 0px 3px;">'.$contact_name.'</td>';
+
+	$parent_id = $adb->query_result($tktresult,$i,"parent_id");
+	$parent_name = '';
+	if($parent_id != '' && $parent_id != NULL)
+	{
+		$parent_name = getParentLink($parent_id);
+	}
+
+        $list .= '<td style="padding:0px 3px 0px 3px;">'.$parent_name.'</td>';
                 $list .= '<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
         $list .= '<td style="padding:0px 3px 0px 3px;">'.$adb->query_result($tktresult,$i,"status").'</td>';
                 $list .= '<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
@@ -128,7 +136,35 @@ for ($i=0; $i<$adb->num_rows($tktresult); $i++)
                 $list .= '<td WIDTH="1" class="blackLine" NOWRAP><IMG SRC="'.$image_path.'blank.gif"></td>';
         $list .= '</tr>';
 }
-$list.='</td></tr></table>';
+
+if($resultcount > 5)
+        $list .= '</td></tr><tr><td colspan="11">&nbsp;</td><td align="right"><a href="index.php?action=index&module=HelpDesk">'.$current_module_strings['LBL_MORE'].'...&nbsp;&nbsp;</a></td>';
+
+$list.='</table>';
 echo $list;
+
+function getParentLink($parent_id)
+{
+	global $adb;
+
+	$sql = "select setype from crmentity where crmid=".$parent_id;
+	$parent_module = $adb->query_result($adb->query($sql),0,'setype');
+	
+	if($parent_module == 'Contacts')
+	{
+		$sql = "select firstname,lastname from contactdetails where contactid=".$parent_id;
+		$parentname = $adb->query_result($adb->query($sql),0,'firstname');
+		$parentname .= ' '.$adb->query_result($adb->query($sql),0,'lastname');
+	        $parent_name = '<a href="index.php?action=DetailView&module='.$parent_module.'&record='.$parent_id.'">'.$parentname.'</a>';
+	}
+	if($parent_module == 'Accounts')
+	{
+		$sql = "select accountname from account where accountid=".$parent_id;
+		$parentname = $adb->query_result($adb->query($sql),0,'accountname');
+	        $parent_name = '<a href="index.php?action=DetailView&module='.$parent_module.'&record='.$parent_id.'">'.$parentname.'</a>';
+	}
+
+	return $parent_name;	
+}
 
 ?>
