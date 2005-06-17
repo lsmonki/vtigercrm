9f41e8b16dc35d00a5877de743cddcf7e29a15b6
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-17 11:40:19 +0000

    Inserting the new values for company query is inserted

--- modules/Settings/SaveCompany.php
+++ modules/Settings/SaveCompany.php
@@ -26,7 +26,14 @@ $sql="select * from organizationdetails where organizationame = '".$org_name."'"
 $result = $adb->query($sql);
 $org_name = $adb->query_result($result,0,'organizationame');
 
+if($org_name=='')
+{
+	$sql="insert into organizationdetails values( '".$organization_name ."','".$organization_address."','". $organization_city."','".$organization_state."','".$organization_code."','".$organization_country."','".$organization_phone."','".$organization_fax."','".$organization_website."')";
+}
+else
+{
 	$sql="update organizationdetails set organizationame = '".$organization_name."', address = '".$organization_address."', city = '".$organization_city."', state = '".$organization_state."',  code = '".$organization_code."', country = '".$organization_country."' ,  phone = '".$organization_phone."' ,  fax = '".$organization_fax."',  website = '".$organization_website."' where organizationame = '".$org_name."'";
+}	
 
 
 $adb->query($sql);
