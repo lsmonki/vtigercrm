699c9703c5ef435d41cb4d83b97988f0cf0c2c5e
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-21 07:48:26 +0000

    passed the imapServerAddress by getting from db

--- modules/squirrelmail-1.4.4/right_main.php
+++ modules/squirrelmail-1.4.4/right_main.php
@@ -85,6 +85,8 @@ require_once('modules/Users/UserInfoUtil.php');
 $mailInfo = getMailServerInfo($current_user);
 $temprow = $adb->fetch_array($mailInfo);
 $secretkey=$temprow["mail_password"];
+$imapServerAddress=$temprow["mail_servername"];
+
 /* end of get globals */
 //$secretkey="p1";
     $key = OneTimePadEncrypt($secretkey, $onetimepad);
