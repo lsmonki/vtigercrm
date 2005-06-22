91992c5684de6349adb5e2b9bea741f7186e505e
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-22 20:23:12 +0000

    Added mapping > 'Quote Stage'=>'Quote Stage', > 'Potential Name'=>'Potential Name', > 'Total'=>'Total Amount',

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -659,9 +659,6 @@ $app_strings = Array(
 'LBL_RSS_FEEDS'=>'RSS Feeds',
 'LBL_ADD_RSS_FEEDS'=>'Add New RSS Feed',
 
-// Added for  4.2 GA
-'LBL_AVAILABLE'=>'Availability',
-
 //Added fields after 4.2 alpha
 'COMBO_ACCOUNTS'=>'Accounts',
 'LNK_NEW_FAQ'=>'New Faq',
@@ -669,6 +666,9 @@ $app_strings = Array(
 'LNK_WEARE'=>'We are?',
 'LBL_ABOUTUS_TITLE'=>'vtiger CRM - About Us',
 'LBL_DISCUSS'=>'Discuss with other users',
+'Quote Stage'=>'Quote Stage',
+'Potential Name'=>'Potential Name',
+'Total'=>'Total Amount',
 
 );
 
