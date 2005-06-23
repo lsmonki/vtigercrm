eecb1f35d252512426923d2d767e81f6406a0180
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-23 15:04:17 +0000

    * changes made for popup

--- modules/Products/VendorPopup.php
+++ modules/Products/VendorPopup.php
@@ -54,26 +54,19 @@ $url_string = ''; // assigning http url string
 $sorder = 'ASC';  // Default sort order
 if(isset($_REQUEST['sorder']) && $_REQUEST['sorder'] != '')
 $sorder = $_REQUEST['sorder'];
-/*
+
 if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] == 'true')
 {
 	$url_string .="&query=true";
-	if (isset($_REQUEST['productname'])) $productname = $_REQUEST['productname'];
-        if (isset($_REQUEST['productcode'])) $productcode = $_REQUEST['productcode'];
-        if (isset($_REQUEST['commissionrate'])) $commissionrate = $_REQUEST['commissionrate'];
-	if (isset($_REQUEST['qtyperunit'])) $qtyperunit = $_REQUEST['qtyperunit'];
-        if (isset($_REQUEST['unitprice'])) $unitprice = $_REQUEST['unitprice'];
-        if (isset($_REQUEST['manufacturer'])) $manufacturer = $_REQUEST['manufacturer'];
-        if (isset($_REQUEST['productcategory'])) $productcategory = $_REQUEST['productcategory'];
-	if (isset($_REQUEST['start_date'])) $start_date = $_REQUEST['start_date'];
-        if (isset($_REQUEST['expiry_date'])) $expiry_date = $_REQUEST['expiry_date'];
-        if (isset($_REQUEST['purchase_date'])) $purchase_date = $_REQUEST['purchase_date'];
-
+	if (isset($_REQUEST['vendorname'])) $vendorname = $_REQUEST['vendorname'];
+        if (isset($_REQUEST['companyname'])) $companyname = $_REQUEST['companyname'];
+        if (isset($_REQUEST['category'])) $category = $_REQUEST['category'];
+	
 	$where_clauses = Array();
 	//$search_query='';
 
 	//Added for Custom Field Search
-	$sql="select * from field where tablename='productcf' order by fieldlabel";
+	$sql="select * from field where tablename='vendorcf' order by fieldlabel";
 	$result=$adb->query($sql);
 	for($i=0;$i<$adb->num_rows($result);$i++)
 	{
@@ -82,13 +75,13 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
 		$uitype[$i]=$adb->query_result($result,$i,'uitype');
 
 	        if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
-	
+
 	        if(isset($customfield[$i]) && $customfield[$i] != '')
 	        {
 			if($uitype[$i] == 56)
-                                $str=" productcf.".$column[$i]." = 1";
+                                $str=" vendorcf.".$column[$i]." = 1";
                         else
-			        $str=" productcf.".$column[$i]." like '$customfield[$i]%'";
+			        $str="vendorcf.".$column[$i]." like '$customfield[$i]%'";
 		        array_push($where_clauses, $str);
 	       	//	  $search_query .= ' and '.$str;
 			$url_string .="&".$column[$i]."=".$customfield[$i];
@@ -96,70 +89,27 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
 	}
 	//upto this added for Custom Field
 
-	if (isset($productname) && $productname !='')
+	if (isset($vendorname) && $vendorname !='')
 	{
-		array_push($where_clauses, "productname like ".PearDatabase::quote($productname.'%'));
+		array_push($where_clauses, "name like ".PearDatabase::quote($vendorname.'%'));
 		//$search_query .= " and productname like '".$productname."%'";
-		$url_string .= "&productname=".$productname;
-	}
-	
-	if (isset($productcode) && $productcode !='')
-	{
-		array_push($where_clauses, "productcode like ".PearDatabase::quote($productcode.'%'));
-		//$search_query .= " and productcode like '".$productcode."%'";
-		$url_string .= "&productcode=".$productcode;
+		$url_string .= "&vendorname=".$vendorname;
 	}
 
-	if (isset($commissionrate) && $commissionrate !='')
+	if (isset($companyname) && $companyname !='')
 	{
-		array_push($where_clauses, "commissionrate like ".PearDatabase::quote($commissionrate.'%'));
-		 //$search_query .= " and commissionrate like '".$commissionrate."%'";
-		 $url_string .= "&commissionrate=".$commissionrate;
+		array_push($where_clauses, "company_name like ".PearDatabase::quote($companyname.'%'));
+		//$search_query .= " and productcode like '".$productcode."%'";
+		$url_string .= "&companyname=".$companyname;
 	}
 	
-	if (isset($qtyperunit) && $qtyperunit !='')
+	if (isset($category) && $category !='')
 	{
-		array_push($where_clauses, "qty_per_unit like ".PearDatabase::quote($qtyperunit.'%'));
-	 	//$search_query .= " and qty_per_unit like '".$qtyperunit."%'";
-		$url_string .= "&qtyperunit=".$qtyperunit;
+		array_push($where_clauses, "category like ".PearDatabase::quote($category.'%'));
+		//$search_query .= " and productcode like '".$productcode."%'";
+		$url_string .= "&category=".$category;
 	}
-	
-	if (isset($unitprice) && $unitprice !='')
-	{
-		array_push($where_clauses, "unit_price like ".PearDatabase::quote($unitprice.'%'));
-	 //	$search_query .= " and unit_price like '".$unitprice."%'";
-		$url_string .= "&unitprice=".$unitprice;
-	}
-	if (isset($manufacturer) && $manufacturer !='' && $manufacturer !='--None--')
-        {
-		array_push($where_clauses, "manufacturer like ".PearDatabase::quote($manufacturer.'%'));
-        	//$search_query .= " and manufacturer like '".$manufacturer."%'";
-                $url_string .= "&manufacturer=".$manufacturer;
-	}
-	if (isset($productcategory) && $productcategory !='' && $productcategory !='--None--')
-        {
-		array_push($where_clauses, "productcategory like ".PearDatabase::quote($productcategory.'%'));
-        	//$search_query .= " and productcategory like '".$productcategory."%'";
-                $url_string .= "&productcategory=".$productcategory;
-	}
-	if (isset($start_date) && $start_date !='')
-        {
-		array_push($where_clauses, "start_date like ".PearDatabase::quote($start_date.'%'));
-                //$search_query .= " and start_date = '".$start_date."%'";
-                $url_string .= "&start_date=".$start_date;
-        } 
-	if (isset($expiry_date) && $expiry_date !='')
-        {
-		array_push($where_clauses, "expiry_date like ".PearDatabase::quote($expiry_date.'%'));
-                //$search_query .= " and expiry_date = '".$expiry_date."%'";
-                $url_string .= "&expiry_date=".$expiry_date;
-        } 
-	if (isset($purchase_date) && $purchase_date !='')
-        {
-		array_push($where_clauses, "purchase_date like ".PearDatabase::quote($purchase_date.'%'));
-                //$search_query .= " and purchase_date = '".$purchase_date."%'";
-                $url_string .= "&purchase_date=".$purchase_date;
-        }
+
 	$where = "";
 	foreach($where_clauses as $clause)
 	{
@@ -169,19 +119,20 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
 	}
 
 	$log->info("Here is the where clause for the list view: $where");
- 
+
 
 }
 
 //Constructing the Search Form
 if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
         // Stick the form header out there.
-	echo get_form_header($current_module_strings['LBL_SEARCH_FORM_TITLE'],'', false);
-        $search_form=new XTemplate ('modules/Products/SearchForm.html');
+	echo get_form_header($current_module_strings['LBL_VENDOR_SEARCH_FORM_TITLE'],'', false);
+        $search_form=new XTemplate ('modules/Products/PopupVendorSearchForm.html');
         $search_form->assign("MOD", $mod_strings);
         $search_form->assign("APP", $app_strings);
 	$clearsearch = 'true';
-	
+
+	$search_form->assign("POPUPTYPE",$popuptype);
 	if ($order_by !='') $search_form->assign("ORDER_BY", $order_by);
 	if ($sorder !='') $search_form->assign("SORDER", $sorder);
 	$search_form->assign("JAVASCRIPT", get_clear_form_js());
@@ -195,27 +146,13 @@ if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
 	$search_form->assign("BASIC_LINK", "index.php?module=Products".$ordby."&action=index".$url_string."&sorder=".$sorder);
 	$search_form->assign("ADVANCE_LINK", "index.php?module=Products&action=index".$ordby."&advanced=true".$url_string."&sorder=".$sorder);
 
-	if ($productname !='') $search_form->assign("PRODUCT_NAME", $productname);
-	if ($commissionrate !='') $search_form->assign("COMMISSION_RATE", $commissionrate);
-	if ($productcode !='') $search_form->assign("PRODUCT_CODE", $productcode);
-	if ($qtyperunit !='') $search_form->assign("QTYPERUNIT", $qtyperunit);
-	if ($unitprice !='') $search_form->assign("UNITPRICE", $unitprice);
-	if (isset($_REQUEST['manufacturer'])) $manufacturer = $_REQUEST['manufacturer'];	
-	if (isset($_REQUEST['productcategory'])) $productcategoty = $_REQUEST['productcategory'];	
-	if (isset($_REQUEST['start_date'])) $start_date = $_REQUEST['start_date'];	
-	if (isset($_REQUEST['expiry_date'])) $expiry_date = $_REQUEST['expiry_date'];	
-	if (isset($_REQUEST['purchase_date'])) $purchase_date = $_REQUEST['purchase_date'];	
-
-//Combo Fields for Manufacturer and Category are moved from advanced to Basic Search
-        if (isset($manufacturer)) $search_form->assign("MANUFACTURER", get_select_options($comboFieldArray['manufacturer_dom'], $manufacturer, $clearsearch));
-        else $search_form->assign("MANUFACTURER", get_select_options($comboFieldArray['manufacturer_dom'], '', $clearsearch));
-        if (isset($productcategory)) $search_form->assign("PRODUCTCATEGORY", get_select_options($comboFieldArray['productcategory_dom'], $productcategoty, $clearsearch));
-        else $search_form->assign("PRODUCTCATEGORY", get_select_options($comboFieldArray['productcategory_dom'], '', $clearsearch));
-
-        if (isset($_REQUEST['advanced']) && $_REQUEST['advanced'] == 'true') 
+	if ($vendorname !='') $search_form->assign("VENDOR_NAME", $vendorname);
+	if ($companyname !='') $search_form->assign("COMPANY_NAME", $companyname);
+	if ($category !='') $search_form->assign("CATEGORY", $category);
+        if (isset($_REQUEST['advanced']) && $_REQUEST['advanced'] == 'true')
 	{
 		$url_string .="&advanced=true";
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','index','productname','true','advanced'));
+		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','index','name','true','advanced'));
 
 		$search_form->assign("SUPPORT_START_DATE",$_REQUEST['start_date']);
 		$search_form->assign("SUPPORT_EXPIRY_DATE",$_REQUEST['expiry_date']);
@@ -223,7 +160,7 @@ if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
 		$search_form->assign("DATE_FORMAT", $current_user->date_format);
 
 		//Added for Custom Field Search
-		$sql="select * from field where tablename='productcf' order by fieldlabel";
+		$sql="select * from field where tablename='vendorcf' order by fieldlabel";
 		$result=$adb->query($sql);
 		for($i=0;$i<$adb->num_rows($result);$i++)
 		{
@@ -232,7 +169,7 @@ if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
 		        if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
 		}
 		require_once('include/CustomFieldUtil.php');
-		$custfld = CustomFieldSearch($customfield, "productcf", "productcf", "productid", $app_strings,$theme,$column,$fieldlabel);
+		$custfld = CustomFieldSearch($customfield, "vendorcf", "vendorcf", "vendorid", $app_strings,$theme,$column,$fieldlabel);
 		$search_form->assign("CUSTOMFIELD", $custfld);
 		//upto this added for Custom Field
 
@@ -240,8 +177,8 @@ if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
                 $search_form->out("advanced");
 	}
 	else
-	{        
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','index','productname','true','basic'));
+	{
+		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','VendorPopup','vendorname','true','basic',$popuptype));
 		$search_form->parse("main");
 	        $search_form->out("main");
 	}
@@ -249,17 +186,16 @@ echo get_form_footer();
 //echo '<br><br>';
 
 }
-*/
 
 //Retreive the list from Database
 
 $list_query = getListQuery("Vendor");
-/*
+
 if(isset($where) && $where != '')
 {
         $list_query .= ' and '.$where;
 }
-*/
+
 $xtpl->assign("VENDORLISTHEADER", get_form_header($current_module_strings['LBL_LIST_VENDOR_FORM_TITLE'],'', false ));
 
 if(isset($order_by) && $order_by != '')
@@ -334,7 +270,7 @@ $url_string .="&order_by=".$order_by;
 if($sorder !='')
 $url_string .="&sorder=".$sorder;
 
-$navigationOutput = getTableHeaderNavigation($navigation_array, $url_string,"Vendor",'Popup');
+$navigationOutput = getTableHeaderNavigation($navigation_array, $url_string,"Vendor",'VendorPopup');
 $xtpl->assign("NAVIGATION", $navigationOutput);
 $xtpl->assign("RECORD_COUNTS", $record_string);
 
