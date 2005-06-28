654a529e1aa9a3a146cf37c09ab2b879f27297eb
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-06-28 15:12:42 +0000

    * check enhanced for product and potential id

--- modules/Quotes/EditView.php
+++ modules/Quotes/EditView.php
@@ -48,13 +48,13 @@ if(isset($_REQUEST['isDuplicate']) && $_REQUEST['isDuplicate'] == 'true') {
 	$focus->id = "";
     	$focus->mode = ''; 	
 }
-if(isset($_REQUEST['potential_id']))
+if(isset($_REQUEST['potential_id']) || $_REQUEST['potential_id'] !='')
 {
         $focus->column_fields['potential_id'] = $_REQUEST['potential_id'];
 	$num_of_products = getNoOfAssocProducts("Potentials",$focus,$focus->column_fields['potential_id']);
         $associated_prod = getAssociatedProducts("Potentials",$focus,$focus->column_fields['potential_id']);
 }
-if(isset($_REQUEST['product_id']))
+if(isset($_REQUEST['product_id']) || $_REQUEST['product_id'] !='')
 {
         $focus->column_fields['product_id'] = $_REQUEST['product_id'];
         $num_of_products = getNoOfAssocProducts("Products",$focus,$focus->column_fields['product_id']);
