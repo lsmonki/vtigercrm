198782491a410a4346631eadafb626f759d3f41d
venkatraj <venkatraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-02 15:35:40 +0000

    - bug fixed to show selectedview after sorting

--- modules/Products/ListView.php
+++ modules/Products/ListView.php
@@ -389,6 +389,8 @@ else
 $record_string= $app_strings[LBL_SHOWING]." " .$start_rec." - ".$end_rec." " .$app_strings[LBL_LIST_OF] ." ".$noofrows;
 
 //Retreive the List View Table Header
+if($viewid !='')
+$url_string .= "&viewname=".$viewid;
 
 $listview_header = getListViewHeader($focus,"Products",$url_string,$sorder,$order_by,"",$oCustomView);
 $xtpl->assign("LISTHEADER", $listview_header);
