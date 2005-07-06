835ad842f64a5bdc703782034abe4977285d9227
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-06 09:16:54 +0000

    fixed migration for windows single machine with both the dbs

--- datamigration.bat
+++ datamigration.bat
@@ -2,16 +2,19 @@ echo off
 set ins_dir4_0=%~1
 set VTIGER_HOME=%cd%
 echo %VTIGER_HOME%
-set version="4.0"
+set version="4.0.1"
 
 :getinstalldir
 if NOT "X%ins_dir4_0%" == "X" goto checkdir
 
-set /p ins_dir4_0="Enter the vtiger CRM 4.0 installation bin directory (For example: C:\Program Files\vtigerCRM3_0\bin: "
+echo "*******************"
+echo "*******************"
+echo "*******************"
+set /p ins_dir4_0="Enter the vtiger CRM 4.0.1 installation bin directory (For example: C:\Program Files\vtigerCRM4_0_1\bin: "
 goto checkdir
 
 :findstrdir
-echo "4.0 install dir is %ins_dir4_0"
+echo "4.0.1 install dir is %ins_dir4_0%"
 echo %WINDIR%
 set FIND_STR="%WINDIR%\system32\findstr.exe"
 set SLEEP_STR="%WINDIR%\system32\ping.exe"
@@ -19,7 +22,7 @@ goto readMySQLparams
 
 :checkdir
 if NOT EXIST "%ins_dir4_0%\startvTiger.bat" (
-	echo "Invalid vtigerCRM 4.0 installation directory specified"
+	echo "Kindly specify a valid vtigerCRM 4.0.1 installation directory please"
 	set ins_dir4_0=
 	goto getinstalldir	
 )
@@ -28,12 +31,12 @@ goto findstrdir
 :readMySQLparams
 
 echo "Reading the vtiger CRM %version% MySQL Parameters"
-if %version% == "4.0" (
-	echo "Inside 4.0 loop"
-	%FIND_STR% /C:"set mysql_" "%ins_dir4_0%\startvTiger.bat" > mysql_params.bat
-)	
 if %version% == "4.0.1" (
 	echo "Inside 4.0.1 loop"
+	%FIND_STR% /C:"set mysql_" "%ins_dir4_0%\startvTiger.bat" > mysql_params.bat
+)	
+if %version% == "4.2" (
+	echo "Inside 4.2 loop"
 	%FIND_STR% /C:"set mysql_" startvTiger.bat > mysql_params.bat
 )	
 call mysql_params.bat
@@ -45,8 +48,8 @@ echo %mysql_port%
 echo %mysql_bundled%
 echo %mysql_dir_4_0_1%
 
-if %version% == "4.0" (
-	set /p mysql_host_name_4_0="Specify the host name of the vtiger CRM 4.0 mysql server:  "
+if %version% == "4.0.1" (
+	set /p mysql_host_name_4_0="Specify the host name of the vtiger CRM 4.0.1 mysql server:  "
 
 	echo ^<?php > ..\apache\htdocs\vtigerCRM\migrator_connection.php
 	echo $mysql_host_name_old = '%mysql_host_name_4_0%'; >> ..\apache\htdocs\vtigerCRM\migrator_connection.php
@@ -66,8 +69,8 @@ echo "Checking whether the vtiger CRM %version% MySQL server is already running"
 "%mysql_dir%\bin\mysql" --port=%mysql_port% --user=%mysql_username% --password=%mysql_password% -e "show databases" > NUL
 IF ERRORLEVEL 1 goto startmysql
 ECHO  "vtiger CRM %version% MySQL Server is already started and running"
-if %version% == "4.0" goto dump4_0mysql
-if %version% == "4.0.1" goto dumpin4_0_1mysql
+if %version% == "4.0.1" goto dump4_0_1mysql
+if %version% == "4.2" goto dumpin4_2mysql
 
 :startmysql
 echo "Starting %version% vtiger MySQL on port specified by the user"
@@ -78,8 +81,8 @@ start mysqld -b .. --datadir=../data --port=%mysql_port%
 IF ERRORLEVEL 1 goto notstarted
 echo "Started vtiger CRM %version% MySQL on port specified by the user"
 cd /d %VTIGER_HOME%
-if %version% == "4.0" goto dump4_0mysql
-if %version% == "4.0.1" goto dumpin4_0_1mysql
+if %version% == "4.0.1" goto dump4_0_1mysql
+if %version% == "4.2" goto dumpin4_2mysql
 
 :notstarted
 cd /d %VTIGER_HOME%
@@ -91,21 +94,28 @@ echo ""
 set /p pt=Free the port and Press Any Key to Continue...
 goto startmysql
 
-:dump4_0mysql
-echo set FOREIGN_KEY_CHECKS=0; > vtiger_4_0_dump.txt
-"%mysql_dir_4_0_1%\mysqldump" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm4 >> vtiger_4_0_dump.txt
+:dump4_0_1mysql
+echo ' about to dump the migrated database to the vtiger_4_2_dump.txt file'
+echo set FOREIGN_KEY_CHECKS=0; > vtiger_4_2_dump.txt
+"%mysql_dir_4_0_1%\mysqldump" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm4_0_1 >> vtiger_4_2_dump.txt
 IF ERRORLEVEL 1 (
 	 echo "Unable to take the vtiger CRM %version% database backup. vtigercrm database may be corrupted"
 	 goto exitmigration
 ) 
 echo "Data dump taken successfully in vtiger_dump.txt"
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "create database vtigercrm_4_0_bkp"
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% --force vtigercrm_4_0_bkp < vtiger_4_0_dump.txt
-rem "%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm_4_0_bkp < migrate_4_0to4_0_1.sql
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "create database vtigercrm_4_0_1_bkp"
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% --force vtigercrm_4_0_1_bkp < vtiger_4_2_dump.txt
+rem "%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm_4_0_1_bkp < migrate_4_0to4_0_1.sql
+
+echo 'about to start the input from the DataMigration.php file '
 ..\php\php.exe -f ..\apache\htdocs\vtigerCRM\Migrate.php
+echo 'exporting the migrated data to the dump file migrated_vtiger_4_0_1_dump file'
+
 echo set FOREIGN_KEY_CHECKS=0; > migrated_vtiger_4_0_1_dump.txt
-"%mysql_dir_4_0_1%\mysqldump" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm_4_0_bkp >> migrated_vtiger_4_0_1_dump.txt
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "drop database vtigercrm_4_0_bkp"
+"%mysql_dir_4_0_1%\mysqldump" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm_4_0_1_bkp >> migrated_vtiger_4_0_1_dump.txt
+
+echo ' about to drop the vtigercrm_4_0_1_bkp database '
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "drop database vtigercrm_4_0_1_bkp"
 goto stopMySQL
 
 :stopMySQL
@@ -117,21 +127,24 @@ if %mysql_bundled% == true (
         cd /d %VTIGER_HOME%
 	%SLEEP_STR% -n 11 127.0.0.1>nul
 )
-goto set4_0_1version
+goto set4_2version
 
-:set4_0_1version
-set version="4.0.1"
+:set4_2version
+set version="4.2"
 goto readMySQLparams
 
-:dumpin4_0_1mysql
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "drop database vtigercrm4_0_1"
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "create database if not exists vtigercrm4_0_1"
-"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm4_0_1 --force < migrated_vtiger_4_0_1_dump.txt  2> migrate_log.txt
+:dumpin4_2mysql
+echo 'about to drop 4_2 db'
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "drop database vtigercrm4_2"
+echo 'about create if not exists drop 4_2 db'
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% -e "create database if not exists vtigercrm4_2"
+echo 'about to force dump data to the 4_2 db from the migrated_vtiger_4_0_1_dump file'
+"%mysql_dir%/bin/mysql" --user=%mysql_username% --password=%mysql_password% --port=%mysql_port% vtigercrm4_2 --force < migrated_vtiger_4_0_1_dump.txt  2> migrate_log.txt
 IF ERRORLEVEL 1 (
-	 echo "Unable to dump data into the vtiger CRM %version% database vtigercrm4_0_1. Check the migrate_log.txt in the %VTIGER_HOME% directory"
+	 echo "Unable to dump data into the vtiger CRM %version% database vtigercrm4_2. Check the migrate_log.txt in the %VTIGER_HOME% directory"
 	 goto exitmigration
 )
-echo "Data successfully migrated into vtiger CRM 4.0.1 database"
+echo "Data successfully migrated into vtiger CRM 4.2 database"
 goto end 
 
 :exitmigration
