750ce4416d9f8d82b8be7a92083772a97fb138af
venkatraj <venkatraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-10 15:58:04 +0000

    - changes made for UI enhancements

--- modules/Home/index.php
+++ modules/Home/index.php
@@ -1,7 +1,7 @@
 <?php
 /*********************************************************************************
  * The contents of this file are subject to the SugarCRM Public License Version 1.1.2
- * ("License"); You may not use this file except in compliance with the 
+ * ("License"); You may not use this file except in compliance with the
  * License. You may obtain a copy of the License at http://www.sugarcrm.com/SPL
  * Software distributed under the License is distributed on an  "AS IS"  basis,
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
@@ -91,7 +91,7 @@ $list .= $app_strings['LBL_GROUP_NAME'].'</td>';
 $list .= '<td WIDTH="1" class="blackLine"><IMG SRC="'.$image_path.'blank.gif"></td>';
 $list .= '<td class="moduleListTitle" style="padding:0px 3px 0px 3px"> ';
 $list .= $app_strings['LBL_ENTITY_TYPE'].'</td>';
-$list .= '</tr>';
+$list .= '<td WIDTH="1" class="blackLine"><IMG SRC="'.$image_path.'blank.gif"></td></tr>';
 $list .= ' ';
 
 
