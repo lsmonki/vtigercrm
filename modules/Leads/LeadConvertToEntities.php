8f0c03358b92f8df12eb4402d4a2fcac8aa3b189
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-13 05:29:39 +0000

    Logging is added -jaguar

--- modules/Leads/LeadConvertToEntities.php
+++ modules/Leads/LeadConvertToEntities.php
@@ -13,6 +13,8 @@ require_once('include/database/PearDatabase.php');
 require_once('modules/Leads/Lead.php');
 //Getting the Parameters from the ConvertLead Form
 $id = $_REQUEST["record"];
+
+
 $module = $_REQUEST["module"];
 $assigned_user_id = $_REQUEST["smowerid"];
 $createpotential = $_REQUEST["createpotential"];
@@ -50,7 +52,7 @@ $crmid = $adb->getUniqueID("crmentity");
 //function for getting the custom values from leads and saving to account/contact/potential custom fields -Jag
 function getInsertValues($type,$type_id)
 {
-	global $id,$adb;
+	global $id,$adb,$vtlog;
 
 	$sql_convert_lead="select * from convertleadmapping ";
 	$convert_result = $adb->query($sql_convert_lead);
@@ -59,67 +61,78 @@ function getInsertValues($type,$type_id)
 	for($i=0;$i<$noofrows;$i++)
 	{
 		$flag="false";
-		//echo ">>>>>>>>>>>>>>>>>>>> in COnvertleadmapp table For    >>>>>>>>>>>>>>>>>>>>>>>";
-		$lead_id=$adb->query_result($convert_result,$i,"leadfid");
-		//Getting the relatd customfields for Accounts/Contact/potential from convertleadmapping table
-
+		$vtlog->logthis("In convertleadmapping function",'info');
+		$lead_id=$adb->query_result($convert_result,$i,"leadfid");  
+		//Getting the relatd customfields for Accounts/Contact/potential from convertleadmapping table	
 		$account_id_val=$adb->query_result($convert_result,$i,"accountfid");
 		$contact_id_val=$adb->query_result($convert_result,$i,"contactfid");
 		$potential_id_val=$adb->query_result($convert_result,$i,"potentialfid");
+		
+		$sql_leads_column="select field.fieldid,field.columnname from field,tab where field.tabid=tab.tabid and generatedtype=2 and tab.name='Leads' and fieldid=".$lead_id; //getting the columnname for the customfield of the lead
 
-		$sql_leads_column="select field.fieldid,field.columnname from field,tab where field.tabid=tab.tabid and generatedtype=2 and tab.name='Leads' and fieldid=".$lead_id; //getting the columnname for the customfield
-		//echo $sql_leads_column;
+		$vtlog->logthis("Lead's custom field coumn name is ".$sql_leads_column,'debug');
 
 		$lead_column_result = $adb->query($sql_leads_column);
 		$leads_no_rows = $adb->num_rows($lead_column_result);
 		if($leads_no_rows>0)
 		{
 			$lead_column_name=$adb->query_result($lead_column_result,0,"columnname");
-			$sql_leads_val="select ".$lead_column_name." from leadscf where leadid=".$id; //custom
-			field value for lead
+			$sql_leads_val="select ".$lead_column_name." from leadscf where leadid=".$id; //custom field value for lead
 			$lead_val_result = $adb->query($sql_leads_val);
 			$lead_value=$adb->query_result($lead_val_result,0,$lead_column_name);
-			//echo ">>>>>>>>>>>Lead column value $insert_value >>>>>>>>>.";
-		}
+			$vtlog->logthis("Lead's custom field value is ".$lead_value,'debug');
+		}	
 		//Query for getting the column name for Accounts/Contacts/Potentials if custom field for lead is mappped
-		$sql_type="select field.fieldid,field.columnname from field,tab where field.tabid=tab.tabid and generatedtype=2 and tab.name=";
-
-		if($type=="Accounts")                                                                                         {
-			if($account_id_val!="" && $account_id_val!=0)                                                                 {
-				$flag="true";                                                                                                 //echo "<h3>  in Account having value  >> lead id -- $lead_id </h3>";
-				$sql_type.="'Accounts' and fieldid=".$account_id_val;                                                 }
-			}                                                                                                             else if($type == "Contacts")
-			{                                                                                                                     if($contact_id_val!="" && $contact_id_val!=0)
-			{                                                                                                                     $flag="true";
-			$sql_type.="'Contacts' and fieldid=".$contact_id_val;
-			//echo $sql_type;                                                                                     }
-		}                                                                                                             else if($type == "Potentials")
-		{                                                                                                                     if($potential_id_val!="" && $potential_id_val!=0)
-			{       
+		$sql_type="select field.fieldid,field.columnname from field,tab where field.tabid=tab.tabid and generatedtype=2 and tab.name="; 
+		if($type=="Accounts")
+		{
+			if($account_id_val!="" && $account_id_val!=0)	
+			{
 				$flag="true";
-				$sql_type.="'Potentials' and fieldid=".$potential_id_val;  
+				$vtlog->logthis("Getting the  Accounts custom field column name  ",'info');
+				$sql_type.="'Accounts' and fieldid=".$account_id_val;
 			}
 		}
-		if($flag=="true")                                                                                             {
-			$type_result=$adb->query($sql_type);
-			if(isset($type_insert_column))                                                                                        $type_insert_column.=",";
-			$type_insert_column.=$adb->query_result($type_result,0,"columnname") ;
-			if(isset($insert_value))
-			$insert_value.=",";
+		else if($type == "Contacts")
+		{	
+			if($contact_id_val!="" && $contact_id_val!=0)	
+			{
+				$flag="true";
+				$vtlog->logthis("Getting the  Contacts custom field column name  ",'info');
+				$sql_type.="'Contacts' and fieldid=".$contact_id_val;
+			}
+		}
+		else if($type == "Potentials")
+		{
+			if($potential_id_val!="" && $potential_id_val!=0)
+                        {
+				$flag="true";
+				$vtlog->logthis("Getting the  Potentials custom field column name  ",'info');
+                                $sql_type.="'Potentials' and fieldid=".$potential_id_val;
+                        }
 
+		}
+		if($flag=="true")
+		{ 
+			$type_result=$adb->query($sql_type);
+		
+			if(isset($type_insert_column))
+				$type_insert_column.=",";
+                	$type_insert_column.=$adb->query_result($type_result,0,"columnname") ;
+
+			if(isset($insert_value))
+				$insert_value.=",";
+			
 			$insert_value.="'".$adb->query_result($lead_val_result,0,$lead_column_name)."'";
-			//echo ">>>>>>>>>>>ACCOUNT column name $type_insert_column >>>>>>>>>";
 		}
 
 	}
+	$vtlog->logthis("columns to be inserted are ".$type_insert_column,'debug');	
+	$vtlog->logthis("columns to be inserted are ".$insert_value,'debug');	
 	$values = array ($type_insert_column,$insert_value);
-	return $values;
+	return $values;	
 }
 //function Ends
-
-
-
-
 
 //$sql_crmentity = "insert into crmentity(crmid,smcreatorid,smownerid,setype,presence,createdtime,modifiedtime,deleted) values(".$crmid.",".$current_user_id.",".$current_user_id.",'Accounts',1,".$date_entered.",".$date_modified.",0)";
 $sql_crmentity = "insert into crmentity(crmid,smcreatorid,smownerid,setype,presence,createdtime,modifiedtime,deleted,description) values(".$crmid.",".$current_user_id.",".$assigned_user_id.",'Accounts',1,".$date_entered.",".$date_modified.",0,'".$row['description']."')";
@@ -141,10 +154,10 @@ $sql_insert_accountshipads = "INSERT INTO accountshipads (accountaddressid,city,
 
 
  $adb->query($sql_insert_accountshipads);
-	
+
 //Getting the custom field values from leads and inserting into Accounts if the field is mapped
 $insert_value=$crmid;
-$insert_column="accountid";
+$insert_column="accountid";	
 $val= getInsertValues("Accounts",$insert_value);
 $insert_column.=",".$val[0];
 $insert_value.=",".$val[1];
@@ -153,8 +166,6 @@ $adb->query($sql_insert_accountcustomfield);
 
 //
 
-//$sql_insert_accountcustomfield = "INSERT INTO accountscf (accountid) VALUES (".$crmid.")";
-//$adb->query($sql_insert_accountcustomfield);
 
  $date_entered = date('YmdHis');
  $date_modified = date('YmdHis');
@@ -182,7 +193,7 @@ $adb->query($sql_insert_contactsubdetails);
 $adb->query($sql_insert_contactaddress);
 
 
-//Getting the customfield values from leads and inserting into the respective ContactCustomfield to which it is mapped
+//Getting the customfield values from leads and inserting into the respected ContactCustomfield to which it is mapped
 $insert_column="contactid";
 $insert_value=$contact_id;
 $val= getInsertValues("Contacts",$contact_id);
@@ -192,11 +203,6 @@ $sql_insert_contactcustomfield = "INSERT INTO contactscf (".$insert_column.") VA
 
 $adb->query($sql_insert_contactcustomfield);
 //
-
-
- //$sql_insert_contactcustomfield = "INSERT INTO contactscf (contactid) VALUES (".$contact_id.")";
-//$adb->query($sql_insert_contactcustomfield);
-
 
 
 if(! isset($createpotential) || ! $createpotential == "on")
@@ -216,7 +222,7 @@ if(! isset($createpotential) || ! $createpotential == "on")
 
 	$adb->query($sql_insert_opp);
 
-//Getting the customfield values from leads and inserting into the respective PotentialCustomfield to which it is mapped
+//Getting the customfield values from leads and inserting into the respected PotentialCustomfield to which it is mapped
 	$insert_column="potentialid";
 	$insert_value=$oppid;
 	$val= getInsertValues("Potentials",$oppid);
@@ -225,12 +231,9 @@ if(! isset($createpotential) || ! $createpotential == "on")
 	$insert_value.=",".$val[1];
 
 	$sql_insert_potentialcustomfield = "INSERT INTO potentialscf (".$insert_column.") VALUES (".$insert_value.")";
-	//
+//
 
-
- //    	$sql_insert_potentialcustomfield = "INSERT INTO potentialscf (potentialid) VALUES (".$oppid.")";
- //$adb->query($sql_insert_potentialcustomfield);
-
+	$adb->query($sql_insert_potentialcustomfield);
         $sql_insert2contpotentialrel ="insert into contpotentialrel values(".$contact_id.",".$oppid .")";
         
         $adb->query($sql_insert2contpotentialrel);
