ec4815f112df022052e9a6bbac1f7020704a07d8
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-14 05:58:48 +0000

    * Class is changed for displaying the lead- Jaguar

--- modules/Settings/LeadCustomFieldMapping.php
+++ modules/Settings/LeadCustomFieldMapping.php
@@ -120,7 +120,7 @@ for($i=0; $i<$noofrows; $i++)
 {
 	$lead_field_id=$adb->query_result($result,$i,"fieldid");
 	$lead_field_label=$adb->query_result($result,$i,"fieldlabel");
-	$display_val.="<tr><td nowrap class='dataLabel' width=\"10%\">".$lead_field_label.":" ;
+	$display_val.="<tr><td nowrap class='customdataLabel' width=\"10%\">".$lead_field_label;
 
 	$convert_sql="select * from convertleadmapping where leadfid=".$lead_field_id;
 	$convert_result = $adb->query($convert_sql);
