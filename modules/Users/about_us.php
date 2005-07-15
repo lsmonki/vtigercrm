e001f30d0af28c8e302cda00fa4c7aa2ccbccc6e
indigoleopard <indigoleopard@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-15 14:23:29 +0000

    added dates for previous releases

--- modules/Users/about_us.php
+++ modules/Users/about_us.php
@@ -10,20 +10,38 @@ global $mod_strings;
 ?>
 <title><?php echo $app_strings['LBL_ABOUTUS_TITLE']?></title>
 <style type="text/css">@import url("<?php echo $theme_path?>style.css");</style>
-<body class="vtigerbg">
-<table width="100%" border="0">
+<body>
+<table width="100%" border="0" style="border:1px solid #CCC;">
 <tr>
 <td><img src="<?php echo $image_path?>vtiger-crm.gif" alt="vigercrm" border="0"></td>
 <td>&nbsp;</td>
 </tr>
+</table><br>
+<table width="100%" style="border:1px solid #CCC;">
 <tr>
-<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>Version:&nbsp;</b></font></td>
+<td class="dataLabel" height="30 px" width="40%"><font face="Helvetica, sans-serif"><b>Version:&nbsp;</b></font></td>
 <td class="dataField" height="30 px">&nbsp;<?php echo $vtiger_version; ?></td>
 </tr>
 <tr>
-<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>Release Date:&nbsp;</b></td>
-<td class="dataField" height="30 px">&nbsp;<?php echo $release_date; ?></font></td>
-</tr>
+<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>4.2 Release Date:&nbsp;</b></td>
+<td class="dataField" height="30 px">&nbsp;<?php echo $release_date; ?><span class="gensmall"> (Currrent version)</span></td>
+	</tr>
+
+<tr>
+<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>4.0.1 Release Date:&nbsp;</b></td>
+<td class="dataField" height="30 px">&nbsp;April 29, 2005</font></td>
+                </tr>
+		
+	
+<tr>
+<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>4.0 Release Date:&nbsp;</b></td>
+<td class="dataField" height="30 px">&nbsp;04-29-2005</font></td>
+	        </tr>
+		<tr>
+		<td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>3.2 Release Date:&nbsp;</b></td>
+		<td class="dataField" height="30 px">&nbsp;12-13-2004</font></td>
+			        </tr>
+
 <tr>
 <td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>Toll Free Number:&nbsp;</b></font></td>
 <td class="dataField" height="30 px">&nbsp;+1-888-720-9500</td>
@@ -32,7 +50,7 @@ global $mod_strings;
 <td class="dataLabel" height="30 px"><font face="Helvetica, sans-serif"><b>vtiger Discussions:&nbsp;</a></b></font></td>
 <td class="dataField" height="30 px">&nbsp;<a href="http://www.vtiger.com/discussions/index.php?c=3" target=_blank><?php echo $app_strings['LBL_DISCUSS']?></a></td>
 </tr>  	
-</table>
+</table><br>
 <table cellpadding="5" width="100%" cellspacing="0" style="border: 1px solid #888888">
 <tr>
 <td ><p align="justify"><font face="Helvetica, sans-serif">vtiger offers an Open Source CRM solution for managing your organization's sales force automation, inventory management, and customer support & service requirements.<br><br>vtiger also provides Open Source business productivity enhancement add-ons, such as Outlook Plug-in, Office Plug-in, and Thunderbird extension that can be used with vtiger CRM.</font>
