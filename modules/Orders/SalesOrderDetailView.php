3aef4b019236c9190ee66e948324520889494a7a
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-07-16 07:12:13 +0000

    * Modified for i18n

--- modules/Orders/SalesOrderDetailView.php
+++ modules/Orders/SalesOrderDetailView.php
@@ -119,7 +119,7 @@ if(isPermitted("SalesOrder",1,$_REQUEST['record']) == 'yes')
 	$xtpl->assign("DUPLICATEBUTTON","<td><input title=\"$app_strings[LBL_DUPLICATE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DUPLICATE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Orders'; this.form.return_action.value='SalesOrderDetailView'; this.form.module.value='Orders';this.form.isDuplicate.value='true'; this.form.action.value='SalesOrderEditView'\" type=\"submit\" name=\"Duplicate\" value=\"$app_strings[LBL_DUPLICATE_BUTTON_LABEL]\"></td>");
 }
 
-	$xtpl->assign("CREATEPDF","<td><input title=\"Export To PDF\" accessKey=\"Alt+e\" class=\"button\" onclick=\"this.form.return_module.value='Orders'; this.form.return_action.value='SalesOrderDetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.module.value='Orders';this.form.action.value='CreateSOPDF'\" type=\"submit\" name=\"Export To PDF\" value=\"Export To PDF\"></td>");
+	$xtpl->assign("CREATEPDF","<td><input title=\"Export To PDF\" accessKey=\"Alt+e\" class=\"button\" onclick=\"this.form.return_module.value='Orders'; this.form.return_action.value='SalesOrderDetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.module.value='Orders';this.form.action.value='CreateSOPDF'\" type=\"submit\" name=\"$app_strings[LBL_EXPORT_TO_PDF]\" value=\"Export To PDF\"></td>");
 
 
 if(isPermitted("Invoice",1,$_REQUEST['record']) == 'yes')
