a769dfdd925fb6bfcda25d7e71fdae6b7a9f0302
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-09-13 07:02:30 +0000

    * Added mail server username and password

--- SendReminder.php
+++ SendReminder.php
@@ -144,6 +144,8 @@ function send_mail($to,$from,$subject,$contents,$mail_server,$mail_server_userna
 	{
 		$mailserverresult=$adb->query("select * from systems where server_type='email'");
 		$mail_server=$adb->query_result($mailserverresult,0,'server');
+		$mail_server_username=$adb->query_result($mailserverresult,0,'server_username');
+		$mail_server_password=$adb->query_result($mailserverresult,0,'server_password');
 		$_REQUEST['server']=$mail_server;
 	}	
 
