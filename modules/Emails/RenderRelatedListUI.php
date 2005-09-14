112f388720beb73c2bb97d3705ea3fe21f92eecd
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-09-14 18:49:02 +0000

    * changes made for scrolling in email

--- modules/Emails/RenderRelatedListUI.php
+++ modules/Emails/RenderRelatedListUI.php
@@ -104,6 +104,12 @@ function renderRelatedUsers($query)
 
   $list .= '<table border="0" cellpadding="0" cellspacing="0" class="formHeaderULine" width="100%">';
   $list .= '<tr height=1><td height=1></td></tr></table>';
+ 
+  $noofrows = $adb->num_rows($result);
+  if ($noofrows > 15)
+  {
+	$list .= '<div style="overflow:auto;height:315px;width:100%;">';
+  }
 
   $list .= '<table border="0" cellpadding="0" cellspacing="0" class="FormBorder" width="100%">';
   $list .= '<tr class="ModuleListTitle" height=20>';
@@ -197,6 +203,11 @@ function renderRelatedUsers($query)
 
   $list .= '<tr><td COLSPAN="10" class="blackLine"><IMG SRC="themes/'.$theme.'/images/blank.gif"></td></tr>';
   $list .= '</table>';
+  if ($noofrows > 15)
+  {
+	  $list .='</div>';
+  }
+
   echo $list;
 
   echo "<BR>\n";
