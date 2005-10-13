8806e09eea6e113a2e5bd10ee2af0abd51d842c6
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-10-13 12:05:00 +0000

    *** empty log message ***

--- modules/Accounts/Forms.php
+++ modules/Accounts/Forms.php
@@ -23,6 +23,10 @@
  * All Rights Reserved.
  * Contributor(s): ______________________________________..
  */
+
+require_once('include/utils.php'); //new
+
+
 function get_validate_record_js () {
 global $mod_strings;
 global $app_strings;
@@ -181,6 +185,7 @@ function get_new_record_form () {
 global $mod_strings;
 global $app_strings;
 global $current_user;
+global $adb;//for dynamic quickcreateform construction
 
 $lbl_required_symbol = $app_strings['LBL_REQUIRED_SYMBOL'];
 $lbl_account_name = $mod_strings['LBL_ACCOUNT_NAME'];
@@ -191,29 +196,106 @@ $lbl_save_button_key = $app_strings['LBL_SAVE_BUTTON_KEY'];
 $lbl_save_button_label = $app_strings['LBL_SAVE_BUTTON_LABEL'];
 $user_id = $current_user->id;
 
-$the_form = get_left_form_header($mod_strings['LBL_NEW_FORM_TITLE']);
-$the_form .= <<<EOQ
-		<form name="ContactSave" onSubmit="return verify_data(ContactSave)" method="POST" action="index.php">
-			<input type="hidden" name="module" value="Accounts">
-			<input type="hidden" name="record" value="">
-			<input type="hidden" name="email1" value="">
-			<input type="hidden" name="email2" value="">
-			<input type="hidden" name="assigned_user_id" value='${user_id}'>
-			<input type="hidden" name="action" value="Save">
-		<FONT class="required">$lbl_required_symbol</FONT>$lbl_account_name<br>
-		<input name='accountname' type="text" value=""><br>
-		$lbl_phone<br>
-		<input name='phone' type="text" value=""><br>
-		$lbl_website<br>
-		http://<input name='website' type="text" value=""><br>
-		<input title="$lbl_save_button_title" accessKey="$lbl_save_button_key" class="button" type="submit" name="button" value="  $lbl_save_button_label  " >
-		</form>
+$qcreate_form = get_left_form_header($mod_strings['LBL_NEW_FORM_TITLE']);
 
-EOQ;
-$the_form .= get_left_form_footer();
-$the_form .= get_validate_record_js();
 
-return $the_form;
+$qcreate_get_field="select * from field where tabid=6 and quickcreate=0 order by quickcreatesequence";
+$qcreate_get_result=$adb->query($qcreate_get_field);
+$qcreate_get_noofrows=$adb->num_rows($qcreate_get_result);
+
+$fieldName_array = Array();//for validation 
+
+
+$qcreate_form.='<form name="ContactSave" onSubmit="return formValidate()" method="POST" action="index.php">';
+$qcreate_form.='<input type="hidden" name="module" value="Accounts">';
+$qcreate_form.='<input type="hidden" name="record" value="">';
+$qcreate_form.='<input type="hidden" name="email1" value="">';
+$qcreate_form.='<input type="hidden" name="email2" value="">';
+$qcreate_form.='<input type="hidden" name="assigned_user_id" value="'.$user_id.'">';
+$qcreate_form.='<input type="hidden" name="action" value="Save">';
+
+$qcreate_form.='<table>';
+
+for($j=0;$j<$qcreate_get_noofrows;$j++)
+{
+	$qcreate_form.='<tr>';
+	$fieldlabel=$adb->query_result($qcreate_get_result,$j,'fieldlabel');
+	$uitype=$adb->query_result($qcreate_get_result,$j,'uitype');
+	$tabid=$adb->query_result($qcreate_get_result,$j,'tabid');
+	
+	$fieldname=$adb->query_result($qcreate_get_result,$j,'fieldname');//for validation
+	$typeofdata=$adb->query_result($qcreate_get_result,$j,'typeofdata');//for validation
+       	$qcreate_form .= get_quickcreate_form($fieldlabel,$uitype,$fieldname,$tabid);
+
+	
+	//to get validationdata
+	//start
+	$fldLabel_array = Array();
+        $fldLabel_array[$fieldlabel] = $typeofdata;
+        $fieldName_array['QCK_'.$fieldname] = $fldLabel_array;
+	
+	//end
+	
+	$qcreate_form.='</tr>';
+}
+
+//for validation
+$validationData = $fieldName_array;
+$fieldName = '';
+$fieldLabel = '';
+$fldDataType = '';
+
+$rows = count($validationData);
+foreach($validationData as $fldName => $fldLabel_array)
+{
+   if($fieldName == '')
+   {
+     $fieldName="'".$fldName."'";
+   }
+   else
+   {
+     $fieldName .= ",'".$fldName ."'";
+   }
+   foreach($fldLabel_array as $fldLabel => $datatype)
+   {
+	if($fieldLabel == '')
+	{
+			
+     		$fieldLabel = "'".$fldLabel ."'";
+	}		
+        else
+        {
+       		$fieldLabel .= ",'".$fldLabel ."'";
+        }
+ 	if($fldDataType == '')
+        {
+      		$fldDataType = "'".$datatype ."'";
+    	}
+	else
+        {
+       		$fldDataType .= ",'".$datatype ."'";
+     	}
+   }
+ }
+
+
+$qcreate_form.='</table>';
+
+$qcreate_form.='<input title="'.$lbl_save_button_title.'" accessKey="'.$lbl_save_button_key.'" class="button" type="submit" name="button" value="'.$lbl_save_button_label.'" >';
+$qcreate_form.='</form>';
+$qcreate_form.='<script type="text/javascript">
+		
+	var fieldname = new Array('.$fieldName.')
+	var fieldlabel = new Array('.$fieldLabel.')
+	var fielddatatype = new Array('.$fldDataType.')
+
+		</script>';
+
+$qcreate_form .= get_left_form_footer();
+
+return $qcreate_form;
+
+
 }
 
 ?>
