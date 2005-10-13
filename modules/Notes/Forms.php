c2be556a4e33e245f6e654d085a8c0e0bdb548d3
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-10-13 10:09:41 +0000

    changes made to construct the quickcreate form dynamically

--- modules/Notes/Forms.php
+++ modules/Notes/Forms.php
@@ -26,6 +26,9 @@
  * All Rights Reserved.
  * Contributor(s): ______________________________________..
  */
+
+require_once('include/utils.php'); //new
+
 function get_validate_record_js () {
 global $mod_strings;
 global $app_strings;
@@ -81,6 +84,8 @@ function get_new_record_form () {
 global $mod_strings;
 global $app_strings;
 global $app_list_strings;
+global $adb;//for dynamic quickcreateform construction
+
 
 $lbl_required_symbol = $app_strings['LBL_REQUIRED_SYMBOL'];
 $lbl_note_subject = $mod_strings['LBL_NOTE_SUBJECT'];
@@ -89,26 +94,99 @@ $lbl_save_button_key = $app_strings['LBL_SAVE_BUTTON_KEY'];
 $lbl_save_button_label = $app_strings['LBL_SAVE_BUTTON_LABEL'];
 $default_parent_type= $app_list_strings['record_type_default_key'];
 
-$the_form = get_left_form_header($mod_strings['LBL_NEW_FORM_TITLE']);
-$the_form .= <<<EOQ
 
-		<form name="NoteSave" onSubmit="return verify_data(NoteSave)" method="POST" action="index.php">
-			<input type="hidden" name="module" value="Notes">
-			<input type="hidden" name="record" value="">
-			<input type="hidden" name="parent_type" value="${default_parent_type}">
-			<input type="hidden" name="action" value="Save">
-		<FONT class="required">$lbl_required_symbol</FONT>$lbl_note_subject<br>
-		<input name='title' maxlength='255' type="text" value=""><br><br>
-		<input title="$lbl_save_button_title" accessKey="$lbl_save_button_key" class="button" type="submit" name="button" value="  $lbl_save_button_label  " >
-		</form>
+$qcreate_form = get_left_form_header($mod_strings['LBL_NEW_FORM_TITLE']);
 
-EOQ;
+$qcreate_get_field="select * from field where tabid=8 and quickcreate=0 order by quickcreatesequence";
+$qcreate_get_result=$adb->query($qcreate_get_field);
+$qcreate_get_noofrows=$adb->num_rows($qcreate_get_result);
+$fieldName_array = Array();//for validation 
 
-$the_form .= get_left_form_footer();
-$the_form .= get_validate_record_js();
+$qcreate_form.='<form name="NoteSave" onSubmit="return formValidate()" method="POST" action="index.php">';
+$qcreate_form.='<input type="hidden" name="module" value="Notes">';
+$qcreate_form.='<input type="hidden" name="record" value="">';
+$qcreate_form.='<input type="hidden" name="parent_type" value="'.$default_parent_type.'">';
+$qcreate_form.='<input type="hidden" name="action" value="Save">';			
+$qcreate_form.='<table>';
 
+for($j=0;$j<$qcreate_get_noofrows;$j++)
+{
+	$qcreate_form.='<tr>';
+	$fieldlabel=$adb->query_result($qcreate_get_result,$j,'fieldlabel');
+	$uitype=$adb->query_result($qcreate_get_result,$j,'uitype');
+	$tabid=$adb->query_result($qcreate_get_result,$j,'tabid');
+	
+	$fieldname=$adb->query_result($qcreate_get_result,$j,'fieldname');//for validation
+	$typeofdata=$adb->query_result($qcreate_get_result,$j,'typeofdata');//for validation
+       	$qcreate_form .= get_quickcreate_form($fieldlabel,$uitype,$fieldname,$tabid);
 
-return $the_form;
+	
+	//to get validationdata
+	//start
+	$fldLabel_array = Array();
+        $fldLabel_array[$fieldlabel] = $typeofdata;
+        $fieldName_array['QCK_'.$fieldname] = $fldLabel_array;
+	
+	//end
+	
+	$qcreate_form.='</tr>';
+	
+}
+
+//for validation
+$validationData = $fieldName_array;
+$fieldName = '';
+$fieldLabel = '';
+$fldDataType = '';
+
+$rows = count($validationData);
+foreach($validationData as $fldName => $fldLabel_array)
+{
+   if($fieldName == '')
+   {
+     $fieldName="'".$fldName."'";
+   }
+   else
+   {
+     $fieldName .= ",'".$fldName ."'";
+   }
+   foreach($fldLabel_array as $fldLabel => $datatype)
+   {
+	if($fieldLabel == '')
+	{
+			
+     		$fieldLabel = "'".$fldLabel ."'";
+	}		
+        else
+        {
+       		$fieldLabel .= ",'".$fldLabel ."'";
+        }
+ 	if($fldDataType == '')
+        {
+      		$fldDataType = "'".$datatype ."'";
+    	}
+	else
+        {
+       		$fldDataType .= ",'".$datatype ."'";
+     	}
+   }
+ }
+ 
+$qcreate_form.='</table>';
+
+$qcreate_form.='<input title="'.$lbl_save_button_title.'" accessKey="'.$lbl_save_button_key.'" class="button" type="submit" name="button" value="'.$lbl_save_button_label.'" >';
+$qcreate_form.='</form>';
+$qcreate_form.='<script type="text/javascript">
+		
+	var fieldname = new Array('.$fieldName.')
+	var fieldlabel = new Array('.$fieldLabel.')
+	var fielddatatype = new Array('.$fldDataType.')
+
+		</script>';
+
+$qcreate_form .= get_left_form_footer();
+return $qcreate_form;
+
 }
 
 ?>
