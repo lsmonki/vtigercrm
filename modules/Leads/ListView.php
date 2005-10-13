c5bed8b16fca8dec8616d03fbcb6933dbbcc7501
cooljaguar <cooljaguar@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-10-13 09:28:16 +0000

    Page Navigation by Raju has been integrated

--- modules/Leads/ListView.php
+++ modules/Leads/ListView.php
@@ -421,6 +421,7 @@ else
 //Retreive the Navigation array
 $navigation_array = getNavigationValues($start, $noofrows, $list_max_entries_per_page);
 
+/*
 // Setting the record count string
 if ($navigation_array['start'] == 1)
 {
@@ -451,6 +452,14 @@ else
 		$end_rec = $noofrows;
 	}
 }
+*/
+
+// Setting the record count string
+//modified by rdhital
+$start_rec = $navigation_array['start'];
+$end_rec = $navigation_array['end_val']; 
+//By Raju Ends
+
 $record_string= $app_strings[LBL_SHOWING]." " .$start_rec." - ".$end_rec." " .$app_strings[LBL_LIST_OF] ." ".$noofrows;
 
 //Retreive the List View Table Header
