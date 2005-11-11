755a6311e5e65b9102d891bf73f958ad2bcd22ac
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-11-11 07:27:27 +0000

    changes made to fix the bug 'Not Held Meetings shown in Activities instead of History'

--- modules/Leads/Lead.php
+++ modules/Leads/Lead.php
@@ -264,7 +264,7 @@ return $exists;
 	function get_activities($id)
 	{
           // First, get the list of IDs.
-	    $query = "SELECT contactdetails.lastname, contactdetails.firstname, contactdetails.contactid, activity.*,seactivityrel.*,crmentity.crmid, crmentity.smownerid, crmentity.modifiedtime, users.user_name,recurringevents.recurringtype from activity inner join seactivityrel on seactivityrel.activityid=activity.activityid inner join crmentity on crmentity.crmid=activity.activityid left join cntactivityrel on cntactivityrel.activityid= activity.activityid left join contactdetails on contactdetails.contactid = cntactivityrel.contactid left join users on users.id=crmentity.smownerid left outer join recurringevents on recurringevents.activityid=activity.activityid where seactivityrel.crmid=".$id." and (activitytype='Task' or activitytype='Call' or activitytype='Meeting') AND ( activity.status is NULL || activity.status != 'Completed' ) and (  activity.eventstatus is NULL ||  activity.eventstatus != 'Held')";
+	    $query = "SELECT contactdetails.lastname, contactdetails.firstname, contactdetails.contactid, activity.*,seactivityrel.*,crmentity.crmid, crmentity.smownerid, crmentity.modifiedtime, users.user_name,recurringevents.recurringtype from activity inner join seactivityrel on seactivityrel.activityid=activity.activityid inner join crmentity on crmentity.crmid=activity.activityid left join cntactivityrel on cntactivityrel.activityid= activity.activityid left join contactdetails on contactdetails.contactid = cntactivityrel.contactid left join users on users.id=crmentity.smownerid left outer join recurringevents on recurringevents.activityid=activity.activityid where seactivityrel.crmid=".$id." and (activitytype='Task' or activitytype='Call' or activitytype='Meeting') and (activity.status is not NULL && activity.status != 'Completed') and (activity.status is not NULL && activity.status != 'Deferred') or (activity.eventstatus !='' && activity.eventstatus = 'Planned')";
           //include('modules/Leads/RenderRelatedListUI.php');
           renderRelatedTasks($query,$id);
         }
@@ -323,7 +323,7 @@ return $exists;
 			left join activitygrouprelation on activitygrouprelation.activityid=activity.activityid 
 			left join users on crmentity.smownerid= users.id
 		where (activity.activitytype = 'Meeting' or activity.activitytype='Call' or activity.activitytype='Task')
-			and (activity.status='Completed' or activity.eventstatus='Held')
+			and (activity.status = 'Completed' or activity.status = 'Deferred' or (activity.eventstatus != 'Planned' and activity.eventstatus != ''))
 			and seactivityrel.crmid=".$id."
 		order by createdtime desc";
 		renderRelatedHistory($query,$id);
