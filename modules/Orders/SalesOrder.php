d9ac237abbe2ee71bdc0e7ef287e3e282cdba3d4
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-11-11 06:55:45 +0000

    changes made to fix the bug 'Not Held Meetings shown in Activities instead of History'

--- modules/Orders/SalesOrder.php
+++ modules/Orders/SalesOrder.php
@@ -172,7 +172,7 @@ class SalesOrder extends CRMEntity {
 	}
 	function get_activities($id)
 	{
-		$query = "SELECT contactdetails.lastname, contactdetails.firstname, contactdetails.contactid, activity.*,seactivityrel.*,crmentity.crmid, crmentity.smownerid, crmentity.modifiedtime, users.user_name from activity inner join seactivityrel on seactivityrel.activityid=activity.activityid inner join crmentity on crmentity.crmid=activity.activityid left join cntactivityrel on cntactivityrel.activityid= activity.activityid left join contactdetails on contactdetails.contactid = cntactivityrel.contactid left join users on users.id=crmentity.smownerid where seactivityrel.crmid=".$id." and (activitytype='Task' or activitytype='Call' or activitytype='Meeting') and crmentity.deleted=0 and ( activity.status is NULL || activity.status != 'Completed' ) and (  activity.eventstatus is NULL ||  activity.eventstatus != 'Held')";
+		$query = "SELECT contactdetails.lastname, contactdetails.firstname, contactdetails.contactid, activity.*,seactivityrel.*,crmentity.crmid, crmentity.smownerid, crmentity.modifiedtime, users.user_name from activity inner join seactivityrel on seactivityrel.activityid=activity.activityid inner join crmentity on crmentity.crmid=activity.activityid left join cntactivityrel on cntactivityrel.activityid= activity.activityid left join contactdetails on contactdetails.contactid = cntactivityrel.contactid left join users on users.id=crmentity.smownerid where seactivityrel.crmid=".$id." and (activitytype='Task' or activitytype='Call' or activitytype='Meeting') and crmentity.deleted=0 and (activity.status is not NULL && activity.status != 'Completed') and (activity.status is not NULL && activity.status !='Deferred') or (activity.eventstatus != '' &&  activity.eventstatus = 'Planned')";
 		renderSalesRelatedActivities($query,$id);
 	}
 	function get_history($id)
@@ -191,7 +191,7 @@ class SalesOrder extends CRMEntity {
 			left join contactdetails on contactdetails.contactid = cntactivityrel.contactid
 			inner join users on crmentity.smcreatorid=users.id
 		where (activitytype='Task' or activitytype='Call' or activitytype='Meeting')
-			and (activity.status='Completed' or activity.eventstatus='Held')
+			and (activity.status = 'Completed' or activity.status = 'Deferred' or (activity.eventstatus !='Planned' and activity.eventstatus != ''))
 			and seactivityrel.crmid=".$id."
 		order by createdtime desc";
 		renderRelatedHistory($query,$id);
