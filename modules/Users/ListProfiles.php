7db3655808c84cb26c095bd4ae4cb8209a4060a7
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-11-14 09:11:08 +0000

    Profile Link Modified

--- modules/Users/ListProfiles.php
+++ modules/Users/ListProfiles.php
@@ -69,16 +69,17 @@ function getStdOutput($profileListResult, $noofrows, $mod_strings)
 		$standCustFld .= '<tr class="'.$trowclass.'">';
 		$profile_name = $adb->query_result($profileListResult,$i,"profilename");
 		$profile_id = $adb->query_result($profileListResult,$i,"profileid");
-		$standCustFld .= '<td width="18%" height="21" style="padding:0px 3px 0px 3px;"><div align="center"><a href="index.php?module=Users&action=ProfileEditView&profileid='.$profile_id.'">edit</a>';
+		$standCustFld .= '<td width="18%" height="21" style="padding:0px 3px 0px 3px;"><div align="center">';
+		//<a href="index.php?module=Users&action=profilePrivileges&profileid='.$profile_id.'">edit</a>
 		global $current_user;
                 $current_profile = fetchUserProfileId($current_user->id);
                 if($profile_id != 1 && $profile_id != 2 && $profile_id != 3 && $profile_id != 4 && $profile_id != $current_profile)
                 {
 
-			$standCustFld .= ' | <a href="index.php?module=Users&action=ProfileDeleteStep1&profileid='.$profile_id.'">del</a>';	
+			$standCustFld .= '<a href="index.php?module=Users&action=ProfileDeleteStep1&profileid='.$profile_id.'">del</a>';	
 		}
 		$standCustFld .= '</div></td>';
-		$standCustFld .= '<td wheight="21" style="padding:0px 3px 0px 3px;"><a href="index.php?module=Users&action=ProfileDetailView&profileid='.$profile_id.'">'.$profile_name.'</a></td></tr>';
+		$standCustFld .= '<td wheight="21" style="padding:0px 3px 0px 3px;"><a href="index.php?module=Users&action=profilePrivileges&profileid='.$profile_id.'">'.$profile_name.'</a></td></tr>';
 		
 	}
 	$standCustFld .='</table>';
