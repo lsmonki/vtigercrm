cf7370f98efafc12076fb5c0d77c116b3b47cae6
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-11-17 11:03:00 +0000

    * Modified to send a test mail to the current user when configure the mail server

--- modules/Settings/Save.php
+++ modules/Settings/Save.php
@@ -19,6 +19,7 @@ $server_type = $_REQUEST['server_type'];
 $sql="select * from systems where server_type = '".$server_type."'";
 $id=$adb->query_result($adb->query($sql),0,"id");
 
+
 if($id=='')
 {
 	$id = $adb->getUniqueID("systems");
@@ -29,5 +30,27 @@ else
 
 $adb->query($sql);
 
-header("Location: index.php?module=Settings&action=index");
+$action = 'index';
+//Added code to send a test mail to the currently logged in user
+require_once("modules/Emails/mail.php");
+global $current_user;
+
+$to_email = getUserEmailId('id',$current_user->id);
+$from_email = $to_email;
+$subject = 'Test mail about the mail server configuration.';
+$description = 'Dear '.$current_user->user_name.', <br><br> This is the test mail which is sent during the outgoing mail server configuration process. <br><br>Thanks <br>'.$current_user->user_name;
+if($to_email != '')
+{
+	$mail_status = send_mail('Users',$to_email,$current_user->user_name,$from_email,$subject,$description);
+	$mail_status_str = $to_email."=".$mail_status."&&&";
+}
+else
+{
+	$mail_status_str = "'".$to_email."'=0&&&";
+}
+$mail_error_str = getMailErrorString($mail_status_str);
+if($mail_status != 1)
+	$action = 'EmailConfig';
+
+header("Location: index.php?module=Settings&action=$action&$mail_error_str");
 ?>
