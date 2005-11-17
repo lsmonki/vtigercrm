fd0126a02baf2cbc1225f6e5e8cf0480ca231840
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-11-17 13:15:46 +0000

    changes made to fix 'No Type field in Custom View'

--- modules/CustomView/language/en_us.lang.php
+++ modules/CustomView/language/en_us.lang.php
@@ -34,6 +34,7 @@ $mod_strings = Array(
 'LBL_SF_ENDDATE'=>'End Date',
 'LBL_AF_HDR1'=>'Set the search conditions to further restrict the list.',
 'LBL_AF_HDR2'=>'You can use &quot;or&quot; filters by entering multiple items in the third column.',
-'LBL_AF_HDR3'=>'You can enter up to 10 items, separated by commas. For example: CA, NY, TX, FL searches for CA or NY or TX or FL.',);
+'LBL_AF_HDR3'=>'You can enter up to 10 items, separated by commas. For example: CA, NY, TX, FL searches for CA or NY or TX or FL.',
+'LBL_AF_HDR4'=>'If "Activity Type" is selected, give any one of the following values "Call","Meeting" or "Task".');
 
 ?>
