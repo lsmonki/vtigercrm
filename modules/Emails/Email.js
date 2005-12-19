41972886b018b0cc3186bc9ee31cc420efd8287c
jerrydgeorge <jerrydgeorge@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-19 07:17:07 +0000

    * javascript functions are moved to Email.js file

---
+++ modules/Emails/Email.js
@@ -0,0 +1,74 @@
+/*********************************************************************************
+
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ********************************************************************************/
+
+
+function showDefaultCustomView(selectView)
+{
+	viewName = selectView.options[selectView.options.selectedIndex].value;
+	document.massdelete.viewname.value=viewName;
+	document.massdelete.action="index.php?module=Emails&action=index&return_module=Emails&return_action=index&viewname="+viewName;
+	document.massdelete.submit();
+}
+function massDelete()
+{
+
+        x = document.massdelete.selected_id.length;
+	var viewid = document.massdelete.viewname.value;
+
+        idstring = "";
+
+        if ( x == undefined)
+        {
+
+                if (document.massdelete.selected_id.checked)
+                {
+                        document.massdelete.idlist.value=document.massdelete.selected_id.value;
+                }
+                else
+                {
+                        alert("Please select atleast one entity");
+                        return false;
+                }
+        }
+        else
+        {
+                xx = 0;
+                for(i = 0; i < x ; i++)
+                {
+                        if(document.massdelete.selected_id[i].checked)
+                        {
+                                idstring = document.massdelete.selected_id[i].value +";"+idstring
+                        xx++
+                        }
+                }
+                if (xx != 0)
+                {
+                        document.massdelete.idlist.value=idstring;
+                }
+                else
+                {
+                        alert("Please select atleast one entity");
+                        return false;
+                }
+        }
+		if(confirm("Are you sure you want to delete the selected "+xx+" records ?"))
+		{	
+        	document.massdelete.action="index.php?module=Users&action=massdelete&return_module=Emails&return_action=ListView&viewname="+viewid;
+		}
+		else
+		{
+			return false;
+		}
+}
+
+
+
+
+
