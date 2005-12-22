176094f221b8a5df1236607a76a7b627686da589
cooljaguar <cooljaguar@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-22 12:37:23 +0000

    Xtemplate coding has been removed and search part has been commented

--- modules/Products/ListView.php
+++ modules/Products/ListView.php
@@ -9,7 +9,6 @@
 *
  ********************************************************************************/
 require_once('include/database/PearDatabase.php');
-//require_once('XTemplate/xtpl.php');
 require_once('Smarty_setup.php');
 require_once('modules/Products/Product.php');
 require_once('include/utils/utils.php');
@@ -34,10 +33,6 @@ require_once($theme_path.'layout_utils.php');
 echo "<br>";
 //echo get_form_header("Product Search", "", false);
 
-//$xtpl=new XTemplate ('modules/Products/ListView.html');
-//$xtpl->assign("MOD", $mod_strings);
-//$xtpl->assign("APP", $app_strings);
-//$xtpl->assign("IMAGE_PATH",$image_path);
 $smarty = new vtigerCRM_Smarty;
 $smarty->assign("MOD", $mod_strings);
 $smarty->assign("APP", $app_strings);
@@ -208,6 +203,7 @@ if(isset($_REQUEST['viewname']) == false)
 //<<<<<customview>>>>>
 
 //Constructing the Search Form
+/*
 if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
         // Stick the form header out there.
 	echo get_form_header($current_module_strings['LBL_SEARCH_FORM_TITLE'],'', false);
@@ -277,6 +273,7 @@ echo get_form_footer();
 //echo '<br><br>';
 
 }
+*/
 
 if($viewid == 0)
 {
@@ -330,7 +327,6 @@ if(isset($where) && $where != '')
         $list_query .= ' and '.$where;
 }
 
-//$xtpl->assign("PRODUCTLISTHEADER", get_form_header($current_module_strings['LBL_LIST_FORM_TITLE'], $other_text, false ));
 $smarty->assign("PRODUCTLISTHEADER", get_form_header($current_module_strings['LBL_LIST_FORM_TITLE'], $other_text, false ));
 
 if(isset($order_by) && $order_by != '')
@@ -420,23 +416,16 @@ if($viewid !='')
 $url_string .= "&viewname=".$viewid;
 
 $listview_header = getListViewHeader($focus,"Products",$url_string,$sorder,$order_by,"",$oCustomView);
-//$xtpl->assign("LISTHEADER", $listview_header);
 $smarty->assign("LISTHEADER", $listview_header);
 
 $listview_entries = getListViewEntries($focus,"Products",$list_result,$navigation_array,"","","EditView","Delete",$oCustomView);
-//$xtpl->assign("LISTENTITY", $listview_entries);
-//$xtpl->assign("SELECT_SCRIPT", $view_script);
 $smarty->assign("LISTENTITY", $listview_entries);
 $smarty->assign("SELECT_SCRIPT", $view_script);
 
 $navigationOutput = getTableHeaderNavigation($navigation_array, $url_string,"Products","index",$viewid);
-//$xtpl->assign("NAVIGATION", $navigationOutput);
-//$xtpl->assign("RECORD_COUNTS", $record_string);
 $smarty->assign("NAVIGATION", $navigationOutput);
 $smarty->assign("RECORD_COUNTS", $record_string);
 
-//$xtpl->parse("main");
-//$xtpl->out("main");
 
 
 $smarty->display("ListView.tpl");
