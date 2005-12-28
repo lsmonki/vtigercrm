6d9c51daa30358646916949ddba62cac0880fdd1
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-28 10:57:53 +0000

    changes made to integrate new UI and Smarty

--- modules/Invoice/DetailView.php
+++ modules/Invoice/DetailView.php
@@ -20,7 +20,8 @@
  * Contributor(s): ______________________________________..
  ********************************************************************************/
 
-require_once('XTemplate/xtpl.php');
+//require_once('XTemplate/xtpl.php');
+require_once('Smarty_setup.php');
 require_once('data/Tracker.php');
 require_once('modules/Invoice/Invoice.php');
 require_once('include/CustomFieldUtil.php');
@@ -29,7 +30,7 @@ require_once('include/utils/utils.php');
 global $mod_strings;
 global $app_strings;
 global $app_list_strings;
-
+global $currentModule;
 $focus = new Invoice();
 //$focus->set_strings();
 //var_dump($focus);
@@ -51,80 +52,40 @@ require_once($theme_path.'layout_utils.php');
 
 $log->info("Order detail view");
 
-$xtpl=new XTemplate ('modules/Invoice/DetailView.html');
-$xtpl->assign("MOD", $mod_strings);
-$xtpl->assign("APP", $app_strings);
 
-$xtpl->assign("THEME", $theme);
-$xtpl->assign("IMAGE_PATH", $image_path);
-$xtpl->assign("PRINT_URL", "phprint.php?jt=".session_id().$GLOBALS['request_string']);
-if (isset($focus->name)) $xtpl->assign("NAME", $focus->name);
-else $xtpl->assign("NAME", "");
-//get Block 1 Information
-$block_1_header = getBlockTableHeader("LBL_INVOICE_INFORMATION");
-$block_1 = getDetailBlockInformation("Invoice",1,$focus->column_fields);
-$xtpl->assign("BLOCK1_HEADER", $block_1_header);
-$xtpl->assign("BLOCK1", $block_1);
+$smarty = new vtigerCRM_Smarty;
+$smarty->assign("MOD", $mod_strings);
+$smarty->assign("APP", $app_strings);
+$smarty->assign("MODULE",$currentModule);
 
-//get Address Information
-$block_2_header = getBlockTableHeader("LBL_ADDRESS_INFORMATION");
-$block_2 = getDetailBlockInformation("Invoice",2,$focus->column_fields);
-$xtpl->assign("BLOCK2_HEADER", $block_2_header);
-$xtpl->assign("BLOCK2", $block_2);
 
-//get Description Information
-$block_3_header = getBlockTableHeader("LBL_DESCRIPTION_INFORMATION");
-$block_3 = getDetailBlockInformation("Invoice",3,$focus->column_fields);
-$xtpl->assign("BLOCK3_HEADER", $block_3_header);
-$xtpl->assign("BLOCK3", $block_3);
+$smarty->assign("THEME", $theme);
+$smarty->assign("IMAGE_PATH", $image_path);
+$smarty->assign("PRINT_URL", "phprint.php?jt=".session_id().$GLOBALS['request_string']);
 
-//$block_4_header = getBlockTableHeader("LBL_RELATED_PRODUCTS");
-$block_4 = getDetailAssociatedProducts('Invoice',$focus);
-//$xtpl->assign("BLOCK4_HEADER", $block_4_header);
-$xtpl->assign("BLOCK4", $block_4);
+if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
+else $smarty->assign("NAME", "");
+$smarty->assign("BLOCKS", getBlocks("Invoice","detail_view",'',$focus->column_fields)); 
 
-$block_6_header = getBlockTableHeader("LBL_TERMS_INFORMATION");
-$block_6 = getDetailBlockInformation("Invoice",6,$focus->column_fields);
-$xtpl->assign("BLOCK6_HEADER", $block_6_header);
-$xtpl->assign("BLOCK6", $block_6);
-
-$block_5 = getDetailBlockInformation("Invoice",5,$focus->column_fields);
-if(trim($block_5) != '')
-{
-        $cust_fld = '<table width="100%" border="0" cellspacing="0" cellpadding="0" class="formOuterBorder">';
-        $cust_fld .=  '<tr><td>';
-	$block_5_header = getBlockTableHeader("LBL_CUSTOM_INFORMATION");
-        $cust_fld .= $block_5_header;
-        $cust_fld .= '<table width="100%" border="0" cellspacing="1" cellpadding="0">';
-        $cust_fld .= $block_5;
-        $cust_fld .= '</table>';
-        $cust_fld .= '</td></tr></table>';
-	$cust_fld .= '<BR>';
-
-}
-
-$xtpl->assign("CUSTOMFIELD", $cust_fld);
-
-$xtpl->assign("ID", $_REQUEST['record']);
-
+$smarty->assign("CUSTOMFIELD", $cust_fld);
+$smarty->assign("ID", $_REQUEST['record']);
+$smarty->assign("SINGLE_MOD", "Invoice");
 
 $permissionData = $_SESSION['action_permission_set'];
 if(isPermitted("Invoice",1,$_REQUEST['record']) == 'yes')
 {
-	$xtpl->assign("EDITBUTTON","<td><input title=\"$app_strings[LBL_EDIT_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_EDIT_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='EditView'\" type=\"submit\" name=\"Edit\" value=\"$app_strings[LBL_EDIT_BUTTON_LABEL]\"></td>");
+	$smarty->assign("EDITBUTTON","<td><input title=\"$app_strings[LBL_EDIT_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_EDIT_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='EditView'\" type=\"submit\" name=\"Edit\" value=\"$app_strings[LBL_EDIT_BUTTON_LABEL]\"></td>");
 
 
-	$xtpl->assign("DUPLICATEBUTTON","<td><input title=\"$app_strings[LBL_DUPLICATE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DUPLICATE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.isDuplicate.value='true'; this.form.action.value='EditView'\" type=\"submit\" name=\"Duplicate\" value=\"$app_strings[LBL_DUPLICATE_BUTTON_LABEL]\"></td>");
+	$smarty->assign("DUPLICATEBUTTON","<td><input title=\"$app_strings[LBL_DUPLICATE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DUPLICATE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.isDuplicate.value='true'; this.form.action.value='EditView'\" type=\"submit\" name=\"Duplicate\" value=\"$app_strings[LBL_DUPLICATE_BUTTON_LABEL]\"></td>");
 }
-	$xtpl->assign("CREATEPDF","<td><input title=\"Export To PDF\" accessKey=\"Alt+e\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='CreatePDF'\" type=\"submit\" name=\"Export To PDF\" value=\"Export To PDF\"></td>");
+	$smarty->assign("CREATEPDF","<td><input title=\"Export To PDF\" accessKey=\"Alt+e\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='CreatePDF'\" type=\"submit\" name=\"Export To PDF\" value=\"Export To PDF\"></td>");
 
 if(isPermitted("Invoice",2,$_REQUEST['record']) == 'yes')
 {
-	$xtpl->assign("DELETEBUTTON","<td><input title=\"$app_strings[LBL_DELETE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DELETE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='index'; this.form.action.value='Delete'; return confirm('$app_strings[NTC_DELETE_CONFIRMATION]')\" type=\"submit\" name=\"Delete\" value=\"$app_strings[LBL_DELETE_BUTTON_LABEL]\"></td>");
+	$smarty->assign("DELETEBUTTON","<td><input title=\"$app_strings[LBL_DELETE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DELETE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Invoice'; this.form.return_action.value='index'; this.form.action.value='Delete'; return confirm('$app_strings[NTC_DELETE_CONFIRMATION]')\" type=\"submit\" name=\"Delete\" value=\"$app_strings[LBL_DELETE_BUTTON_LABEL]\"></td>");
 }
 
-$xtpl->parse("main");
-$xtpl->out("main");
 
 //Security check for related list
 global $profile_id;
@@ -133,6 +94,6 @@ $permissionData = $_SESSION['action_permission_set'];
 getRelatedLists("Invoice",$focus);
 
 echo "<BR>\n";
-
+$smarty->display("DetailView.tpl");
 
 ?>
