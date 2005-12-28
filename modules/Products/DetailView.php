ff6304d85ac35df81bcdecb747e30505ed74885b
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-28 11:17:57 +0000

    changes made to integrate new UI and Smarty.

--- modules/Products/DetailView.php
+++ modules/Products/DetailView.php
@@ -10,7 +10,8 @@
  ********************************************************************************/
 require_once('include/database/PearDatabase.php');
 //require_once('HelpDeskUtil.php');
-require_once('XTemplate/xtpl.php');
+//require_once('XTemplate/xtpl.php');
+require_once('Smarty_setup.php');
 require_once('include/utils/utils.php');
 require_once('modules/Products/Product.php');
 require_once('include/utils/utils.php');
@@ -37,33 +38,40 @@ $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
 require_once($theme_path.'layout_utils.php');
 
-$xtpl=new XTemplate ('modules/Products/DetailView.html');
-$xtpl->assign("MOD", $mod_strings);
-$xtpl->assign("APP", $app_strings);
-if (isset($focus->name)) $xtpl->assign("NAME", $focus->name);
-else $xtpl->assign("NAME", "");
+$smarty = new vtigerCRM_Smarty;
+$smarty->assign("MOD", $mod_strings);
+$smarty->assign("APP", $app_strings);
+
+if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
+else $smarty->assign("NAME", "");
+
+$smarty->assign("BLOCKS", getBlocks("Products","detail_view",'',$focus->column_fields));
+
+/*
 $block_1 = getDetailBlockInformation("Products",1,$focus->column_fields);
-$xtpl->assign("BLOCK1", $block_1);
+$smarty->assign("BLOCK1", $block_1);
 $block_2 = getDetailBlockInformation("Products",2,$focus->column_fields);
-$xtpl->assign("BLOCK2", $block_2);
+$smarty->assign("BLOCK2", $block_2);
 $block_3 = getDetailBlockInformation("Products",3,$focus->column_fields);
-$xtpl->assign("BLOCK3", $block_3);
+$smarty->assign("BLOCK3", $block_3);
 $block_4 = getDetailBlockInformation("Products",4,$focus->column_fields);
-$xtpl->assign("BLOCK4", $block_4);
+$smarty->assign("BLOCK4", $block_4);
 $block_6 = getDetailBlockInformation("Products",6,$focus->column_fields);
-$xtpl->assign("BLOCK6", $block_6);
+$smarty->assign("BLOCK6", $block_6);
+
 $block_1_header = getBlockTableHeader("LBL_PRODUCT_INFORMATION");
 $block_2_header = getBlockTableHeader("LBL_PRICING_INFORMATION");
 $block_3_header = getBlockTableHeader("LBL_STOCK_INFORMATION");
 $block_4_header = getBlockTableHeader("LBL_DESCRIPTION_INFORMATION");
 $block_6_header = getBlockTableHeader("LBL_IMAGE_INFORMATION");
-$xtpl->assign("BLOCK1_HEADER", $block_1_header);
-$xtpl->assign("BLOCK2_HEADER", $block_2_header);
-$xtpl->assign("BLOCK3_HEADER", $block_3_header);
-$xtpl->assign("BLOCK4_HEADER", $block_4_header);
-$xtpl->assign("BLOCK6_HEADER", $block_6_header);
 
+$smarty->assign("BLOCK1_HEADER", $block_1_header);
+$smarty->assign("BLOCK2_HEADER", $block_2_header);
+$smarty->assign("BLOCK3_HEADER", $block_3_header);
+$smarty->assign("BLOCK4_HEADER", $block_4_header);
+$smarty->assign("BLOCK6_HEADER", $block_6_header);
 $block_5 = getDetailBlockInformation("Products",5,$focus->column_fields);
+
 if(trim($block_5) != '')
 {
         $cust_fld = '<table width="100%" border="0" cellspacing="0" cellpadding="0" class="formOuterBorder">';
@@ -77,32 +85,31 @@ if(trim($block_5) != '')
         $cust_fld .= '<BR>';
 
 }
+*/
 
-$xtpl->assign("CUSTOMFIELD", $cust_fld);
-
+$smarty->assign("CUSTOMFIELD", $cust_fld);
+//$smarty->assign("ID", $_REQUEST['record']);
+$smarty->assign("SINGLE_MOD","Products");
 
 $permissionData = $_SESSION['action_permission_set'];
 if(isPermitted("Products",1,$_REQUEST['record']) == 'yes')
 {
-	$xtpl->assign("EDITBUTTON","<td><input title=\"$app_strings[LBL_EDIT_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_EDIT_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='EditView'\" type=\"submit\" name=\"Edit\" value=\"$app_strings[LBL_EDIT_BUTTON_LABEL]\"></td>");
+	$smarty->assign("EDITBUTTON","<input title=\"$app_strings[LBL_EDIT_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_EDIT_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='DetailView'; this.form.return_id.value='".$_REQUEST['record']."'; this.form.action.value='EditView'\" type=\"submit\" name=\"Edit\" value=\"$app_strings[LBL_EDIT_BUTTON_LABEL]\">");
 
 
-	$xtpl->assign("DUPLICATEBUTTON","<td><input title=\"$app_strings[LBL_DUPLICATE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DUPLICATE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='DetailView'; this.form.isDuplicate.value='true'; this.form.action.value='EditView'\" type=\"submit\" name=\"Duplicate\" value=\"$app_strings[LBL_DUPLICATE_BUTTON_LABEL]\"></td>");
+	$smarty->assign("DUPLICATEBUTTON","<input title=\"$app_strings[LBL_DUPLICATE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DUPLICATE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='DetailView'; this.form.isDuplicate.value='true'; this.form.action.value='EditView'\" type=\"submit\" name=\"Duplicate\" value=\"$app_strings[LBL_DUPLICATE_BUTTON_LABEL]\">");
 }
 
 
 if(isPermitted("Products",2,$_REQUEST['record']) == 'yes')
 {
-	$xtpl->assign("DELETEBUTTON","<td><input title=\"$app_strings[LBL_DELETE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DELETE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='index'; this.form.action.value='Delete'; return confirm('$app_strings[NTC_DELETE_CONFIRMATION]')\" type=\"submit\" name=\"Delete\" value=\"$app_strings[LBL_DELETE_BUTTON_LABEL]\"></td>");
+	$smarty->assign("DELETEBUTTON","<input title=\"$app_strings[LBL_DELETE_BUTTON_TITLE]\" accessKey=\"$app_strings[LBL_DELETE_BUTTON_KEY]\" class=\"button\" onclick=\"this.form.return_module.value='Products'; this.form.return_action.value='index'; this.form.action.value='Delete'; return confirm('$app_strings[NTC_DELETE_CONFIRMATION]')\" type=\"submit\" name=\"Delete\" value=\"$app_strings[LBL_DELETE_BUTTON_LABEL]\">");
 }
 
 
-
-$xtpl->assign("IMAGE_PATH", $image_path);
-$xtpl->assign("PRINT_URL", "phprint.php?jt=".session_id().$GLOBALS['request_string']);
-$xtpl->assign("ID", $_REQUEST['record']);
-$xtpl->parse("main");
-$xtpl->out("main");
+$smarty->assign("IMAGE_PATH", $image_path);
+$smarty->assign("PRINT_URL", "phprint.php?jt=".session_id().$GLOBALS['request_string']);
+$smarty->assign("ID", $_REQUEST['record']);
 
 /*
 require_once('modules/Products/binaryfilelist.php');
@@ -141,7 +148,7 @@ echo '</td></tr></form></tbody></table>';
 global $profile_id;
 $tab_per_Data = getAllTabsPermission($profile_id);
 $permissionData = $_SESSION['action_permission_set'];
-getRelatedLists("Products",$focus);
+//getRelatedLists("Products",$focus);
 
 
 /*
@@ -172,5 +179,7 @@ if($tab_per_Data[8] == 0)
 
 
 */
+$smarty->assign("MODULE", $module);
+$smarty->display("DetailView.tpl");
 
 ?>
