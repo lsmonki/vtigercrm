878dca3888b4bffced124317bbe7f0fd637915d8
ask_ahmed <ask_ahmed@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-28 09:58:47 +0000

    Initial Version

---
+++ modules/Activities/CallRelatedList.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once('Smarty_setup.php');
+require_once('modules/Activities/Activity.php');
+
+$focus = new Activity();
+$MODULE = $_REQUEST['module'];
+$RECORD = $_REQUEST['record'];
+
+if (isset($_REQUEST['record']) && isset($_REQUEST['record'])) {
+	$focus->retrieve_entity_info($_REQUEST['record'],"Activities");
+	$focus->id = $_REQUEST['record'];
+	$focus->name=$focus->column_fields['subject'];
+	
+	$log->debug("id is ".$focus->id);
+	$log->debug("name is ".$focus->name);
+}
+
+$smarty = new vtigerCRM_Smarty;
+
+if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
+
+$related_array = getRelatedLists("Activities", $focus);
+$smarty->assign("RELATEDLISTS", $related_array);
+$smarty->assign("ID", $RECORD);
+$smarty->assign("CurrentModule", $MODULE);
+$smarty->display("RelatedLists.tpl");
+
+
+
+
+?>
---
+++ modules/Invoice/CallRelatedList.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once('Smarty_setup.php');
+require_once('modules/Invoice/Invoice.php');
+$focus = new Invoice();
+$currentmodule = $_REQUEST['module'];
+$RECORD = $_REQUEST['record'];
+if(isset($_REQUEST['record']) && isset($_REQUEST['record'])) {
+    $focus->retrieve_entity_info($_REQUEST['record'],"Invoice");
+    $focus->id = $_REQUEST['record'];
+    $focus->name=$focus->column_fields['subject'];
+//$vtlog->logthis("id is ".$focus->id,'debug');
+$log->debug("id is ".$focus->id);
+//$vtlog->logthis("name is ".$focus->name,'debug');
+$log->debug("name is ".$focus->name);
+//$vtlog->logthis("name is ".$focus->name,'debug');
+}
+
+$smarty = new vtigerCRM_Smarty;
+
+
+if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
+$related_array=getRelatedLists("Invoice",$focus);
+$smarty->assign("RELATEDLISTS", $related_array);
+//echo '<pre>';print_r($related_array);echo '</pre>';
+$smarty->assign("ID",$RECORD );
+$smarty->assign("MODULE",$currentmodule);
+$smarty->display("RelatedLists.tpl");
+?>
