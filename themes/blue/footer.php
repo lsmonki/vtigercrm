f6fcf43473dddb69e6f7f8a3a092f66d0690428e
jerrydgeorge <jerrydgeorge@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-28 07:55:03 +0000

    Module Lists in footer has been removed

--- themes/blue/footer.php
+++ themes/blue/footer.php
@@ -23,32 +23,6 @@ global $app_strings;
 <!--end body panes-->
 </td></tr>
 <tr><td colspan="2" align="center">
-	<table CELLSPACING=3 border=0><tr>
-      <td align=center noWrap colSpan=4>
-	  <A href="index.php?module=Home&action=index"><?php echo $app_list_strings['moduleList']['Home']; ?></A> |
-	  <A href="index.php?module=Dashboard&action=index"><?php echo $app_list_strings['moduleList']['Dashboard']; ?></A> |
-	  <A href="index.php?module=Leads&action=index"><?php echo $app_list_strings['moduleList']['Leads']; ?></A> |
-	  <A href="index.php?module=Contacts&action=index"><?php echo $app_list_strings['moduleList']['Contacts']; ?></A> |
-	  <A href="index.php?module=Accounts&action=index"><?php echo $app_list_strings['moduleList']['Accounts']; ?></A> |
-	  <A href="index.php?module=Potentials&action=index"><?php echo $app_list_strings['moduleList']['Potentials']; ?></A> |
-	  <A href="index.php?module=Notes&action=index"><?php echo $app_list_strings['moduleList']['Notes']; ?></A> |
-	  <A href="index.php?module=Emails&action=index"><?php echo $app_list_strings['moduleList']['Emails']; ?></A> |
-	  <A href="index.php?module=Activities&action=index"><?php echo $app_list_strings['moduleList']['Activities']; ?></A> |
-	  <A href="index.php?module=Products&action=index"><?php echo $app_list_strings['moduleList']['Products']; ?></A> |
-	  <A href="index.php?module=HelpDesk&action=index"><?php echo $app_list_strings['moduleList']['HelpDesk']; ?></A> |
-	  <A href="index.php?module=Calendar&action=index"><?php echo $app_list_strings['moduleList']['Calendar']; ?></A>
-	  <br>
-	  <A href="index.php?module=Quotes&action=index"><?php echo $app_list_strings['moduleList']['Quotes']; ?></A> |
-	  <A href="index.php?module=PurchaseOrder&action=index"><?php echo $app_list_strings['moduleList']['PurchaseOrder']; ?></A> |
-          <A href="index.php?module=SalesOrder&action=index"><?php echo $app_list_strings['moduleList']['SalesOrder']; ?></A> |
-
-	  <A href="index.php?module=Invoice&action=index"><?php echo $app_list_strings['moduleList']['Invoice']; ?></A> |
-	  
-          <A href="index.php?module=Rss&action=index"><?php echo $app_list_strings['moduleList']['Rss']; ?></A> |
-	  <A href="index.php?module=Reports&action=index"><?php echo $app_list_strings['moduleList']['Reports']; ?></A> 
-
-	  </td>
-    </tr></table>
 </td></tr></table>
 </body>
 </html>
