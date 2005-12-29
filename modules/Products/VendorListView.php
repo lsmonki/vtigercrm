d1759d3c02398e8a01a27146cbc299f364f88409
ask_ahmed <ask_ahmed@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-29 11:58:10 +0000

    Buttons & Combo box separated in ListView Ui

--- modules/Products/VendorListView.php
+++ modules/Products/VendorListView.php
@@ -32,11 +32,23 @@ require_once($theme_path.'layout_utils.php');
 echo "<br>";
 //echo get_form_header("Product Search", "", false);
 
+if(isset($_REQUEST['category']) && $_REQUEST['category'] !='')
+{
+	$category = $_REQUEST['category'];
+}
+else
+{
+	$category = getParentTabFromModule($currentModule);
+}
+
 $smarty = new vtigerCRM_Smarty;
 $smarty->assign("MOD", $mod_strings);
 $smarty->assign("APP", $app_strings);
 $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MODULE",$currentModule);
+$smarty->assign("CUSTOMVIEW",$customstrings);
+$smarty->assign("BUTTONS",$other_text);
+$smarty->assign("CATEGORY",$category);
 /*
 $comboFieldNames = Array('manufacturer'=>'manufacturer_dom'
                       ,'productcategory'=>'productcategory_dom');
@@ -283,15 +295,13 @@ echo get_form_footer();
 
 
 // Buttons and View options
-$other_text = '<table width="100%" border="0" cellpadding="1" cellspacing="0">
-	<form name="massdelete" method="POST">
-	<tr>
+$other_text = '	<form name="massdelete" method="POST">
 	<input name="idlist" type="hidden">
 	<input name="viewname" type="hidden" value="'.$viewid.'">
 	<td>';
 if(isPermitted('Vendor',2,'') == 'yes')
 {
-	$other_text .=	'<input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/>&nbsp;';
+	$other_text .=	'<input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/>&nbsp;</form>';
 }
 
 if($viewid == 0)
@@ -308,15 +318,13 @@ $cvHTML = '<a href="index.php?module=Products&action=CustomView&smodule=VENDOR&r
 <span class="sep">|</span>
 <a href="index.php?module=Products&action=CustomView&smodule=VENDOR" class="link">'.$app_strings['LNK_CV_CREATEVIEW'].'</a>';
 }
-	$other_text .='<td align="right">'.$app_strings[LBL_VIEW].'
+	$customstrings = '<td align="right">'.$app_strings[LBL_VIEW].'
                         <SELECT NAME="view" onchange="showDefaultCustomView(this)">
                                 <OPTION VALUE="0">'.$mod_strings[LBL_ALL].'</option>
 				'.$customviewcombo_html.'
                         </SELECT>
 			'.$cvHTML.'
-                </td>
-        </tr>
-        </table>';
+                </td>';
 
 //Retreive the list from Database
 //<<<<<<<<<customview>>>>>>>>>
