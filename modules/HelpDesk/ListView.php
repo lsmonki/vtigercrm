2526218ddc109515a45d08a87e9eea246f3dd7e9
ask_ahmed <ask_ahmed@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-29 12:18:52 +0000

    Buttons & Combo box separated in ListView Ui

--- modules/HelpDesk/ListView.php
+++ modules/HelpDesk/ListView.php
@@ -38,6 +38,15 @@ global $theme;
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
 
+if(isset($_REQUEST['category']) && $_REQUEST['category'] !='')
+{
+	$category = $_REQUEST['category'];
+}
+else
+{
+	$category = getParentTabFromModule($currentModule);
+}
+
 $focus = new HelpDesk();
 
 $url_string = ''; // assigning http url string
@@ -280,14 +289,12 @@ if($viewid != 0)
         $CActionDtls = $oCustomView->getCustomActionDetails($viewid);
 }
 // Buttons and View options
-$other_text = '<table width="100%" border="0" cellpadding="1" cellspacing="0">
-	<form name="massdelete" method="POST">
-	<tr>
+$other_text = '	<form name="massdelete" method="POST">
 	<input name="idlist" type="hidden">
 	<input name="viewname" type="hidden" value="'.$viewid.'">';
 if(isPermitted('HelpDesk',2,'') == 'yes')
 {
-        $other_text .='<td><input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/></td>';
+        $other_text .='<td><input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/></td></form>';
 }
 
 if($viewid == 0)
@@ -306,23 +313,24 @@ else
 		<a href="index.php?module=HelpDesk&action=CustomView" class="link">'.$app_strings['LNK_CV_CREATEVIEW'].'</a>';
 }
 
-$other_text .='<td align="right">'.$app_strings[LBL_VIEW].'
+$customstrings ='<td align="right">'.$app_strings[LBL_VIEW].'
 			<SELECT NAME="view" onchange="showDefaultCustomView(this)">
 				<OPTION VALUE="0">'.$mod_strings[LBL_ALL].'</option>
 				'.$customviewcombo_html.'
 			</SELECT>
 			'.$cvHTML.'
-		</td>
-	</tr>
-	</table>';
+		</td>';
 
 $customview= get_form_header($current_module_strings['LBL_LIST_FORM_TITLE'],$other_text, false);
 $smarty = new vtigerCRM_Smarty;
-$smarty->assign("CUSTOMVIEW",$customview);
+$smarty->assign("CUSTOMVIEW",$customstrings);
 $smarty->assign("MOD", $mod_strings);
 $smarty->assign("APP", $app_strings);
 $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MODULE",$currentModule);
+$smarty->assign("BUTTONS",$other_text);
+$smarty->assign("CATEGORY",$category);
+
 //Retreive the list from Database
 //<<<<<<<<<customview>>>>>>>>>
 if($viewid != "0")
--- modules/Quotes/ListView.php
+++ modules/Quotes/ListView.php
@@ -43,6 +43,15 @@ global $theme;
 $comboFieldNames = Array('quotestage'=>'quotestage_dom');
 $comboFieldArray = getComboArray($comboFieldNames);
 
+if(isset($_REQUEST['category']) && $_REQUEST['category'] !='')
+{
+	$category = $_REQUEST['category'];
+}
+else
+{
+	$category = getParentTabFromModule($currentModule);
+}
+
 // focus_list is the means of passing data to a ListView.
 global $focus_list;
 
@@ -236,15 +245,13 @@ $search_form->assign("CUSTOMFIELD", $custfld);
 }
 */
 
-$other_text = '<table width="100%" border="0" cellpadding="1" cellspacing="0">
-	<form name="massdelete" method="POST">
-	<tr>
+$other_text = '<form name="massdelete" method="POST">
 	<input name="idlist" type="hidden">
 	<input name="viewname" type="hidden" value="'.$viewid.'">
 	<td>';
 if(isPermitted('Quotes',2,'') == 'yes')
 {
-        $other_text .=	'<input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/></td>';
+        $other_text .=	'<input class="button" type="submit" value="'.$app_strings[LBL_MASS_DELETE].'" onclick="return massDelete()"/></td></form>';
 }
 
 if($viewid == 0)
@@ -261,15 +268,13 @@ $cvHTML = '<a href="index.php?module=Quotes&action=CustomView&record='.$viewid.'
 <span class="sep">|</span>
 <a href="index.php?module=Quotes&action=CustomView" class="link">'.$app_strings['LNK_CV_CREATEVIEW'].'</a>';
 }
-	$other_text .='<td align="right">'.$app_strings[LBL_VIEW].'
+	$customstrings = '<td align="right">'.$app_strings[LBL_VIEW].'
                         <SELECT NAME="view" onchange="showDefaultCustomView(this)">
                                 <OPTION VALUE="0">'.$mod_strings[LBL_ALL].'</option>
 				'.$customviewcombo_html.'
                         </SELECT>
 			'.$cvHTML.'
-                </td>
-        </tr>
-        </table>';
+                </td>';
 
 
 //echo get_form_header($current_module_strings['LBL_LIST_FORM_TITLE'],$other_text, false);
@@ -286,7 +291,9 @@ $smarty->assign("MOD", $mod_strings);
 $smarty->assign("APP", $app_strings);
 $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MODULE",$currentModule);
-$smarty->assign("CUSTOMVIEW",$customView);
+$smarty->assign("CUSTOMVIEW",$customstrings);
+$smarty->assign("BUTTONS", $other_text);
+$smarty->assign("CATEGORY",$category);
 
 //Retreive the list from Database
 //<<<<<<<<<customview>>>>>>>>>
