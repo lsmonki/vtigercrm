b707a3f8eb06ab7d58d861971d697a09cd464639
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2005-12-30 10:47:40 +0000

    * Added initial version

---
+++ modules/Vendors/language/en_us.lang.php
@@ -0,0 +1,34 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+
+$mod_strings = array(
+
+'Vendor Name'=>'Vendor Name',
+'Name'=>'Name',
+'Company Name'=>'Company Name',
+'Phone'=>'Phone',
+'Email'=>'Email',
+'Website'=>'Website',
+'GL Account'=>'GL Account',
+'Category'=>'Category',
+'Street'=>'Street',
+'City'=>'City',
+'State'=>'State',
+'Postal Code'=>'Postal Code',
+'Country'=>'Country',
+'Description'=>'Description',
+'Po Box'=>'Po Box',
+
+
+
+);
+?>
