264ea597be3d98c357ee62e3dd60591275eca408
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-03 13:08:41 +0000

    * Changed the return action from PriceBookDetailView to DetailView

--- modules/PriceBooks/Save.php
+++ modules/PriceBooks/Save.php
@@ -51,7 +51,7 @@ $return_id = $focus->id;
 if(isset($_REQUEST['return_module']) && $_REQUEST['return_module'] != "") $return_module = $_REQUEST['return_module'];
 else $return_module = "PriceBooks";
 if(isset($_REQUEST['return_action']) && $_REQUEST['return_action'] != "") $return_action = $_REQUEST['return_action'];
-else $return_action = "PriceBookDetailView";
+else $return_action = "DetailView";
 if(isset($_REQUEST['return_id']) && $_REQUEST['return_id'] != "") $return_id = $_REQUEST['return_id'];
 
 header("Location: index.php?action=$return_action&module=$return_module&record=$return_id");
