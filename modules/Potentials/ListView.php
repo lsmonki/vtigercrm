58f766b8371c7eaf776263da7e759d7f9a521e81
cooljaguar <cooljaguar@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-05 07:41:02 +0000

    The old functionality of search has been removed and call for new search has been included

--- modules/Potentials/ListView.php
+++ modules/Potentials/ListView.php
@@ -22,7 +22,6 @@ require_once('include/logging.php');
 require_once('include/ListView/ListView.php');
 require_once('include/ComboUtil.php');
 require_once('include/utils/utils.php');
-require_once('include/utils/SearchUtils.php');
 require_once('modules/CustomView/CustomView.php');
 
 global $app_strings;
@@ -71,123 +70,31 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] == 'true')
 {
 	// we have a query
 	$url_string .="&query=true";
-	if (isset($_REQUEST['potentialname'])) $name = $_REQUEST['potentialname'];
-	if (isset($_REQUEST['accountname'])) $accountname = $_REQUEST['accountname'];
-	if (isset($_REQUEST['closingdate'])) $date_closed = $_REQUEST['closingdate'];
-	if (isset($_REQUEST['amount'])) $amount = $_REQUEST['amount'];
-	if (isset($_REQUEST['nextstep'])) $nextstep = $_REQUEST['nextstep'];
-	if (isset($_REQUEST['probability'])) $probability = $_REQUEST['probability'];
 
-	if (isset($_REQUEST['leadsource'])) $lead_source = $_REQUEST['leadsource'];
-	if (isset($_REQUEST['opportunity_type'])) $opportunity_type = $_REQUEST['opportunity_type'];
-	if (isset($_REQUEST['sales_stage'])) $sales_stage = $_REQUEST['sales_stage'];
-	if (isset($_REQUEST['current_user_only'])) $current_user_only = $_REQUEST['current_user_only'];
-	if (isset($_REQUEST['assigned_user_id'])) $assigned_user_id = $_REQUEST['assigned_user_id'];
-	if (isset($_REQUEST['closingdate_start'])) $date_closed_start = $_REQUEST['closingdate_start'];
-	if (isset($_REQUEST['closingdate_end'])) $date_closed_end = $_REQUEST['closingdate_end'];
-
-	$where_clauses = array();
-
-//Added for Custom Field Search
-$sql="select * from field where tablename='potentialscf' order by fieldlabel";
-$result=$adb->query($sql);
-for($i=0;$i<$adb->num_rows($result);$i++)
-{
-        $column[$i]=$adb->query_result($result,$i,'columnname');
-        $fieldlabel[$i]=$adb->query_result($result,$i,'fieldlabel');
-        $uitype[$i]=$adb->query_result($result,$i,'uitype');
-        if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
-
-        if(isset($customfield[$i]) && $customfield[$i] != '')
-        {
-		if($uitype[$i] == 56)
-			$str = " potentialscf.".$column[$i]." = 1";
-		elseif($uitype[$i] == 15)//Added to handle the picklist customfield - after 4.2 patch2
-                        $str = " potentialscf.".$column[$i]." = '".$customfield[$i]."'";
-		else
-	                $str = " potentialscf.".$column[$i]." like '$customfield[$i]%'";
-                array_push($where_clauses, $str);
-		$url_string .="&".$column[$i]."=".$customfield[$i];
-        }
-}
-//upto this added for Custom Field
-
-	if(isset($name) && $name != "") {
-			array_push($where_clauses, "potential.potentialname like ".PearDatabase::quote($name.'%')."");
-			$url_string .= "&potentialname=".$name;
-	}
-	if(isset($accountname) && $accountname != "") {
-			array_push($where_clauses, "account.accountname like ".PearDatabase::quote($accountname.'%')."");
-			$url_string .= "&accountname=".$accountname;
+	//Call for search function - Jaguar
+	$where=Search($currentModule);
 
 
-	}
-	if(isset($lead_source) && $lead_source == "None") {
-			array_push($where_clauses, "potential.leadsource = ".PearDatabase::quote($lead_source)."");
-			$url_string .= "&leadsource=".$lead_source;
-	}
-	// added to handle request from dashboard GS
-	if(isset($lead_source) && $lead_source != "") {
-			array_push($where_clauses, "potential.leadsource = ".PearDatabase::quote($lead_source)."");
-			$url_string .= "&leadsource=".$lead_source;
-	}
-	if(isset($opportunity_type) && $opportunity_type != "") {
-			array_push($where_clauses, "potential.potentialtype = ".PearDatabase::quote($opportunity_type)."");
-			$url_string .= "&opportunity_type=".$opportunity_type;
-	}
-	if(isset($amount) && $amount != "") {
-			array_push($where_clauses, "potential.amount like ".PearDatabase::quote($amount.'%')."");
-			$url_string .= "&amount=".$amount;
-	}
-	if(isset($nextstep) && $nextstep != "") {
-			array_push($where_clauses, "potential.nextstep like ".PearDatabase::quote($nextstep.'%')."");
-			$url_string .= "&nextstep=".$nextstep;
-	}
-	if(isset($sales_stage) && $sales_stage != "") {
-			if($sales_stage=='Other')
-				array_push($where_clauses, "(potential.sales_stage <> 'Closed Won' and potential.sales_stage <> 'Closed Lost')");
+	//Added for Custom Field Search
+	$sql="select * from field where tablename='potentialscf' order by fieldlabel";
+	$result=$adb->query($sql);
+	for($i=0;$i<$adb->num_rows($result);$i++)
+	{
+		$column[$i]=$adb->query_result($result,$i,'columnname');
+		$fieldlabel[$i]=$adb->query_result($result,$i,'fieldlabel');
+		$uitype[$i]=$adb->query_result($result,$i,'uitype');
+		if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
+
+		if(isset($customfield[$i]) && $customfield[$i] != '')
+		{
+			if($uitype[$i] == 56)
+				$str = " potentialscf.".$column[$i]." = 1";
+			elseif($uitype[$i] == 15)//Added to handle the picklist customfield - after 4.2 patch2
+				$str = " potentialscf.".$column[$i]." = '".$customfield[$i]."'";
 			else
-				array_push($where_clauses, "potential.sales_stage = ".PearDatabase::quote($sales_stage));
-			$url_string .= "&sales_stage=".$sales_stage;
-	}
-	if(isset($probability) && $probability != "") {
-			array_push($where_clauses, "potential.probability like ".PearDatabase::quote($probability.'%')."");
-			$url_string .= "&probability=".$probability;
-
-	}
-	if(isset($current_user_only) && $current_user_only != "") {
-			array_push($where_clauses, "crmentity.smownerid='$current_user->id'");
-			$url_string .= "&current_user_only=".$current_user_only;
-	}
-	if(isset($date_closed) && $date_closed != "") {
-			array_push($where_clauses, $adb->getDBDateString("potential.closingdate")." like ".PearDatabase::quote($date_closed.'%')."");
-			$url_string .= "&closingdate=".$date_closed;
-	}
-	if(isset($date_closed_start) && $date_closed_start != "" && isset($date_closed_end) && $date_closed_end != "")
-	{
-			array_push($where_clauses, "potential.closingdate >= ".PearDatabase::quote($date_closed_start)." and potential.closingdate <= ".PearDatabase::quote($date_closed_end));
-			$url_string .= "&closingdate_start=".$date_closed_start;
-	}
-
-	$where = "";
-	foreach($where_clauses as $clause)
-	{
-		if($where != "")
-		$where .= " and ";
-		$where .= $clause;
-	}
-
-	if (!empty($assigned_user_id)) {
-		if (!empty($where)) {
-			$where .= " AND ";
-		}
-		$where .= "crmentity.smownerid IN(";
-		foreach ($assigned_user_id as $key => $val) {
-			$where .= "".PearDatabase::quote($val)."";
-			$where .= ($key == count($assigned_user_id) - 1) ? ")" : ", ";
-			// ACIPIA - to allow prev/next button to use criterias
-			$url_string .= '&' . urlencode( 'assigned_user_id[]' ) . '=' . $val ;
-			// ACIPIA 
+				$str = " potentialscf.".$column[$i]." like '$customfield[$i]%'";
+			array_push($where_clauses, $str);
+			$url_string .="&".$column[$i]."=".$customfield[$i];
 		}
 	}
 
