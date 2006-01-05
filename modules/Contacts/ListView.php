c9d835e805a70265a5e2c751cb246fb2ba172662
cooljaguar <cooljaguar@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-05 07:27:56 +0000

    The old functionality of search has been deleted and the 'call for new function' has been added

--- modules/Contacts/ListView.php
+++ modules/Contacts/ListView.php
@@ -70,27 +70,7 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] == 'true')
 {
 	// we have a query
 	$url_string .="&query=true";
-	if (isset($_REQUEST['firstname'])) $firstname = $_REQUEST['firstname'];
-	if (isset($_REQUEST['lastname'])) $lastname = $_REQUEST['lastname'];
-	if (isset($_REQUEST['accountname'])) $accountname = $_REQUEST['accountname'];
-	if (isset($_REQUEST['leadsource'])) $leadsource = $_REQUEST['leadsource'];
-	if (isset($_REQUEST['donotcall'])) $donotcall = $_REQUEST['donotcall'];
-	if (isset($_REQUEST['phone'])) $phone = $_REQUEST['phone'];
-	if (isset($_REQUEST['email'])) $email = $_REQUEST['email'];
-	if (isset($_REQUEST['yahooid'])) $yahooid = $_REQUEST['yahooid'];
-	if (isset($_REQUEST['assistant'])) $assistant = $_REQUEST['assistant'];
-	if (isset($_REQUEST['emailoptout'])) $emailoptout = $_REQUEST['emailoptout'];
-	if (isset($_REQUEST['mailingstreet'])) $mailingstreet = $_REQUEST['mailingstreet'];
-	if (isset($_REQUEST['mailingcity'])) $mailingcity = $_REQUEST['mailingcity'];
-	if (isset($_REQUEST['mailingstate'])) $mailingstate = $_REQUEST['mailingstate'];
-	if (isset($_REQUEST['mailingzip'])) $mailingzip = $_REQUEST['mailingzip'];
-	if (isset($_REQUEST['mailingcountry'])) $mailingcountry = $_REQUEST['mailingcountry'];
-	if (isset($_REQUEST['current_user_only'])) $current_user_only = $_REQUEST['current_user_only'];
-	if (isset($_REQUEST['assigned_user_id'])) $assigned_user_id = $_REQUEST['assigned_user_id'];
-
-
-	$where_clauses = Array();
-
+	$where=Search($currentModule);
 //Added for Custom Field Search
 $sql="select * from field where tablename='contactscf' order by fieldlabel";
 $result=$adb->query($sql);
@@ -115,98 +95,6 @@ for($i=0;$i<$adb->num_rows($result);$i++)
 }
 //upto this added for Custom Field
 
-
-	if(isset($lastname) && $lastname != "") {
-			array_push($where_clauses, "contactdetails.lastname like ".PearDatabase::quote($lastname.'%')."");
-			$url_string .= "&lastname=".$lastname;
-	}
-	if(isset($firstname) && $firstname != "") {
-			array_push($where_clauses, "contactdetails.firstname like ".PearDatabase::quote($firstname.'%')."");
-			$url_string .= "&firstname=".$firstname;
-	}
-	if(isset($accountname) && $accountname != "")	{
-			array_push($where_clauses, "account.accountname like ".PearDatabase::quote($accountname.'%')."");
-			$url_string .= "&accountname=".$accountname;
-	}
-	if(isset($leadsource) && $leadsource != "") {
-			array_push($where_clauses, "contactsubdetails.leadsource = ".PearDatabase::quote($leadsource)."");
-			$url_string .= "&leadsource=".$leadsource;
-	}
-	if(isset($donotcall) && $donotcall != "") {
-			array_push($where_clauses, "contactdetails.donotcall = ".PearDatabase::quote($donotcall)."");
-			$url_string .= "&donotcall=".$donotcall;
-	}
-	if(isset($phone) && $phone != "") {
-			array_push($where_clauses, "(contactdetails.phone like ".PearDatabase::quote('%'.$phone.'%')." OR contactdetails.mobile like ".PearDatabase::quote('%'.$phone.'%')." OR contactdetails.fax like ".PearDatabase::quote('%'.$phone.'%').")");
-			$url_string .= "&phone=".$phone;
-	}
-	if(isset($email) && $email != "") {
-			array_push($where_clauses, "(contactdetails.email like ".PearDatabase::quote($email.'%').")");
-			$url_string .= "&email=".$email;
-	}
-	if(isset($yahooid) && $yahooid != "") {
-			array_push($where_clauses, "contactdetails.yahooid like ".PearDatabase::quote($yahooid.'%')."");
-			$url_string .= "&yahooid=".$yahooid;
-	}
-	if(isset($assistant) && $assistant != "") {
-			array_push($where_clauses, "contactsubdetails.assistant like ".PearDatabase::quote($assistant.'%')."");
-			$url_string .= "&assistant=".$assistant;
-	}
-	if(isset($mailingstreet) && $mailingstreet != "") {
-			array_push($where_clauses, "(contactaddress.mailingstreet like ".PearDatabase::quote($mailingstreet.'%')." OR contactaddress.otherstreet like ".PearDatabase::quote($mailingstreet.'%').")");
-			$url_string .= "&mailingstreet=".$mailingstreet;
-	}
-	if(isset($mailingcity) && $mailingcity != "") {
-			array_push($where_clauses, "(contactaddress.mailingcity like ".PearDatabase::quote($mailingcity.'%')." OR contactaddress.othercity like ".PearDatabase::quote($mailingcity.'%').")");
-			$url_string .= "&mailingcity=".$mailingcity;
-	}
-	if(isset($mailingstate) && $mailingstate != "") {
-			array_push($where_clauses, "(contactaddress.mailingstate like ".PearDatabase::quote($mailingstate.'%')." OR contactaddress.otherstate like ".PearDatabase::quote($mailingstate.'%').")");
-			$url_string .= "&mailingstate=".$mailingstate;
-	}
-	if(isset($mailingzip) && $mailingzip != "") {
-			array_push($where_clauses, "(contactaddress.mailingzip like ".PearDatabase::quote($mailingzip.'%')." OR contactaddress.otherzip like ".PearDatabase::quote($mailingzip.'%').")");
-			$url_string .= "&mailingzip=".$mailingzip;
-	}
-	if(isset($mailingcountry) && $mailingcountry != "") {
-			array_push($where_clauses, "(contactaddress.mailingcountry like ".PearDatabase::quote($mailingcountry.'%')." OR contactaddress.othercountry like ".PearDatabase::quote($mailingcountry.'%').")");
-			$url_string .= "&mailingcountry=".$mailingcountry;
-	}
-	if(isset($current_user_only) && $current_user_only != "") {
-			array_push($where_clauses, "crmentity.smownerid='$current_user->id'");
-			$url_string .= "&current_user_only=".$current_user_only;
-	}	
-	if(isset($emailoptout) && $emailoptout != "") {
-			array_push($where_clauses, "contactdetails.emailoptout = 1");
-			$url_string .= "&emailoptout=".$emailoptout;
-	}
-        else
-	{
-			array_push($where_clauses, "contactdetails.emailoptout = 0");
-	}
-	
-	$where = "";
-	foreach($where_clauses as $clause)
-	{
-		if($where != "")
-		$where .= " and ";
-		$where .= $clause;
-	}
-
-	if (!empty($assigned_user_id)) {
-		if (!empty($where)) {
-			$where .= " AND ";
-		}
-		$where .= "crmentity.smownerid IN(";
-		foreach ($assigned_user_id as $key => $val) {
-			$where .= "".PearDatabase::quote($val)."";
-			$where .= ($key == count($assigned_user_id) - 1) ? ")" : ", ";
-			// ACIPIA - to allow prev/next button to use criterias
-		        $url_string .= '&' . urlencode( 'assigned_user_id[]' ) . '=' . $val ;
-			//ACIPIA
-		}
-	}
-
 	$log->info("Here is the where clause for the list view: $where");
 
 }
@@ -217,84 +105,6 @@ $customviewcombo_html = $oCustomView->getCustomViewCombo();
 $viewid = $oCustomView->getViewId($currentModule);
 $viewnamedesc = $oCustomView->getCustomViewByCvid($viewid);
 //<<<<<customview>>>>>
-/*****************changed for new UI and Smarty --by mangai *****************/
-/*
-if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
-	// Stick the form header out there.
-	$search_form=new XTemplate ('modules/Contacts/SearchForm.html');
-	$search_form->assign("MOD", $current_module_strings);
-	$search_form->assign("APP", $app_strings);
-
-	
-	$search_form->assign("VIEWID",$viewid);
-
-	$search_form->assign("JAVASCRIPT", get_clear_form_js());
-
-	$search_form->assign("BASIC_LINK", "index.php?module=Contacts&action=index".$url_string."&viewname=".$viewid);
-	$search_form->assign("ADVANCE_LINK", "index.php?module=Contacts&action=index&advanced=true".$url_string."&viewname=".$viewid);
-
-	if (isset($firstname)) $search_form->assign("FIRST_NAME", $_REQUEST['firstname']);
-	if (isset($lastname)) $search_form->assign("LAST_NAME", $_REQUEST['lastname']);
-	if (isset($accountname)) $search_form->assign("ACCOUNT_NAME", $_REQUEST['accountname']);
-	$search_form->assign("JAVASCRIPT", get_clear_form_js());
-
-	echo get_form_header($current_module_strings['LBL_SEARCH_FORM_TITLE'], "", false);
-
-
-	if(isset($current_user_only)) $search_form->assign("CURRENT_USER_ONLY", "checked");
-
-	if (isset($_REQUEST['advanced']) && $_REQUEST['advanced'] == 'true') {
-
-		$url_string .="&advanced=true";
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Contacts','index','lastname','true','advanced',"","","","",$viewid));
-
-		if(isset($accountname)) $search_form->assign("ACCOUNT_NAME", $accountname);
-		//if(isset($createdtime)) $search_form->assign("DATE_ENTERED", $createdtime);
-		//if(isset($modifiedtime)) $search_form->assign("DATE_MODIFIED", $modifiedtime);
-		//if(isset($modified_user_id)) $search_form->assign("MODIFIED_USER_ID", $modified_user_id);
-		//if(isset($donotcall)) $search_form->assign("DO_NOT_CALL", $donotcall);
-		if(isset($phone)) $search_form->assign("PHONE", $phone);
-		if(isset($email)) $search_form->assign("EMAIL", $email);
-		if(isset($yahooid)) $search_form->assign("YAHOO_ID", $yahooid);
-		if(isset($assistant)) $search_form->assign("ASSISTANT", $assistant);
-		if(isset($emailoptout)) $search_form->assign("EMAIL_OPT_OUT", "CHECKED");
-		if(isset($mailingstreet)) $search_form->assign("ADDRESS_STREET", $mailingstreet);
-		if(isset($mailingcity)) $search_form->assign("ADDRESS_CITY", $mailingcity);
-		if(isset($mailingstate)) $search_form->assign("ADDRESS_STATE", $mailingstate);
-		if(isset($mailingzip)) $search_form->assign("ADDRESS_POSTALCODE", $mailingzip);
-		if(isset($mailingcountry)) $search_form->assign("ADDRESS_COUNTRY", $mailingcountry);
-
-		if (isset($leadsource)) $search_form->assign("LEAD_SOURCE_OPTIONS", get_select_options($comboFieldArray['leadsource_dom'], $leadsource, $_REQUEST['advanced']));
-		else $search_form->assign("LEAD_SOURCE_OPTIONS", get_select_options($comboFieldArray['leadsource_dom'], '', $_REQUEST['advanced']));
-
-		if (!empty($assigned_user_id)) $search_form->assign("USER_FILTER", get_select_options_with_id(get_user_array(FALSE), $assigned_user_id));
-		else $search_form->assign("USER_FILTER", get_select_options_with_id(get_user_array(FALSE), ''));
-
-//Added for Custom Field Search
-$sql="select * from field where tablename='contactscf' order by fieldlabel";
-$result=$adb->query($sql);
-for($i=0;$i<$adb->num_rows($result);$i++)
-{
-        $column[$i]=$adb->query_result($result,$i,'columnname');
-        $fieldlabel[$i]=$adb->query_result($result,$i,'fieldlabel');
-        if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
-}
-require_once('include/CustomFieldUtil.php');
-$custfld = CustomFieldSearch($customfield, "contactscf","contactscf", "contactid", $app_strings,$theme,$column,$fieldlabel);
-$search_form->assign("CUSTOMFIELD", $custfld);
-//upto this added for Custom Field
-
-		$search_form->parse("advanced");
-		$search_form->out("advanced");
-	}
-	else {
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Contacts','index','lastname','true','basic',"","","","",$viewid));
-		$search_form->parse("main");
-		$search_form->out("main");
-	}
-	echo get_form_footer();
-	echo "\n<BR>\n";
-}*/
 
 
 // Buttons and View options
