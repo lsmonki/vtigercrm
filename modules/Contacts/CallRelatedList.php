73b9c59c2673146715e69e2fca9fbd89c9590726
ask_ahmed <ask_ahmed@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-06 07:09:33 +0000

    CallRelatedList files' hidden form tag added

--- modules/Contacts/CallRelatedList.php
+++ modules/Contacts/CallRelatedList.php
@@ -30,4 +30,26 @@ $hidden = '<form border="0" action="index.php" method="post" name="form" id="for
 	$hidden .= '<input type="hidden" name="mode">';
 	$hidden .= '<input type="hidden" name="contact_id" value="'.$focus->id.'">';
 	$hidden .= '<input type="hidden" name="account_id" value="'.$accountid.'">';
-	$hidden .= '<input type="hidden" name="return_mod
+	$hidden .= '<input type="hidden" name="return_module" value="Contacts">';
+	$hidden .= '<input type="hidden" name="return_action" value="CallRelatedList">';
+	$hidden .= '<input type="hidden" name="return_id" value="'.$focus->id.'">';
+	$hidden .= '<input type="hidden" name="parent_id" value="'.$focus->id.'">';
+	$hidden .= '<input type="hidden" name="action">';
+	$smarty->assign("HIDDEN",$hidden);
+	
+
+if(isset($_request['isduplicate']) && $_request['isduplicate'] == 'true') {
+        $focus->id = "";
+}
+$category = getparenttab();
+$smarty->assign("CATEGORY",$category);
+
+$smarty->assign("id",$focus->id);
+$smarty->assign("NAME",$focus->name);
+$related_array = getrelatedlists("Contacts",$focus);
+$smarty->assign("RELATEDLISTS", $related_array);
+$smarty->assign("MODULE",$currentmodule);
+$smarty->assign("ID",$record );
+$smarty->display("relatedlists.tpl");
+
+?>
