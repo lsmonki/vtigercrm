320d1853608596e0422d70234582bf496bd38961
jerrydgeorge <jerrydgeorge@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-07 07:36:05 +0000

    Rss feed related code are  added to listview files as the header arragement is changed

--- modules/Rss/ListView.php
+++ modules/Rss/ListView.php
@@ -59,6 +59,18 @@ if(isset($_REQUEST[record]))
 {
 	$rss_html = $oRss->getStarredRssHTML();
 }
+if($currentModule == "Rss")
+{
+	require_once("modules/".$currentModule."/Forms.php");
+	if (function_exists('get_rssfeeds_form'))
+	{
+		$rss_form->assign("RSSFEEDS_TITLE","<div style='float:left'>".$app_strings['LBL_RSS_FEEDS'].":</div><div style='float:right;'><a href='javascript:openPopUp(\"addRssFeedIns\",this,\"index.php?action=Popup&module=Rss\",\"addRssFeedWin\",350,150,\"menubar=no,toolbar=no,location=no,status=no,scrollbars=yes,resizable=yes\");' title='".$app_strings['LBL_ADD_RSS_FEEDS']."'>Add<img src='".$image_path."/addrss.gif' border=0 align=absmiddle></a>&nbsp;</div>");
+		$rss_form->assign("RSSFEEDS", get_rssfeeds_form());
+	}
+}
+
+
+
 //$all_rss_html = $oRss->getAllRssFeeds();
 //$stared_rss_html = $oRss->getStaredRssFeeds();
 //$crm_rss_html = $oRss->getCRMRssFeeds();
