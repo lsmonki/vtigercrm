6e4bff7940399299bfd1d65a9482d9a1738a5b83
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-12 09:26:12 +0000

    hidden fields have been removed

--- modules/PriceBooks/CallRelatedList.php
+++ modules/PriceBooks/CallRelatedList.php
@@ -34,17 +34,6 @@ $related_array=getRelatedLists("PriceBooks",$focus);
 
 $smarty = new vtigerCRM_Smarty;
 
-$hidden = '<form border="0" action="index.php" method="post" name="form" id="form">';
-        $hidden .= '<input type="hidden" name="module">';
-        $hidden .= '<input type="hidden" name="mode">';
-        $hidden .= '<input type="hidden" name="pricebook_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="return_module" value="PriceBooks">';
-        $hidden .= '<input type="hidden" name="return_action" value="CallRelatedList">';
-        $hidden .= '<input type="hidden" name="return_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="parent_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="action">';	
-	$smarty->assign("HIDDEN",$hidden);
-
 if(isset($focus->name))
 	$smarty->assign("NAME", $focus->name);
 $category = getParentTab();
@@ -54,6 +43,7 @@ $smarty->assign("id",$focus->id);
 $smarty->assign("ID",$RECORD );
 $smarty->assign("MODULE",$currentmodule);
 $smarty->assign("RELATEDLISTS", $related_array);
+$smarty->assign("SINGLE_MOD","PriceBook");
 $smarty->display("RelatedLists.tpl");
 
 ?>
