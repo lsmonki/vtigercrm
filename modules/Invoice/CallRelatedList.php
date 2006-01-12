fa59faa1a31ecb123b0648b467826ef4c32c5345
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-12 09:19:04 +0000

    hidden fields have been removed

--- modules/Invoice/CallRelatedList.php
+++ modules/Invoice/CallRelatedList.php
@@ -1,4 +1,14 @@
 <?php
+/*********************************************************************************
+  ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+   * ("License"); You may not use this file except in compliance with the License
+   * The Original Code is:  vtiger CRM Open Source
+   * The Initial Developer of the Original Code is vtiger.
+   * Portions created by vtiger are Copyright (C) vtiger.
+   * All Rights Reserved.
+  *
+   ********************************************************************************/
+
 
 require_once('Smarty_setup.php');
 require_once('modules/Invoice/Invoice.php');
@@ -15,17 +25,6 @@ $log->debug("name is ".$focus->name);
 
 $smarty = new vtigerCRM_Smarty;
 
-$hidden = '<form border="0" action="index.php" method="post" name="form" id="form">';
-        $hidden .= '<input type="hidden" name="module">';
-        $hidden .= '<input type="hidden" name="mode">';
-        $hidden .= '<input type="hidden" name="invoiceid" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="return_module" value="Invoice">';
-        $hidden .= '<input type="hidden" name="return_action" value="CallRelatedList">';
-        $hidden .= '<input type="hidden" name="return_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="parent_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="action">';	
-	$smarty->assign("HIDDEN",$hidden);
-
 if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
 $related_array=getRelatedLists("Invoice",$focus);
 $smarty->assign("RELATEDLISTS", $related_array);
@@ -35,5 +34,6 @@ $smarty->assign("CATEGORY",$category);
 $smarty->assign("id",$focus->id);
 $smarty->assign("ID",$RECORD );
 $smarty->assign("MODULE",$currentmodule);
+$smarty->assign("SINGLE_MOD",$currentmodule);
 $smarty->display("RelatedLists.tpl");
 ?>
