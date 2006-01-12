412a43b551362de7688d1e2f8034197a69528c71
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-12 09:51:54 +0000

    hidden fields have been removed

--- modules/PurchaseOrder/CallRelatedList.php
+++ modules/PurchaseOrder/CallRelatedList.php
@@ -1,11 +1,21 @@
 <?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
 
 require_once('Smarty_setup.php');
 require_once('modules/PurchaseOrder/PurchaseOrder.php');
 $focus = new Order();
 $currentmodule = $_REQUEST['module'];
 $RECORD = $_REQUEST['record'];
-if(isset($_REQUEST['record']) && isset($_REQUEST['record'])) {
+if(isset($_REQUEST['record']) && $_REQUEST['record']!='') {
     $focus->retrieve_entity_info($_REQUEST['record'],"PurchaseOrder");
     $focus->id = $_REQUEST['record'];
     $focus->name=$focus->column_fields['subject'];
@@ -14,17 +24,6 @@ $log->debug("name is ".$focus->name);
 }
 
 $smarty = new vtigerCRM_Smarty;
-
-$hidden = '<form border="0" action="index.php" method="post" name="form" id="form">';
-        $hidden .= '<input type="hidden" name="module">';
-        $hidden .= '<input type="hidden" name="mode">';
-        $hidden .= '<input type="hidden" name="'.$sid.'" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="return_module" value="PurchaseOrder">';
-       	$hidden .= '<input type="hidden" name="return_action" value="CallRelatedList">';
-        $hidden .= '<input type="hidden" name="return_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="parent_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="action">';	
-	$smarty->assign("HIDDEN",$hidden);
 
 if (isset($focus->name)) $smarty->assign("NAME", $focus->name);
 $related_array=getRelatedLists("PurchaseOrder",$focus);
@@ -35,5 +34,6 @@ $smarty->assign("CATEGORY",$category);
 $smarty->assign("id",$focus->id);
 $smarty->assign("ID",$RECORD );
 $smarty->assign("MODULE",$currentmodule);
+$smarty->assign("SINGLE_MOD",$currentmodule);
 $smarty->display("RelatedLists.tpl");
 ?>
--- modules/Vendors/CallRelatedList.php
+++ modules/Vendors/CallRelatedList.php
@@ -34,17 +34,6 @@ $related_array=getRelatedLists("Vendors",$focus);
 
 $smarty = new vtigerCRM_Smarty;
 
-$hidden = '<form border="0" action="index.php" method="post" name="form" id="form">';
-        $hidden .= '<input type="hidden" name="module">';
-        $hidden .= '<input type="hidden" name="mode">';
-        $hidden .= '<input type="hidden" name="'.$sid.'" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="return_module" value="Vendors">';
-        $hidden .= '<input type="hidden" name="return_action" value="CallRelatedList">';
-        $hidden .= '<input type="hidden" name="return_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="parent_id" value="'.$focus->id.'">';
-        $hidden .= '<input type="hidden" name="action">';
-	$smarty->assign("HIDDEN",$hidden);
-
 $category = getParentTab();
 $smarty->assign("CATEGORY",$category);
 
@@ -54,6 +43,7 @@ $smarty->assign("NAME", $focus->name);
 $smarty->assign("id",$focus->id);
 $smarty->assign("ID",$RECORD );
 $smarty->assign("MODULE",$currentmodule);
+$smarty->assign("SINGLE_MOD","Vendor");
 $smarty->assign("RELATEDLISTS", $related_array);
 $smarty->display("RelatedLists.tpl");
 
