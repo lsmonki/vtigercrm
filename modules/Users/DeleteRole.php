e932903a82f117e4ea0951b88767bdf1b5227708
crouchingtiger <crouchingtiger@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-16 06:39:32 +0000

    fixed issue

--- modules/Users/DeleteRole.php
+++ modules/Users/DeleteRole.php
@@ -13,17 +13,7 @@ global $adb;
 $del_id =  $_REQUEST['delete_role_id'];
 $tran_id = $_REQUEST['transfer_role_id'];
 
-//Updating the user2 role table
-$sql1 = "update user2role set roleid=".$tran_id." where roleid=".$del_id;
-$adb->query($sql1);
-
-//Deleteing from role2profile table
-$sql2 = "delete from role2profile where roleid=".$del_id;
-$adb->query($sql2);
-
-//delete from role table;
-$sql9 = "delete from role where roleid=".$del_id;
-$adb->query($sql9);
+deleteRole($del_id,$tran_id);
 
 header("Location: index.php?action=listroles&module=Users");
 ?>
