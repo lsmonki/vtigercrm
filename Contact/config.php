b045ac6e89bc9920bded87a4c939907e99af7a4d
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-19 13:33:57 +0000

    * Added intial version to split the config parameters from send_data.php file

---
+++ Contact/config.php
@@ -0,0 +1,24 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+
+$Server_Path   = 'http://rajeshkannan:80'; 	//This is the path where have you run ther vtiger server.
+
+$proxyhost     = '';       			//This is the proxy host 
+$proxyport     = '';       			//This is proxy port 
+$proxyusername = '';   				//This is the proxy setting user name 
+$proxypassword = '';   				//This is the proxy setting password
+
+//Default (1) is admin user id. if you want to assign the contacts to a vtiger user then you can give the user id
+$assigned_user_id = 1;
+
+
+?>
