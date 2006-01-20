7efb2247f3eb50794dd8cdcda1b0a68496206f1a
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-20 04:17:48 +0000

    changes made for contactimage checking

--- modules/Contacts/Save.php
+++ modules/Contacts/Save.php
@@ -70,18 +70,165 @@ foreach($focus->column_fields as $fieldname => $val)
 	if (!isset($_REQUEST['do_not_call'])) $focus->do_not_call = 'off';
 //}
 
-//$focus->saveentity("Contacts");
-$focus->save("Contacts");
-$return_id = $focus->id;
-//save_customfields($focus->id);
+//Checking If image is given or not
 
-if(isset($_REQUEST['return_module']) && $_REQUEST['return_module'] != "") $return_module = $_REQUEST['return_module'];
-else $return_module = "Contacts";
-if(isset($_REQUEST['return_action']) && $_REQUEST['return_action'] != "") $return_action = $_REQUEST['return_action'];
-else $return_action = "DetailView";
-if(isset($_REQUEST['return_id']) && $_REQUEST['return_id'] != "") $return_id = $_REQUEST['return_id'];
+$uploaddir = $root_directory."test/contact/" ;//set this to which location you need to give the contact image
+$log->info("The Location to Save the Contact Image is ".$uploaddir);
+$file_path_name = $_FILES['imagename']['name'];
+$image_error="false";
+$saveimage="true";
 
-if(isset($_REQUEST['activity_mode']) && $_REQUEST['activity_mode'] != '') $activitymode = $_REQUEST['activity_mode'];
+$file_name = basename($file_path_name);
+//if the image is given
+if($file_name!="")
+{
+         $log->debug("Contact Image is given for uploading");
+        $image_name_val=file_exist_fn($file_name,0);
+
+        $encode_field_values="";
+        $errormessage="";
+
+        $move_upload_status=move_uploaded_file($_FILES["imagename"]["tmp_name"],$uploaddir.$image_name_val);
+        $image_error="false";
+
+        //if there is an error in the uploading of image
+
+        $filetype= $_FILES['imagename']['type'];
+        $filesize = $_FILES['imagename']['size'];
+
+        $filetype_array=explode("/",$filetype);
+
+        $file_type_val_image=strtolower($filetype_array[0]);
+        $file_type_val=strtolower($filetype_array[1]);
+          $log->info("The File type of the Contact Image is :: ".$file_type_val);
+          //checking the uploaded image is if an image type or not
+	if(!$move_upload_status) //if any error during file uploading
+        {
+                $log->debug("Error is present in uploading Contact Image.");
+                $errorCode =  $_FILES['imagename']['error'];
+                        if($errorCode == 4)
+                        {
+                                $errorcode="no-image";
+                                $saveimage="false";
+                                $image_error="true";
+                        }
+                        else if($errorCode == 2)
+                        {
+                                $errormessage = 2;
+                                $saveimage="false";
+                                $image_error="true";
+                        }
+                        else if($errorCode == 3 )
+                        {
+                                $errormessage = 3;
+                                $saveimage="false";
+                                $image_error="true";
+                        }
+        }
+        else
+        {
+                 $log->debug("Successfully uploaded the Contact Image.");
+                if($filesize != 0)
+                {
+                        if (($file_type_val == "jpeg" ) || ($file_type_val == "png") || ($file_type_val == "jpg" ) || ($file_type_val == "pjpeg" ) || ($file_type_val == "x-png") || ($file_type_val == "gif") ) //Checking whether the file is an image or not
+                        {
+                                        $saveimage="true";
+                                        $image_error="false";
+                        }
+                        else
+                        {
+                                $savelogo="false";
+                                $image_error="true";
+                                $errormessage = "image";
+                        }
+	        }
+                else
+                {       $savelogo="false";
+                        $image_error="true";
+                        $errormessage = "invalid";
+                }
+
+        }
+}
+else //if image is not given
+{
+        $log->debug("Contact Image is not given for uploading.");
+        if($mode=="edit" && $image_error=="false" )
+        {
+                $image_name_val=getContactImageName($focus->id);
+                $saveimage="true";
+        }
+        else
+        {
+                $focus->column_fields['imagename']="";
+        }
+}
+
+ //code added for returning back to the current view after edit from list view
+if($_REQUEST['return_viewname'] == '') $return_viewname='0';
+if($_REQUEST['return_viewname'] != '')$return_viewname=$_REQUEST['return_viewname'];
+
+if($image_error=="true") //If there is any error in the file upload then moving all the data to EditView.
+{
+        //re diverting the page and reassigning the same values as image error occurs
+        if($_REQUEST['activity_mode'] != '')$activity_mode=$_REQUEST['activity_mode'];
+        if($_REQUEST['return_module'] != '')$return_module=$_REQUEST['return_module'];
+        if($_REQUEST['return_action'] != '')$return_action=$_REQUEST['return_action'];
+        if($_REQUEST['return_id'] != '')$return_id=$_REQUEST['return_id'];
+
+         $log->debug("There is an error during the upload of contact image.");
+        $field_values_passed.="";
+        foreach($focus->column_fields as $fieldname => $val)
+        {
+                if(isset($_REQUEST[$fieldname]))
+                {
+                         $log->debug("Assigning the previous values given for the contact to respective fields
+ ");
+                        $field_values_passed.="&";
+                        $value = $_REQUEST[$fieldname];
+                        $focus->column_fields[$fieldname] = $value;
+                        $field_values_passed.=$fieldname."=".$value;
+
+                }
+        }
+        $values_pass=$field_values_passed;
+        $encode_field_values=base64_encode($values_pass);
+
+        $error_module = "Contacts";
+        $error_action = "EditView";
+
+	if(isset($_request['return_id']) && $_request['return_id'] != "")
+                $return_id = $_request['return_id'];
+        if(isset($_request['activity_mode']))
+                $return_action .= '&activity_mode='.$_request['activity_mode'];
+
+        if($mode=="edit")
+        {
+                $return_id=$_REQUEST['record'];
+        }
+        header("location: index.php?action=$error_action&module=$error_module&record=$return_id&return_id=$return_id&return_action=$return_action&return_module=$return_module&activity_mode=$activity_mode&return_viewname=$return_viewname&saveimage=$saveimage&error_msg=$errormessage&image_error=$image_error&encode_val=$encode_field_values");
+
+}
+if($saveimage=="true")
+{
+        $focus->column_fields['imagename']=$image_name_val;
+        $log->debug("Assign the Image name to the field name ");
+}
+//Saving the contact
+if($image_error=="false")
+{
+	//$focus->saveentity("Contacts");
+	$focus->save("Contacts");
+	$return_id = $focus->id;
+	//save_customfields($focus->id);
+
+	if(isset($_REQUEST['return_module']) && $_REQUEST['return_module'] != "") $return_module = $_REQUEST['return_module'];
+	else $return_module = "Contacts";
+	if(isset($_REQUEST['return_action']) && $_REQUEST['return_action'] != "") $return_action = $_REQUEST['return_action'];
+	else $return_action = "DetailView";
+	if(isset($_REQUEST['return_id']) && $_REQUEST['return_id'] != "") $return_id = $_REQUEST['return_id'];
+
+	if(isset($_REQUEST['activity_mode']) && $_REQUEST['activity_mode'] != '') $activitymode = $_REQUEST['activity_mode'];
 
 $local_log->debug("Saved record with id of ".$return_id);
 
@@ -173,6 +320,7 @@ $log->info("This Page is redirected to : ".$return_module." / ".$return_action."
 if($_REQUEST['return_viewname'] == '') $return_viewname='0';
 if($_REQUEST['return_viewname'] != '')$return_viewname=$_REQUEST['return_viewname'];
 header("Location: index.php?action=$return_action&module=$return_module&record=$return_id&activity_mode=$activitymode&viewname=$return_viewname");
+}
 //Code to save the custom field info into database
 function save_customfields($entity_id)
 {
@@ -263,4 +411,49 @@ function save_customfields($entity_id)
 	}*/
 	
 }
+function file_exist_fn($filename,$exist)
+{
+        global $uploaddir;
+
+        if(!isset($exist))
+        {
+                $exist=0;
+        }
+        $filename_path=$uploaddir.$filename;
+        if (file_exists($filename_path)) //Checking if the file name already exists in the directory
+        {
+                if($exist!=0)
+                {
+                        $previous=$exist-1;
+                        $next=$exist+1;
+                        $explode_name=explode("_",$filename);
+                        $implode_array=array();
+                        for($j=0;$j<count($explode_name); $j++)
+                        {
+                                if($j!=0)
+                                {
+                                        $implode_array[]=$explode_name[$j];
+                                }
+                        }
+                        $implode_name=implode("_", $implode_array);
+                        $test_name=$implode_name;
+                }
+                else
+                {
+                        $implode_name=$filename;
+                }
+                $exist++;
+                $filename_val=$exist."_".$implode_name;
+                $testfilename = file_exist_fn($filename_val,$exist);
+                if($testfilename!="")
+                {
+                        return $testfilename;
+                }
+        }
+	else
+        {
+                return $filename;
+        }
+}
+
 ?>
