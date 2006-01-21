410fa2ef5d310a1945759989c9cd1d3c116572f5
cooljaguar <cooljaguar@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-01-21 12:35:18 +0000

    The function for 'Lead by Industry' has been added

--- modules/Dashboard/Charts_for_leads.php
+++ modules/Dashboard/Charts_for_leads.php
@@ -16,23 +16,24 @@ require_once('include/utils/CommonUtils.php');
 
 
 $period=($HTTP_GET_VARS['period'])?$HTTP_GET_VARS['period']:"tmon";
-	$dates_values=start_end_dates($period);
-        $date_start=$dates_values[0];
-        $end_date=$dates_values[1];
-        $period_type=$dates_values[2];
-        $width=$dates_values[3];
-         $height=$dates_values[4];
+$dates_values=start_end_dates($period);
+$date_start=$dates_values[0];
+$end_date=$dates_values[1];
+$period_type=$dates_values[2];
+$width=$dates_values[3];
+$height=$dates_values[4];
 
-
+$no_days_dates=get_days_n_dates($date_start,$end_date);
+$days=$no_days_dates[0];
+$date_array=$no_days_dates[1];
 $user_id=$current_user->id;
 
-
+	$query ="select crmentity.crmid,crmentity.createdtime, leaddetails.*, crmentity.smownerid, leadscf.* from leaddetails inner join crmentity on crmentity.crmid=leaddetails.leadid inner join leadsubdetails on leadsubdetails.leadsubscriptionid=leaddetails.leadid inner join leadaddress on leadaddress.leadaddressid=leadsubdetails.leadsubscriptionid inner join leadscf on leaddetails.leadid = leadscf.leadid left join leadgrouprelation on leadscf.leadid=leadgrouprelation.leadid left join groups on groups.groupname=leadgrouprelation.groupname where crmentity.deleted=0 and leaddetails.converted=0";
 
 //function to get the Graph for Lead Source
-
 function leadStatus_chart($user_id,$date_start,$end_date)
 {
-	global $adb;
+	global $adb,$query;
 
 	$lead_status_qry="select leadstatus from leadstatus";
 	$status_result=$adb->query($lead_status_qry);
@@ -49,8 +50,6 @@ function leadStatus_chart($user_id,$date_start,$end_date)
 		else
 			$status_name="$name";
 
-		//array_push($aTargets[$stage], "index.php?module=Potentials&action=ListView&date_closed=$month&sales_stage=".urlencode($stage)."&query=true");
-
 		$link_val="index.php?module=Leads&action=index&search_text=$name&search_field=leadstatus&searchtype=BasicSearch&query=true";
 
 		if($target_val!="")
@@ -61,14 +60,7 @@ function leadStatus_chart($user_id,$date_start,$end_date)
 		{
 			$target_val="$link_val";
 		}
-
 	}
-
-
-
-	//$query = getListQuery("Leads");
-
-	$query ="select crmentity.crmid, leaddetails.leadstatus, crmentity.smownerid, leadscf.* from leaddetails inner join crmentity on crmentity.crmid=leaddetails.leadid inner join leadsubdetails on leadsubdetails.leadsubscriptionid=leaddetails.leadid inner join leadaddress on leadaddress.leadaddressid=leadsubdetails.leadsubscriptionid inner join leadscf on leaddetails.leadid = leadscf.leadid left join leadgrouprelation on leadscf.leadid=leadgrouprelation.leadid left join groups on groups.groupname=leadgrouprelation.groupname where crmentity.deleted=0 and leaddetails.converted=0";
 
 	$where= " and crmentity.smownerid=".$user_id." and crmentity.modifiedtime between '%".$date_start."%' and '%".$end_date."%'" ;
 	$query.=$where;
@@ -78,6 +70,9 @@ function leadStatus_chart($user_id,$date_start,$end_date)
 	$lead_aray=$adb->fetch_array($result);
 
 	$status_count_array[]=array();
+
+	$lead_status_table="<table border=0 cellspacing=1 cellpadding=3><tr>
+                                <th>Lead Status</th><th>Total</th></tr>";
 
 	if($no_of_rows!=0)
 	{
@@ -102,33 +97,22 @@ function leadStatus_chart($user_id,$date_start,$end_date)
 			$status_val="$status_val,$val";
 		else
 			$status_val="$val";
+		 $lead_status_table.="<tr><td>$status</td><td>$val</td></tr>";
 
 		$total +=$val;
 	}
-
+	 $lead_status_table.="<tr><td>Total</td><td>$total </td></tr>
+				</table>";
 
 	$title_of_graph ="Leads total by Status is $total";
 
-	echo <<< END
-
-		<table border="0" cellspacing="0" cellpadding="5" ><tr><td>
-		<img src="modules/Dashboard/horizontal_bargraph.php?refer_code=$status_name&referdata=$status_val&target=$target_val&width=350&height=400&top=20&left=110&title=$title_of_graph" border="0">
-
-		</td>
-		<td>
-                <img src="modules/Dashboard/pie_graph.php?refer_code=$status_name&referdata=$status_val&target=$target_val&width=650&height=300&title=$title_of_graph" border="0">
-		</td>
-		</tr>
-		
-		</table>
-END;
-
+	$Lead_status_val=array($status_name,$status_val,$title_of_graph,$target_val,$lead_status_table);
+        return $Lead_status_val;
 }
-
+//Function to get the Leads Details Based upon the Lead Source
 function leadSource_chart($user_id,$date_start,$end_date)
 {
-	//global $user_id,$date_start,$end_date,$adb;
-	global $adb;
+	global $adb,$query;
 	
 	$lead_source_qry="select leadsource from leadsource";
         $source_result=$adb->query($lead_source_qry);
@@ -145,8 +129,6 @@ function leadSource_chart($user_id,$date_start,$end_date)
                 else
                         $source_name="$name";
 
-                //array_push($aTargets[$stage], "index.php?module=Potentials&action=ListView&date_closed=$month&sales_stage=".urlencode($stage)."&query=true");
-
                 $link_val="index.php?module=Leads&action=index&search_text=$name&search_field=leadsource&searchtype=BasicSearch&query=true";
 
                 if($target_val!="")
@@ -157,10 +139,7 @@ function leadSource_chart($user_id,$date_start,$end_date)
                 {
                         $target_val="$link_val";
                 }
-
         }
-	
-	 $query ="select crmentity.crmid, leaddetails.leadsource, crmentity.smownerid, leadscf.* from leaddetails inner join crmentity on crmentity.crmid=leaddetails.leadid inner join leadsubdetails on leadsubdetails.leadsubscriptionid=leaddetails.leadid inner join leadaddress on leadaddress.leadaddressid=leadsubdetails.leadsubscriptionid inner join leadscf on leaddetails.leadid = leadscf.leadid left join leadgrouprelation on leadscf.leadid=leadgrouprelation.leadid left join groups on groups.groupname=leadgrouprelation.groupname where crmentity.deleted=0 and leaddetails.converted=0";
 
         $where= " and crmentity.smownerid=".$user_id." and crmentity.modifiedtime between '%".$date_start."%' and '%".$end_date."%'" ;
         $query.=$where;
@@ -180,6 +159,9 @@ function leadSource_chart($user_id,$date_start,$end_date)
                 }
         }
         $total=0;
+
+	$lead_source_table="<table border=0 cellspacing=1 cellpadding=3><tr>
+                                <th>Lead Status</th><th>Total</th></tr>";
         for($i=0;$i<count($source_array);$i++)
         {
                 $source=$source_array[$i];
@@ -195,35 +177,248 @@ function leadSource_chart($user_id,$date_start,$end_date)
                 else
                         $source_val="$val";
 
+		$lead_source_table.="<tr><td>$source</td><td>$val</td></tr>";
                 $total +=$val;
         }
+	$lead_source_table.="<tr><td>Total</td><td>$total </td></tr>
+                                </table>";
 
 
         $title_of_graph ="Leads total by Source is $total";
 
-        echo <<< END
-
-                <table border="0" cellspacing="0" cellpadding="5" ><tr>
-		<td>
-		<img src="modules/Dashboard/horizontal_bargraph.php?refer_code=$source_name&referdata=$source_val&target=$target_val&width=350&height=400&top=20&left=120&title=$title_of_graph" border="0">
-
-		
-                </td>
-		<td>
-                <img src="modules/Dashboard/pie_graph.php?refer_code=$source_name&referdata=$source_val&target=$target_val&width=630&height=300&title=$title_of_graph" border="0">
-
-		</td>
-		</tr>
-                </table>
-END;
-
-
-
+        $Lead_source_val=array($source_name,$source_val,$title_of_graph,$target_val,$lead_source_table);
+        return $Lead_source_val;
 }
 
 
-leadStatus_chart($user_id,$date_start,$end_date);
-leadSource_chart($user_id,$date_start,$end_date);
+function lead_by_industry($user_id,$date_start,$end_date)
+{
+	
+	global $adb,$query;
+	global $days,$date_array,$period_type;
+	$insdustry_query="select * from industry";
+
+        $where= " and crmentity.smownerid=".$user_id." and crmentity.modifiedtime between '%".$date_start."%'
+and '%".$end_date."%'" ;
+        $query.=$where;
+        $result=$adb->query($query);
+        $no_of_rows=$adb->num_rows($result);
+        $industry_count_array=array();
+	$industry_name_array=array();
+	$count_by_date[]=array();
+	$industry_tot_cnt_array=array();
+
+	$industry_name_val="";
+	$industry_cnt_crtd_date="";
+
+        if($no_of_rows!=0)
+        {
+                while($row = $adb->fetch_array($result))
+                {
+			$industry_name= $row['industry'];
+			if($industry_name=="")
+				$industry_name="Un Assigned";
+
+			$crtd_time=$row['createdtime'];
+			$crtd_time_array=explode(" ",$crtd_time);
+			$crtd_date=$crtd_time_array[0];	
+	
+			if(!isset($industry_tot_cnt_array[$crtd_date]))
+				$industry_tot_cnt_array[$crtd_date]=0;
+
+			$industry_tot_cnt_array[$crtd_date]+=1;
+
+			if (in_array($industry_name,$industry_name_array) == false)
+			{
+				array_push($industry_name_array,$industry_name); // getting all the unique industry into the array
+			}
+			if(!isset($industry_count_array[$industry_name]))
+				$industry_count_array[$industry_name]=0;
+			$industry_count_array[$industry_name]++;
+
+			if(!isset($count_by_date[$industry_name][$crtd_date]))
+
+				$count_by_date[$industry_name][$crtd_date]=0;
+
+			$count_by_date[$industry_name][$crtd_date]+=1;
+                }
+		$lead_by_industry_cnt=count($industry_name_array);
+		
+		if($lead_by_industry_cnt!=0)
+		{		
+			$url_string="";
+		
+			$industry_cnt_table="<table border=0 cellspacing=1 cellpadding=3><tr>
+                                <th>Ticket Status</th>";
+							
+			//Assigning the Header values to the Graph
+			for($i=0; $i<$days; $i++)
+			{
+				$tdate=$date_array[$j];
+				$values=Graph_n_table_format($period_type,$tdate);	
+				$table_format=$values[1];
+				$industry_cnt_table.= "<th>$table_format</th>";
+			}
+			$industry_cnt_table .= "<th>Total</th></tr>" ;				
+	
+			for ($i=0;$i<count($industry_name_array); $i++)
+			{
+				$industry=$industry_name_array[$i];
+				if($industry=="")
+					$industry="Un Assigned";
+			
+				$industry_cnt_table .= "<tr><td>$industry</td>";
+
+				$industry_cnt_crtd_date="";
+				for($j=0;$j<$days;$j++)
+				{
+					$tdate=$date_array[$j];
+
+					if (!isset($count_by_date[$industry][$tdate]))
+					{
+						$count_by_date[$industry][$tdate]="0"; 
+					}
+					$cnt_by_date=$count_by_date[$industry][$tdate];
+					$industry_cnt_table .= "<td>$cnt_by_date </td>";	
+
+					$values=Graph_n_table_format($period_type,$tdate);
+					$graph_format=$values[0];
+					$table_format=$values[1];
+
+					//passing the created dates to graph
+					if($industry_graph_date!="")
+						$industry_graph_date="$industry_graph_date,$graph_format";
+					else
+						$industry_graph_date="$graph_format";
+			
+					//passing the industry count by date to graph
+					if($industry_cnt_crtd_date!="")
+						$industry_cnt_crtd_date.=",$cnt_by_date";
+					else
+						$industry_cnt_crtd_date="$cnt_by_date";
+				}
+
+				$industry_count_val=$industry_count_array[$industry];
+
+				$tot_industry_cnt=array_sum($count_by_date[$industry]);
+				 $industry_cnt_table .= "<td align=center>$tot_industry_cnt</td></tr>";
+
+				//Passing industry name to graph
+				if($industry_name_val!="") $industry_name_val.=",$industry";
+				else $industry_name_val="$industry";
+				//Passing count to graph
+				if($industry_cnt_val!="") $industry_cnt_val.=",$industry_count_val";
+				else $industry_cnt_val="$industry_count_val";	
+
+				if($i==0)
+					$urlstring .=$industry_cnt_crtd_date;
+				else
+					$urlstring .="K".$industry_cnt_crtd_date;
+			}
+			 $industry_cnt_table .="</tr><tr><td class=\"$class\">Total</td>";
+			for($k=0; $k<$days;$k++)
+			{
+				$tdate=$date_array[$k];
+				if(!isset($industry_tot_cnt_array[$tdate]))
+					$industry_tot_cnt_array[$tdate]="0";
+				$tot= $industry_tot_cnt_array[$tdate];
+				if($period_type!="yday")
+					$industry_cnt_table.="<td>$tot</td>";
+			}
+			$cnt_total=array_sum($industry_tot_cnt_array);
+			$industry_cnt_table.="<td align=\"center\" class=\"$class\">$cnt_total</td></tr></table>";
+			$industry_cnt_table.="</table>";
+			$title_of_graph=" Leads by industry : $cnt_total";
+			$x_title="Industry";
+			$y_title="Count";
+
+			$Lead_industry_val=array($industry_name_val,$industry_cnt_val,$title_of_graph,$target_val,$industry_graph_date,$urlstring,$industry_cnt_table);
+  		      	return $Lead_industry_val;
+		}
+		else
+		{
+			$data=0;
+		}
+        }
+	else
+	{
+		$data=0;
+	}
+	return $data;
+}
+//Lead Status Values
+$leadStatus_values=leadStatus_chart($user_id,$date_start,$end_date);
+$status_name=$leadStatus_values[0];
+$status_val=$leadStatus_values[1];
+$status_graph_title=$leadStatus_values[2];
+$status_target_val=$leadStatus_values[3];
+$lead_status_table=$leadStatus_values[4];
+
+//Lead Source Values
+$leadSource_values=leadSource_chart($user_id,$date_start,$end_date);
+$source_name=$leadSource_values[0];
+$source_val=$leadSource_values[1];
+$source_graph_title=$leadSource_values[2];
+$source_target_val=$leadSource_values[3];
+$lead_source_table=$leadSource_values[4];
+
+//Lead Industry Values
+$leadIndustry_values=lead_by_industry($user_id,$date_start,$end_date);
+if($leadIndustry_values!=0)
+{
+	$industry_name_val=$leadIndustry_values[0];
+	$industry_cnt_val=$leadIndustry_values[1];
+	$industry_graph_title=$leadIndustry_values[2];
+	$indus_target_val=$leadIndustry_values[3];	
+	$industry_graph_date=$leadIndustry_values[4];
+	$urlstring=$leadIndustry_values[5];
+	$industry_cnt_table=$leadIndustry_values[6];
+}
+	echo <<< END
+		<table border="0" cellspacing="0" cellpadding="5" ><tr><td>
+	   <img src="modules/Dashboard/horizontal_bargraph.php?refer_code=$status_name&referdata=$status_val&target=$status_target_val&width=350&height=400&top=20&left=110&title=$status_graph_title" border="0">
+        </td><td>
+	   <img src="modules/Dashboard/pie_graph.php?refer_code=$status_name&referdata=$status_val&target=$target_val&width=650&height=300&title=$status_graph_title" border="0">
+        </td></tr>
+        <tr><td>
+        	$lead_status_table
+      	</td></tr>
+	<tr>
+	<td>
+       	<img src="modules/Dashboard/horizontal_bargraph.php?refer_code=$source_name&referdata=$source_val&target=$source_target_val&width=350&height=400&top=20&left=120&title=$source_graph_title" border="0">
+        </td><td>
+          <img src="modules/Dashboard/pie_graph.php?refer_code=$source_name&referdata=$source_val&width=630&height=300&title=$source_graph_title" border="0">
+        </td></tr>
+	<tr><td>
+		$lead_source_table
+	</td></tr>
+
+END;
+
+	if($leadIndustry_values!=0)
+	{
+	echo <<< END
+
+	<tr><td>
+		
+	        <img src="modules/Dashboard/line_graph.php?refer_code=$industry_graph_date&referdata=$industry_name_val&width=530&height=300&datavalue=$urlstring&title=$industry_graph_title" border="0">
+	</td><td>
+	<img src="modules/Dashboard/pie_graph.php?refer_code=$industry_name_val&referdata=$industry_cnt_val&target=$indus_target_val&width=530&height=300&title=$industry_graph_title" border="0">
+	</td></tr>
+	<tr><td>
+		
+        <img src="modules/Dashboard/horizontal_bargraph.php?refer_code=$industry_name_val&referdata=$industry_cnt_val&width=350&height=400&top=20&left=110&title=$industry_graph_title" border="0">
+	</td><td>
+		
+        <img src="modules/Dashboard/vertical_bargraph.php?refer_code=$industry_name_val&referdata=$industry_cnt_val&width=450&height=300&top=20&left=110&title=$industry_graph_title" border="0">
+	</td></tr>
+	<tr><td>
+	$industry_cnt_table
+	</td></tr>
+        </table>
+END;
+
+	}
 
 
 ?>
