f9c038059b70f83e9a4ba6f5d3eff54931ba4be9
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-03 05:27:33 +0000

    changes made to support multiple currency

--- modules/Settings/SaveCurrencyInfo.php
+++ modules/Settings/SaveCurrencyInfo.php
@@ -11,18 +11,31 @@
 
 require_once('include/database/PearDatabase.php');
 global $adb;
-
+$db = new PearDatabase();
 $currency_name = $_REQUEST['currency_name'];
 $currency_code= $_REQUEST['currency_code'];
 $currency_symbol= $_REQUEST['currency_symbol'];
+$conversion_rate= $_REQUEST['conversion_rate'];
+$currency_status= $_REQUEST['currency_status'];
 
 
-$sql1 = "delete from currency_info";
-$adb->query($sql1);
+//$sql1 = "delete from currency_info";
+//$adb->query($sql1);
 
-$sql2 = "insert into currency_info values('".$currency_name."','".$currency_code."','".$currency_symbol."')";
-$adb->query($sql2);
-
-$loc = "Location: index.php?module=Settings&action=index";
+//$sql2 = "insert into currency_info values('".$currency_name."','".$currency_code."','".$currency_symbol."')";
+//$adb->query($sql2);
+if(isset($_REQUEST['record']) && $_REQUEST['record']!='')
+{
+        $sql = "update currency_info set currency_name ='".$currency_name."', currency_code ='".$currency_code
+."', currency_symbol ='".$currency_symbol."', conversion_rate ='".$conversion_rate."',currency_status='".$currency_status."' where id =".$_REQUEST['record'];
+}
+else
+{
+        $sql = "insert into currency_info values(".$db->getUniqueID("currency_info").",'".$currency_name."','"
+.$currency_code."','".$currency_symbol."','".$conversion_rate."','".$currency_status."',0)";
+}
+//echo $sql;
+$adb->query($sql);
+$loc = "Location: index.php?module=Settings&action=CurrencyListView";
 header($loc);
 ?>
