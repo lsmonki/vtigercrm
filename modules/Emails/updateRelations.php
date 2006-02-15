f13f67bb1d357fb088cf0e9bae628b226d3d0cb4
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-15 12:12:26 +0000

    Added multiple contacts addition functionality

--- modules/Emails/updateRelations.php
+++ modules/Emails/updateRelations.php
@@ -11,21 +11,44 @@
 
 require_once('include/database/PearDatabase.php');
 global $adb;
+$idlist = $_REQUEST['idlist'];
+
+//echo '<pre>'; print_r($_REQUEST['entityid']); echo '</pre>';
+
+if(isset($_REQUEST['idlist']) && $_REQUEST['idlist'] != '')
+{
+	//split the strings & store in an array
+	$storearray = explode (";",$idlist);
+	foreach($storearray as $id)
+	{
+		if($id != '')
+		{
+			$record = $_REQUEST["parentid"];
+			$sql = "insert into seactivityrel values (". PearDatabase::quote($id).",".PearDatabase::quote($_REQUEST["parentid"]) .")";
+			$adb->query($sql);
+		}
+	}
+	header("Location: index.php?action=CallRelatedList&module=Emails&record=".$record);
+}
+elseif (isset($_REQUEST['entityid']) && $_REQUEST['entityid'] != '')
+{
+	$record = $_REQUEST["parid"];
+	//$sql = "insert into seactivityrel values (". $_REQUEST["entityid"] .",".$_REQUEST["parid"] .")";
+	$sql = "insert into seactivityrel values (". PearDatabase::quote($_REQUEST["entityid"]).",".PearDatabase::quote($_REQUEST["parid"]) .")";
+	$adb->query($sql);
+	header("Location: index.php?action=CallRelatedList&module=Emails&record=".$record);
+}
+
+
 
 if(isset($_REQUEST['user_id']) && $_REQUEST['user_id'] != '')
 {
 	$record = $_REQUEST['record'];
 	//$sql = "insert into salesmanactivityrel values (". $_REQUEST["user_id"] .",".$_REQUEST["record"] .")";
 	$sql = "insert into salesmanactivityrel values (".PearDatabase::quote($_REQUEST["user_id"]).",".PearDatabase::quote($_REQUEST["record"]).")";
-}
-else
-{
-	$record = $_REQUEST["parid"];
-	//$sql = "insert into seactivityrel values (". $_REQUEST["entityid"] .",".$_REQUEST["parid"] .")";
-	$sql = "insert into seactivityrel values (". PearDatabase::quote($_REQUEST["entityid"]).",".PearDatabase::quote($_REQUEST["parid"]) .")";
+	$adb->query($sql);
+	header("Location: index.php?action=CallRelatedList&module=Emails&record=".$record);
 }
 
-$adb->query($sql);
-header("Location: index.php?action=CallRelatedList&module=Emails&record=".$record);
 
 ?>
