5eaa782ad8548f8150b82e08dcea93a288a330bc
mfedyk <mfedyk@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-19 10:09:36 +0000

    llow more lead columns to be sorted

--- modules/Leads/Lead.php
+++ modules/Leads/Lead.php
@@ -92,7 +92,7 @@ class Lead extends CRMEntity {
 	//construct this from database;	
 	var $column_fields = Array();
 
-        var $sortby_fields = Array('lastname','email','phone');		  
+        var $sortby_fields = Array('lastname','email','phone','fax','company','leadstatus','modifiedtime','createdtime');
        
 	var $combofieldNames = Array('leadsource'=>'leadsource_dom'
                       ,'salutation'=>'salutation_dom'
