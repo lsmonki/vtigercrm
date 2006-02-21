dca91c07df35e9ecadb26e2e011105b8b3f8d62a
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-21 10:31:45 +0000

    Added 2 if else block to separate Create New from Edit View.

--- modules/HelpDesk/EditView.php
+++ modules/HelpDesk/EditView.php
@@ -42,7 +42,13 @@ $image_path=$theme_path."images/";
 require_once($theme_path.'layout_utils.php');
 
 $disp_view = getView($focus->mode);
-$smarty->assign("BLOCKS",getBlocks("HelpDesk",$disp_view,$mode,$focus->column_fields));
+if($disp_view == 'edit_view')
+	$smarty->assign("BLOCKS",getBlocks("HelpDesk",$disp_view,$mode,$focus->column_fields));
+else	
+{
+	$smarty->assign("BASBLOCKS",getBlocks("HelpDesk",$disp_view,$mode,$focus->column_fields,'BAS'));
+}
+
 $smarty->assign("OP_MODE",$disp_view);
 
 $smarty->assign("MODULE",$currentModule);
@@ -134,6 +140,9 @@ $smarty->assign("VALIDATION_DATA_FIELDNAME",$fieldName);
 $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$fldDataType);
 $smarty->assign("VALIDATION_DATA_FIELDLABEL",$fieldLabel);
 
-$smarty->display("salesEditView.tpl");
+if($focus->mode == 'edit')
+	$smarty->display("salesEditView.tpl");
+else
+	$smarty->display("CreateView.tpl");
 
 ?>
