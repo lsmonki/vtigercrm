194da31547ef50f966b79266fd2b795f82fe2a16
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-21 10:41:46 +0000

    Added 2 if else block to separate Create New from Edit View.

--- modules/Products/EditView.php
+++ modules/Products/EditView.php
@@ -79,8 +79,12 @@ $image_path=$theme_path."images/";
 require_once($theme_path.'layout_utils.php');
 
 $disp_view = getView($focus->mode);
-$smarty->assign("BLOCKS",getBlocks("Products",$disp_view,$mode,$focus->column_fields));
-//echo '<pre>';print_r(getBlocks("Products",$disp_view,$mode,$focus->column_fields));echo '</pre>';
+if($disp_view == 'edit_view')
+	$smarty->assign("BLOCKS",getBlocks("Products",$disp_view,$mode,$focus->column_fields));
+else	
+{
+	$smarty->assign("BASBLOCKS",getBlocks("Products",$disp_view,$mode,$focus->column_fields,'BAS'));
+}
 $smarty->assign("OP_MODE",$disp_view);
 
 $smarty->assign("MODULE",$currentModule);
@@ -195,5 +199,8 @@ $smarty->assign("VALIDATION_DATA_FIELDNAME",$fieldName);
 $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$fldDataType);
 $smarty->assign("VALIDATION_DATA_FIELDLABEL",$fieldLabel);
 
+if($focus->mode == 'edit')
 $smarty->display('salesEditView.tpl');
+else
+$smarty->display('CreateView.tpl');
 ?>
