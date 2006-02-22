abad7c9df536a7220049beed0b8894465881b9ea
mfedyk <mfedyk@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-22 03:18:35 +0000

    Fix activities relatedlist won't display well if translated, by Kleo. fixes #27

--- include/utils.php
+++ include/utils.php
@@ -2720,7 +2720,7 @@ function getListViewHeader($focus, $module,$sort_qry='',$sorder='',$order_by='',
 				}
 			}
 			//Added condition to hide the close column in Related Lists
-			if($name == 'Close' && $relatedlist != '')
+			if($name == $app_strings['Close'] && $relatedlist != '')
 			{
 				$list_header .= '';
 			}
