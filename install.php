f6775e23b5c17ef2225c60f1224ef75fe70ada0c
mfedyk <mfedyk@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-02-27 04:57:27 +0000

    create install/5createTables.inc.php and modify install.php to use it when called from the command line.  see #28

--- install.php
+++ install.php
@@ -34,6 +34,12 @@ if(get_magic_quotes_gpc() == 1) {
   $_GET = array_map("stripslashes_checkstrings", $_GET);
 }
 
+//Run command line if no web var detected
+if (!isset($_SERVER['REQUEST_METHOD'])) {
+	require("install/5createTables.inc.php");
+	exit;
+}
+
 if (isset($_POST['file']))
   $the_file = $_POST['file'];
 else
