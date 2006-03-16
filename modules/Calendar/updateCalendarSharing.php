fb7d951e728fee0543a8fc12896dd3a0902d799d
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-16 07:35:06 +0000

    updateCalendarSharing.php has been added to update the calendar sharing

---
+++ modules/Calendar/updateCalendarSharing.php
@@ -0,0 +1,35 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+require_once('include/database/PearDatabase.php');
+global $adb;
+$sharedid = $_REQUEST['sharedid'];
+if(isset($_REQUEST['sharedid']) && $_REQUEST['sharedid'] != '')
+{
+        //split the string and store in an array
+        $storearray = explode (";",$sharedid);
+        foreach($storearray as $sid)
+        {
+                if($sid != '')
+                {
+			$check_query = "select * from sharedcalendar where userid=".$_REQUEST["current_userid"]." and sharedid=".$sid;
+			$result = $adb->query($check_query);
+			if($adb->num_rows($result) == 0)
+			{
+				$sql = "insert into sharedcalendar values (".$_REQUEST["current_userid"].",".$sid.")";
+			        $adb->query($sql);
+			}
+                }
+        }
+        header("Location: index.php?action=calendar_share&module=Calendar");
+}
+
+?>
+
