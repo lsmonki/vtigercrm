349d086a453d4a6b32c3c2ab63c3215fc07c5c77
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-16 11:59:22 +0000

    Modified the file for smartification of create new Email Template

--- modules/Users/createemailtemplate.php
+++ modules/Users/createemailtemplate.php
@@ -9,27 +9,35 @@
 *
  ********************************************************************************/
 require_once('database/DatabaseConnection.php');
-require_once('XTemplate/xtpl.php');
+require_once('Smarty_setup.php');
 require_once('include/utils/utils.php');
-
-
+require_once('data/Tracker.php');
+require_once('include/utils/UserInfoUtil.php');
+require_once('include/database/PearDatabase.php');
 
 global $app_strings;
 global $app_list_strings;
 global $mod_strings;
 global $current_user;
+global $current_language;
 
 global $theme;
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
+$smod_strings = return_module_language($current_language,'Settings');
 require_once($theme_path.'layout_utils.php');
 
-$xtpl=new XTemplate ('modules/Users/createemailtemplate.html');
-$xtpl->assign("MOD", $mod_strings);
-$xtpl->assign("APP", $app_strings);
-$xtpl->parse("main");
-$xtpl->out("main");
+$smarty = new vtigerCRM_smarty;
 
+$smarty->assign("APP", $app_strings);
+$smarty->assign("IMAGE_PATH", $image_path);
+$smarty->assign("THEME_PATH", $theme_path);
+$smarty->assign("UMOD", $mod_strings);
+
+$smarty->assign("MOD", $smod_strings);
+$smarty->assign("MODULE", 'Settings');
+
+$smarty->display("CreateEmailTemplate.tpl");
 
 ?>
 
