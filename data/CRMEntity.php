2526ebe32058f858921bb3ae7be46e30cf888825
mfedyk <mfedyk@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-23 02:59:39 +0000

    Fix restricted user modifying record erases hidden fields, by mcrowe.  closes #63

--- data/CRMEntity.php
+++ data/CRMEntity.php
@@ -439,7 +439,18 @@ $vtlog->logthis("module is =".$module,'info');
 	  }
 
 	  $tabid= getTabid($module);	
-	  $sql = "select * from field where tabid=".$tabid." and tablename='".$table_name."' and displaytype in (1,3)"; 
+	  // MWC -- Fix hidden field saving issue
+	  // $sql = "select * from field where tabid=" . $tabid . " and tablename='" . $table_name . "' and displaytype in (1,3)";
+	  $sql = "select * from field 
+		      inner join profile2field on profile2field.fieldid=field.fieldid 
+		      inner join def_org_field on def_org_field.fieldid=field.fieldid  
+		  where field.tabid=" . $tabid . " 
+		      and field.tablename='" . $table_name . "' 
+		      and field.displaytype in (1,3)
+		      and profile2field.visible=0 
+		      and def_org_field.visible=0 
+		      and profile2field.profileid=" . $profile_id . " 
+		  order by sequence";
 	  $result = $adb->query($sql);
 	  $noofrows = $adb->num_rows($result);
 	  for($i=0; $i<$noofrows; $i++)
@@ -918,7 +929,16 @@ $vtlog->logthis("type is ".$type,'debug');
     }
 
     $tabid = getTabid($module);
-    $sql1 =  "select * from field where tabid=".$tabid;
+    // MWC -- Fix hidden field saving issue
+    // $sql1 = "select * from field where tabid=" . $tabid;
+    $sql1 = "select * from field 
+                inner join profile2field on profile2field.fieldid=field.fieldid 
+                inner join def_org_field on def_org_field.fieldid=field.fieldid  
+            where field.tabid=" . $tabid . " 
+                and profile2field.visible=0 
+                and def_org_field.visible=0 
+                and profile2field.profileid=" . $profile_id . " 
+            order by sequence";
     $result1 = $adb->query($sql1);
     $noofrows = $adb->num_rows($result1);
     for($i=0; $i<$noofrows; $i++)
