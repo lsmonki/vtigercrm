410671aebb59ddc94fc8d7e0ac72b4f7b1cc9aae
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-28 07:03:04 +0000

    junk code has been removed

--- modules/Vendors/index.php
+++ modules/Vendors/index.php
@@ -16,8 +16,6 @@ global $theme;
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
 require_once ($theme_path."layout_utils.php");
-global $mod_strings;
-
 include ('modules/Vendors/ListView.php');
 
 ?>
