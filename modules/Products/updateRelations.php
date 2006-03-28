878a0b697b2325122b89119199946b184ee11a3a
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-28 08:27:47 +0000

    junk code has been removed

--- modules/Products/updateRelations.php
+++ modules/Products/updateRelations.php
@@ -12,14 +12,10 @@
 require_once('include/database/PearDatabase.php');
 global $adb;
 
-//if($_REQUEST['module']=='Users')
-//	$sql = "insert into salesmanactivityrel values (". $_REQUEST["entityid"] .",".$_REQUEST["parid"] .")";
-//else
 if($_REQUEST['destination_module']=='Products')
 {
 	if($_REQUEST['smodule']=='VENDOR')
 	{
-		#include('modules/Products/SaveVendor.php');
 		$sql = "update products set vendor_id=".$_REQUEST['parid']." where productid=".$_REQUEST['entityid'];
 		$adb->query($sql);
 	}
@@ -34,8 +30,6 @@ if($_REQUEST['destination_module']=='Contacts')
 }
 
 
-	#$sql = "insert into seproductsrel values (". $_REQUEST["parid"] .",".$_REQUEST["entityid"] .")";
-#$adb->query($sql);
  header("Location:index.php?action=DetailView&module=Vendors&record=".$_REQUEST["parid"]);
 
 
