a5fed59ace8533ff354bc6e995d72704dbe14dd0
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-28 08:16:43 +0000

    junk code has been removed

--- modules/Products/AddProductsToPriceBook.php
+++ modules/Products/AddProductsToPriceBook.php
@@ -15,17 +15,9 @@ require_once('include/utils/utils.php');
 require_once('include/utils/utils.php');
 require_once('include/ComboUtil.php');
 
-global $app_strings;
-global $mod_strings;
-global $current_language;
+global $app_strings,$mod_strings,$current_language,$theme,$log;
 $current_module_strings = return_module_language($current_language, 'Products');
 
-global $list_max_entries_per_page;
-global $urlPrefix;
-
-
-global $theme;
-global $log;
 $pricebook_id = $_REQUEST['pricebook_id'];
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
@@ -33,7 +25,6 @@ require_once($theme_path.'layout_utils.php');
 $pricebookname = getPriceBookName($pricebook_id);
 echo get_module_title($current_module_strings['LBL_MODULE_NAME'], $current_module_strings['LBL_ADD_PRODUCTS_PRICEBOOK'].": ".$pricebookname, true);
 echo "<br>";
-//echo get_form_header("Product Search", "", false);
 
 $xtpl=new XTemplate ('modules/Products/AddProductsToPriceBook.html');
 $xtpl->assign("MOD", $mod_strings);
@@ -68,8 +59,6 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
         if (isset($_REQUEST['purchase_date'])) $purchase_date = $_REQUEST['purchase_date'];
 
 	$where_clauses = Array();
-	//$search_query='';
-
 	//Added for Custom Field Search
 	$sql="select * from field where tablename='productcf' order by fieldlabel";
 	$result=$adb->query($sql);
@@ -88,7 +77,6 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
                         else
 			        $str=" productcf.".$column[$i]." like '$customfield[$i]%'";
 		        array_push($where_clauses, $str);
-	       	//	  $search_query .= ' and '.$str;
 			$url_string .="&".$column[$i]."=".$customfield[$i];
 	        }
 	}
@@ -97,65 +85,55 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
 	if (isset($productname) && $productname !='')
 	{
 		array_push($where_clauses, "productname like ".PearDatabase::quote($productname.'%'));
-		//$search_query .= " and productname like '".$productname."%'";
 		$url_string .= "&productname=".$productname;
 	}
 	
 	if (isset($productcode) && $productcode !='')
 	{
 		array_push($where_clauses, "productcode like ".PearDatabase::quote($productcode.'%'));
-		//$search_query .= " and productcode like '".$productcode."%'";
 		$url_string .= "&productcode=".$productcode;
 	}
 
 	if (isset($commissionrate) && $commissionrate !='')
 	{
 		array_push($where_clauses, "commissionrate like ".PearDatabase::quote($commissionrate.'%'));
-		 //$search_query .= " and commissionrate like '".$commissionrate."%'";
 		 $url_string .= "&commissionrate=".$commissionrate;
 	}
 	
 	if (isset($qtyperunit) && $qtyperunit !='')
 	{
 		array_push($where_clauses, "qty_per_unit like ".PearDatabase::quote($qtyperunit.'%'));
-	 	//$search_query .= " and qty_per_unit like '".$qtyperunit."%'";
 		$url_string .= "&qtyperunit=".$qtyperunit;
 	}
 	
 	if (isset($unitprice) && $unitprice !='')
 	{
 		array_push($where_clauses, "unit_price like ".PearDatabase::quote($unitprice.'%'));
-	 //	$search_query .= " and unit_price like '".$unitprice."%'";
 		$url_string .= "&unitprice=".$unitprice;
 	}
 	if (isset($manufacturer) && $manufacturer !='' && $manufacturer !='--None--')
         {
 		array_push($where_clauses, "manufacturer like ".PearDatabase::quote($manufacturer.'%'));
-        	//$search_query .= " and manufacturer like '".$manufacturer."%'";
                 $url_string .= "&manufacturer=".$manufacturer;
 	}
 	if (isset($productcategory) && $productcategory !='' && $productcategory !='--None--')
         {
 		array_push($where_clauses, "productcategory like ".PearDatabase::quote($productcategory.'%'));
-        	//$search_query .= " and productcategory like '".$productcategory."%'";
                 $url_string .= "&productcategory=".$productcategory;
 	}
 	if (isset($start_date) && $start_date !='')
         {
 		array_push($where_clauses, "start_date like ".PearDatabase::quote($start_date.'%'));
-                //$search_query .= " and start_date = '".$start_date."%'";
                 $url_string .= "&start_date=".$start_date;
         } 
 	if (isset($expiry_date) && $expiry_date !='')
         {
 		array_push($where_clauses, "expiry_date like ".PearDatabase::quote($expiry_date.'%'));
-                //$search_query .= " and expiry_date = '".$expiry_date."%'";
                 $url_string .= "&expiry_date=".$expiry_date;
         } 
 	if (isset($purchase_date) && $purchase_date !='')
         {
 		array_push($where_clauses, "purchase_date like ".PearDatabase::quote($purchase_date.'%'));
-                //$search_query .= " and purchase_date = '".$purchase_date."%'";
                 $url_string .= "&purchase_date=".$purchase_date;
         }
 	$where = "";
@@ -168,84 +146,6 @@ if(isset($_REQUEST['query']) && $_REQUEST['query'] != '' && $_REQUEST['query'] =
 
 	$log->info("Here is the where clause for the list view: $where");
  
-
-}
-
-//Constructing the Search Form
-if (!isset($_REQUEST['search_form']) || $_REQUEST['search_form'] != 'false') {
-        // Stick the form header out there.
-	echo get_form_header($current_module_strings['LBL_SEARCH_FORM_TITLE'],'', false);
-        $search_form=new XTemplate ('modules/Products/AddProductsSearchForm.html');
-        $search_form->assign("MOD", $mod_strings);
-        $search_form->assign("APP", $app_strings);
-        $search_form->assign("PRICEBOOKID", $pricebook_id);
-	$clearsearch = 'true';
-	
-	if ($order_by !='') $search_form->assign("ORDER_BY", $order_by);
-	if ($sorder !='') $search_form->assign("SORDER", $sorder);
-	$search_form->assign("JAVASCRIPT", get_clear_form_js());
-	if($order_by != '') {
-		$ordby = "&order_by=".$order_by;
-	}
-	else
-	{
-		$ordby ='';
-	}
-	$search_form->assign("BASIC_LINK", "index.php?module=Products".$ordby."&action=AddProductsToPriceBook".$url_string."&sorder=".$sorder."&pricebook_id=".$pricebook_id);
-	$search_form->assign("ADVANCE_LINK", "index.php?module=Products&action=AddProductsToPriceBook".$ordby."&advanced=true".$url_string."&sorder=".$sorder."&pricebook_id=".$pricebook_id);
-
-	if ($productname !='') $search_form->assign("PRODUCT_NAME", $productname);
-	if ($commissionrate !='') $search_form->assign("COMMISSION_RATE", $commissionrate);
-	if ($productcode !='') $search_form->assign("PRODUCT_CODE", $productcode);
-	if ($qtyperunit !='') $search_form->assign("QTYPERUNIT", $qtyperunit);
-	if ($unitprice !='') $search_form->assign("UNITPRICE", $unitprice);
-	if (isset($_REQUEST['manufacturer'])) $manufacturer = $_REQUEST['manufacturer'];	
-	if (isset($_REQUEST['productcategory'])) $productcategoty = $_REQUEST['productcategory'];	
-	if (isset($_REQUEST['start_date'])) $start_date = $_REQUEST['start_date'];	
-	if (isset($_REQUEST['expiry_date'])) $expiry_date = $_REQUEST['expiry_date'];	
-	if (isset($_REQUEST['purchase_date'])) $purchase_date = $_REQUEST['purchase_date'];	
-
-//Combo Fields for Manufacturer and Category are moved from advanced to Basic Search
-        if (isset($manufacturer)) $search_form->assign("MANUFACTURER", get_select_options($comboFieldArray['manufacturer_dom'], $manufacturer, $clearsearch));
-        else $search_form->assign("MANUFACTURER", get_select_options($comboFieldArray['manufacturer_dom'], '', $clearsearch));
-        if (isset($productcategory)) $search_form->assign("PRODUCTCATEGORY", get_select_options($comboFieldArray['productcategory_dom'], $productcategoty, $clearsearch));
-        else $search_form->assign("PRODUCTCATEGORY", get_select_options($comboFieldArray['productcategory_dom'], '', $clearsearch));
-
-        if (isset($_REQUEST['advanced']) && $_REQUEST['advanced'] == 'true') 
-	{
-		$url_string .="&advanced=true";
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','AddProductsToPriceBook','productname','true','advanced','','','','&pricebook_id='.$pricebook_id));
-
-		$search_form->assign("SUPPORT_START_DATE",$_REQUEST['start_date']);
-		$search_form->assign("SUPPORT_EXPIRY_DATE",$_REQUEST['expiry_date']);
-		$search_form->assign("PURCHASE_DATE",$_REQUEST['purchase_date']);
-		$search_form->assign("DATE_FORMAT", $current_user->date_format);
-
-		//Added for Custom Field Search
-		$sql="select * from field where tablename='productcf' order by fieldlabel";
-		$result=$adb->query($sql);
-		for($i=0;$i<$adb->num_rows($result);$i++)
-		{
-		        $column[$i]=$adb->query_result($result,$i,'columnname');
-		        $fieldlabel[$i]=$adb->query_result($result,$i,'fieldlabel');
-		        if (isset($_REQUEST[$column[$i]])) $customfield[$i] = $_REQUEST[$column[$i]];
-		}
-		require_once('include/CustomFieldUtil.php');
-		$custfld = CustomFieldSearch($customfield, "productcf", "productcf", "productid", $app_strings,$theme,$column,$fieldlabel);
-		$search_form->assign("CUSTOMFIELD", $custfld);
-		//upto this added for Custom Field
-
-                $search_form->parse("advanced");
-                $search_form->out("advanced");
-	}
-	else
-	{        
-		$search_form->assign("ALPHABETICAL",AlphabeticalSearch('Products','AddProductsToPriceBook','productname','true','basic','','','','&pricebook_id='.$pricebook_id));
-		$search_form->parse("main");
-	        $search_form->out("main");
-	}
-echo get_form_footer();
-//echo '<br><br>';
 
 }
 
@@ -275,7 +175,6 @@ if(isset($order_by) && $order_by != '')
 {
         $list_query .= ' ORDER BY '.$order_by.' '.$sorder;
 }
-//echo $list_query;
 $list_result = $adb->query($list_query);
 $num_rows = $adb->num_rows($list_result);
 
@@ -360,17 +259,12 @@ for($i=0; $i<$num_rows; $i++)
 	
 }
 
-
-//$listview_entries = getListViewEntries($focus,"Products",$list_result,$navigation_array);
-//$xtpl->assign("LISTENTITY", $listview_entries);
-
 if($order_by !='')
 $url_string .="&order_by=".$order_by;
 if($sorder !='')
 $url_string .="&sorder=".$sorder;
 
 $xtpl->assign("LISTENTITY", $list_body);
-//$xtpl->assign("RECORD_COUNTS", $record_string);
 
 $xtpl->parse("main");
 $xtpl->out("main");
