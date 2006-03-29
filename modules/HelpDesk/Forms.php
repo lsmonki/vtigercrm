ad8e310e9296d571e7295df2f5bf51f8c944754f
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-29 11:03:47 +0000

    Removed function trim() from Forms.php Ahmed

--- modules/HelpDesk/Forms.php
+++ modules/HelpDesk/Forms.php
@@ -45,20 +45,6 @@ function get_validate_record_js ()
 		<script type="text/javascript" language="Javascript">
 		<!--  to hide script contents from old browsers
 
-		function trim(s) 
-		{
-			while (s.substring(0,1) == " ") 
-			{
-				s = s.substring(1, s.length);
-			}
-			while (s.substring(s.length-1, s.length) == ' ') 
-			{
-				s = s.substring(0,s.length-1);
-			}
-
-			return s;
-		}
-
 		function verify_data(form) 
 		{
 			var isError = false;
