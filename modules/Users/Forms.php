dccb8821cb252e9fe48ddccf03ae0ff8e986e221
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-29 11:01:22 +0000

    Removed function trim() from Forms.php Ahmed

--- modules/Users/Forms.php
+++ modules/Users/Forms.php
@@ -45,17 +45,6 @@ $the_script  = <<<EOQ
 <script type="text/javascript" language="Javascript">
 <!--  to hide script contents from old browsers
 
-function trim(s) {
-	while (s.substring(0,1) == " ") {
-		s = s.substring(1, s.length);
-	}
-	while (s.substring(s.length-1, s.length) == ' ') {
-		s = s.substring(0,s.length-1);
-	}
-
-	return s;
-}
-
 function verify_data(form) {
 	var isError = false;
 	var errorMessage = "";
