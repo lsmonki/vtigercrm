69806ff7255634afeec7ff5cf14fac0843a73a15
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-29 11:21:48 +0000

    Removed function trim() from Forms.php Ahmed

--- modules/Reports/NewReport1.php
+++ modules/Reports/NewReport1.php
@@ -119,17 +119,6 @@ if(isset($_REQUEST["record"]))
         {
 	    document.location.href = "index.php?module=Reports&action=index";		
         }       
-	function trim(s) 
-	{
-		while (s.substring(0,1) == " ") {
-			s = s.substring(1, s.length);
-		}
-		while (s.substring(s.length-1, s.length) == ' ') {
-			s = s.substring(0,s.length-1);
-		}
-	
-		return s;
-	}
         function updateYAxisFields( colToTotalField, value, bool )
         {
         }
