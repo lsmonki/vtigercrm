51e97fd4a30dcb1517038d03a8e2f41df1180fb9
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-29 11:12:44 +0000

    Removed function trim() from Forms.php Ahmed

--- modules/Help/Forms.php
+++ modules/Help/Forms.php
@@ -120,17 +120,6 @@ function isDate(dtStr){
 return true
 }
 
-function trim(s) {
-	while (s.substring(0,1) == " ") {
-		s = s.substring(1, s.length);
-	}
-	while (s.substring(s.length-1, s.length) == ' ') {
-		s = s.substring(0,s.length-1);
-	}
-
-	return s;
-}
-
 function verify_data(form) {
 	var isError = false;
 	var errorMessage = "";
