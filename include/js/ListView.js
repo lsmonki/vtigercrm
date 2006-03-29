d604e0b5e47a8576b377a761e7c480e1f050ca9c
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-29 11:18:13 +0000

    js fn common for listview has been moved to ListView.js

---
+++ include/js/ListView.js
@@ -0,0 +1,75 @@
+/*********************************************************************************
+
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ********************************************************************************/
+
+function massDelete(module)
+{
+        x = document.massdelete.selected_id.length;
+	var viewid = document.massdelete.viewname.value;
+        idstring = "";
+
+        if ( x == undefined)
+        {
+
+                if (document.massdelete.selected_id.checked)
+                {
+                        document.massdelete.idlist.value=document.massdelete.selected_id.value;
+                }
+                else
+                {
+                        alert("Please select atleast one entity");
+                        return false;
+                }
+        }
+        else
+        {
+                xx = 0;
+                for(i = 0; i < x ; i++)
+                {
+                        if(document.massdelete.selected_id[i].checked)
+                        {
+                                idstring = document.massdelete.selected_id[i].value +";"+idstring
+                        xx++
+                        }
+                }
+                if (xx != 0)
+                {
+                        document.massdelete.idlist.value=idstring;
+                }
+               else
+                {
+                        alert("Please select atleast one entity");
+                        return false;
+                }
+        }
+		if(confirm("Are you sure you want to delete the selected "+xx+" records ?"))
+		{
+			show("status");
+			var ajaxObj = new Ajax(ajaxSaveResponse);
+			var urlstring ="module=Users&action=massdelete&return_module="+module+"&viewname="+viewid+"&idlist="+idstring;
+	    	ajaxObj.process("index.php?",urlstring);
+		}
+		else
+		{
+			return false;
+		}
+
+}
+
+function showDefaultCustomView(selectView,module)
+{
+
+		show("status");
+		var ajaxObj = new Ajax(ajaxSaveResponse);
+		var viewName = selectView.options[selectView.options.selectedIndex].value;
+		var urlstring ="module="+module+"&action="+module+"Ajax&file=ListView&ajax=true&viewname="+viewName;
+	    ajaxObj.process("index.php?",urlstring);
+}
+
+
