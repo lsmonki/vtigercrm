fa3659077d3540e823c994e8accd34fa21a9b9ca
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-30 05:51:54 +0000

    changes made for duplicate code removal

--- modules/Activities/EditView.php
+++ modules/Activities/EditView.php
@@ -123,48 +123,11 @@ $smarty->assign("ID", $focus->id);
  $activities_tables = Array('activity','crmentity'); 
  $tabid = getTabid($tab_type);
  $validationData = getDBValidationData($activities_tables,$tabid);
- $fieldName = '';
- $fieldLabel = '';
- $fldDataType = '';
+ $data = split_validationdataArray($validationData);
 
- $rows = count($validationData);
- foreach($validationData as $fldName => $fldLabel_array)
- {
-   if($fieldName == '')
-   {
-     $fieldName="'".$fldName."'";
-   }
-   else
-   {
-     $fieldName .= ",'".$fldName ."'";
-   }
-   foreach($fldLabel_array as $fldLabel => $datatype)
-   {
-	if($fieldLabel == '')
-	{
-			
-     		$fieldLabel = "'".$fldLabel ."'";
-	}		
-      else
-       {
-      $fieldLabel .= ",'".$fldLabel ."'";
-        }
- 	if($fldDataType == '')
-         {
-      		$fldDataType = "'".$datatype ."'";
-    	}
-	 else
-        {
-       		$fldDataType .= ",'".$datatype ."'";
-     	}
-   }
- }
-
-
-
-$smarty->assign("VALIDATION_DATA_FIELDNAME",$fieldName);
-$smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$fldDataType);
-$smarty->assign("VALIDATION_DATA_FIELDLABEL",$fieldLabel);
+ $smarty->assign("VALIDATION_DATA_FIELDNAME",$data['fieldname']);
+ $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
+ $smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
 
 if($focus->mode == 'edit')
 	$smarty->display("salesEditView.tpl");
