9dc48b7988142f5a429870299b6f32f73b741523
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-30 13:18:02 +0000

    changes made for duplicate code removal

--- modules/Quotes/ListView.php
+++ modules/Quotes/ListView.php
@@ -87,25 +87,8 @@ if(isPermitted('Quotes',2,'') == 'yes')
 
 if($viewnamedesc['viewname'] == 'All')
 {
-$cvHTML = '<td><a href="index.php?module=Quotes&action=CustomView">'.$app_strings['LNK_CV_CREATEVIEW'].'</a>
-<span class="small">|</span>
-<span class="small" disabled>'.$app_strings['LNK_CV_EDIT'].'</span>
-<span class="small">|</span>
-<span class="small" disabled>'.$app_strings['LNK_CV_DELETE'].'</span></td>';
-}else
-{
-$cvHTML = '<td><a href="index.php?module=Quotes&action=CustomView">'.$app_strings['LNK_CV_CREATEVIEW'].'</a>
-<span class="small">|</span>
-<a href="index.php?module=Quotes&action=CustomView&record='.$viewid.'">'.$app_strings['LNK_CV_EDIT'].'</a>
-<span class="small">|</span>
-<a href="index.php?module=CustomView&action=Delete&dmodule=Quotes&record='.$viewid.'">'.$app_strings['LNK_CV_DELETE'].'</a></td>';
+	$smarty->assign("ALL", 'All');
 }
-	$customstrings = '<td align="right" class="small">'.$app_strings[LBL_VIEW].'</td>
-			<td><SELECT NAME="viewname" class="small" onchange="showDefaultCustomView(this,\'Quotes\')">
-				'.$customviewcombo_html.'
-                        </SELECT></td>
-			'.$cvHTML;
-
 
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
@@ -114,7 +97,8 @@ $smarty->assign("APP", $app_strings);
 $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MODULE",$currentModule);
 $smarty->assign("SINGLE_MOD",'Quote');
-$smarty->assign("CUSTOMVIEW",$customstrings);
+$smarty->assign("CUSTOMVIEW_OPTION",$customviewcombo_html);
+$smarty->assign("VIEWID", $viewid);
 $smarty->assign("BUTTONS", $other_text);
 $category = getParentTab();
 $smarty->assign("CATEGORY",$category);
