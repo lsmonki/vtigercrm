510a40472d7051f01950ab666fefdd61a74da3de
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-03-30 07:27:39 +0000

    changes made for duplicate code removal

--- modules/Vendors/EditView.php
+++ modules/Vendors/EditView.php
@@ -79,51 +79,12 @@ $smarty->assign("IMAGE_PATH", $image_path);$smarty->assign("PRINT_URL", "phprint
 $vendor_tables = Array('vendor'); 
 
 $validationData = getDBValidationData($vendor_tables);
-$fieldName = '';
-$fieldLabel = '';
-$fldDataType = '';
-
-$rows = count($validationData);
-foreach($validationData as $fldName => $fldLabel_array)
-{
-	if($fieldName == '')
-	{
-		$fieldName="'".$fldName."'";
-	}
-	else
-	{
-		$fieldName .= ",'".$fldName ."'";
-	}
-	foreach($fldLabel_array as $fldLabel => $datatype)
-	{
-		if($fieldLabel == '')
-		{
-
-			$fieldLabel = "'".$fldLabel ."'";
-		}		
-		else
-		{
-			$fieldLabel .= ",'".$fldLabel ."'";
-		}
-		if($fldDataType == '')
-		{
-			$fldDataType = "'".$datatype ."'";
-		}
-		else
-		{
-			$fldDataType .= ",'".$datatype ."'";
-		}
-	}
-}
-
-
+$data = split_validationdataArray($validationData);
 $category = getParentTab();
 $smarty->assign("CATEGORY",$category);
-
-$smarty->assign("VALIDATION_DATA_FIELDNAME",$fieldName);
-$smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$fldDataType);
-$smarty->assign("VALIDATION_DATA_FIELDLABEL",$fieldLabel);
-
+$smarty->assign("VALIDATION_DATA_FIELDNAME",$data['fieldname']);
+$smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
+$smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
 if($focus->mode == 'edit')
 $smarty->display('salesEditView.tpl');
 else
