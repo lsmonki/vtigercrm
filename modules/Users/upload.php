374e91f56f695593f3f98ee93b4d6c01090907c9
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-05 05:01:09 +0000

    Made the necessary changes for smartification of new ui for mail merge template - Ahmed

--- modules/Users/upload.php
+++ modules/Users/upload.php
@@ -1,4 +1,6 @@
-<!--*********************************************************************************
+<?php
+
+/***********************************************************************************
 ** The contents of this file are subject to the vtiger CRM Public License Version 1.0
  * ("License"); You may not use this file except in compliance with the License
  * The Original Code is:  vtiger CRM Open Source
@@ -7,59 +9,35 @@
  * All Rights Reserved.
 * 
  ********************************************************************************/
--->
+
+require_once('Smarty_setup.php');
+require_once('include/database/PearDatabase.php');
+require_once('include/utils/utils.php');
+require_once('modules/Users/add2db.php');
+
+global $app_strings;
+global $mod_strings;
+global $app_list_strings;
+global $adb;
+global $theme;
+$theme_path="themes/".$theme."/";
+$image_path=$theme_path."images/";
+require_once($theme_path.'layout_utils.php');
+
+$smarty = new vtigerCRM_Smarty;
+
+
+$smarty->assign("MOD", return_module_language($current_language,'Settings'));
+$smarty->assign("IMAGE_PATH",$image_path);
+$smarty->assign("APP", $app_strings);
+$smarty->assign("UMOD", $mod_strings);
+
+$smarty->display('CreateWordTemplate.tpl');
+
+?>
 
 
 
-<HTML>
-<BODY>
-<FORM METHOD="post" action="index.php?module=Users&action=add2db" enctype="multipart/form-data">
-	<?php
-		$ret_action = "index";
-                $ret_module = "Settings"; 
-//$ret_id = $_REQUEST['return_id'];
 
-	?>
-<?php echo get_module_title($mod_strings['LBL_MODULE_NAME'],$mod_strings['LBL_ATTACH_FILE'],false);?>
-<br>
- <INPUT TYPE="hidden" NAME="MAX_FILE_SIZE" VALUE="100000">
- <INPUT TYPE="hidden" NAME="return_module" VALUE="<?php echo $ret_module ?>">
- <INPUT TYPE="hidden" NAME="return_id" VALUE="<?php echo $ret_id ?>">
- <TABLE width="50%" BORDER="0" cellspacing="0" cellpadding="0" class="formOuterBorder">
- <tr>
- 	<td class="formSecHeader"><?php echo $mod_strings['LBL_FILE_INFORMATION']; ?></td>
-</tr>
- 	<td> 
-	<TABLE BORDER="0" cellspacing="1" cellpadding="5">
-	<TR>
-	   <TD class="dataLabel"><?php echo $mod_strings['LBL_MODULENAMES']; ?>:</TD>
-	   <TD><SELECT name="target_module">
-	<option value="Leads"><?php echo $app_strings['COMBO_LEADS'];?></option>
-	<option value="Accounts"><?php echo $app_strings['COMBO_ACCOUNTS'];?></option>
-	<option value="Contacts"><?php echo $app_strings['COMBO_CONTACTS'];?></option>
-    <option value="HelpDesk"><?php echo $app_strings['COMBO_HELPDESK'];?></option>
-	</SELECT>
-	</TD> 
-  </TR>
-  <TR>
-   <TD class="dataLabel"><?php echo $mod_strings['LBL_FILE']; ?>:</TD>
-   <TD><INPUT TYPE="file" NAME="binFile"></TD>
-  </TR>
-    <TR>
-   <TD width="25%" valign="top" class="dataLabel"><?php echo $mod_strings['LBL_DESCRIPTION']; ?>:</TD>
-   <TD width="75%"><TEXTAREA NAME="txtDescription" ROWS="3" COLS="50"></TEXTAREA></TD>
-  </TR>
-       </TABLE></td>
- </tr>
- </table>
- <br>
-  <TABLE width="50%" BORDER="0" cellspacing="0" cellpadding="0">
-  <TR>
-   <TD><div align="center">
-          <INPUT class="button" TYPE="submit" VALUE="<?php echo $mod_strings['LBL_UPLOAD']; ?>">
-        </div></TD>
-  </TR>
- </TABLE>
-</FORM>
-</BODY>
-</HTML>
+
+
