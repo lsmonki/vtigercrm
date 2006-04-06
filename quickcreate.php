81d1ffa167a5dc613cba0119377dbc3c6b406cd9
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-06 13:21:18 +0000

    changes made to integrate new UI

--- quickcreate.php
+++ quickcreate.php
@@ -4,7 +4,7 @@ require_once("Smarty_setup.php");
 require_once("include/utils/CommonUtils.php");
 require_once("include/FormValidationUtil.php");
 
-global $mod_strings;
+global $mod_strings,$current_user;
 global $app_strings;
 global $adb;
 global $app_list_strings;
@@ -18,13 +18,19 @@ require_once($theme_path.'layout_utils.php');
 $smarty = new vtigerCRM_Smarty;
 
 $qcreate_array = QuickCreate("$module");
-$smarty->assign("QUICKCREATE", $qcreate_array);
+$validationData = $qcreate_array['data'];
+$data = split_validationdataArray($validationData);
+$smarty->assign("QUICKCREATE", $qcreate_array['form']);
 $smarty->assign("THEME",$theme);
 $smarty->assign("APP",$app_strings);
 $smarty->assign("MOD",$mod_strings);
 $smarty->assign("THEME",$theme);
 $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("MODULE",$module);
+$smarty->assign("USERID",$current_user->id);
+$smarty->assign("VALIDATION_DATA_FIELDNAME",$data['fieldname']);
+$smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
+$smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
 $smarty->assign("CATEGORY",$category);
 
 $smarty->display("QuickCreate.tpl");
