198f12819e70d659184eaceea79878eba1ae7238
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-07 07:51:02 +0000

    Added the parentab redirection for Account module

--- modules/Accounts/Save.php
+++ modules/Accounts/Save.php
@@ -83,6 +83,7 @@ $focus->save("Accounts");
 $return_id = $focus->id;
 //save_customfields($focus->id);
 
+if(isset($_REQUEST['parenttab']) && $_REQUEST['parenttab'] != "") $parenttab = $_REQUEST['parenttab'];
 if(isset($_REQUEST['return_module']) && $_REQUEST['return_module'] != "") $return_module = $_REQUEST['return_module'];
 else $return_module = "Accounts";
 if(isset($_REQUEST['return_action']) && $_REQUEST['return_action'] != "") $return_action = $_REQUEST['return_action'];
@@ -95,7 +96,7 @@ $local_log->debug("Saved record with id of ".$return_id);
 //code added for returning back to the current view after edit from list view
 if($_REQUEST['return_viewname'] == '') $return_viewname='0';
 if($_REQUEST['return_viewname'] != '')$return_viewname=$_REQUEST['return_viewname'];
-header("Location: index.php?action=$return_action&module=$return_module&record=$return_id&viewname=$return_viewname");
+header("Location: index.php?action=$return_action&module=$return_module&parenttab=$parenttab&record=$return_id&viewname=$return_viewname");
 //Code to save the custom field info into database
 function save_customfields($entity_id)
 {
