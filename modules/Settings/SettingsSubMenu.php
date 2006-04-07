dda27d87f28a6c6bda6e2716327dc3895bc01344
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-07 12:47:02 +0000

    changed to old UI

--- modules/Settings/SettingsSubMenu.php
+++ modules/Settings/SettingsSubMenu.php
@@ -37,47 +37,11 @@ $module_array=Array('Leads'=>'Leads',
 			);
 if($_REQUEST['type']=='CustomField')
 {
-	$smarty->assign("MODULES",$module_array);
-	$module = 'Leads';
-	$smarty->assign("MODULE",$module);
-	$smarty->assign("CFENTRIES",getCFListEntries($module));
-	//$smarty->display("CustomFieldindex.tpl");
-	$smarty->display("CustomFieldList.tpl");
+	$smarty->display("CustomFieldindex.tpl");
 }
 	$smarty->display("CustomFieldindex.tpl");
 elseif($_REQUEST['type']=='PickList')
 	$smarty->display("PickListindex.tpl");
 elseif($_REQUEST['type']=='FieldOrder')
 	$smarty->display("FieldOrderindex.tpl");
-
-function getCFListEntries($module)
-{
-	$tabid = getTabid($module);
-	global $adb;
-	global $theme;
-	$theme_path="themes/".$theme."/";
-	$image_path=$theme_path."images/";
-	$dbQuery = "select fieldid,columnname,fieldlabel,uitype,displaytype from field where tabid=".$tabid." and generatedtype=2 order by sequence";
-	$result = $adb->query($dbQuery);
-	$row = $adb->fetch_array($result);
-	$count=1;
-	$cflist=Array();
-	if($row!='')
-	{
-		do
-		{
-			$cf_element=Array();
-			$cf_element['no']=$count;
-			$cf_element['label']=$row["fieldlabel"];
-			$fld_type_name = getCustomFieldTypeName($row["uitype"]);
-			$cf_element['type']=$fld_type_name;
-			$cf_element['tool']='<a href="index.php?module=Settings&action=CreateCustomField&fieldid='.$row["fieldid"].'&tabid='.$tabid.'&uitype='.$row["uitype"].'&fld_module='.$fld_module.'&parenttab=Settings" ><img src="'.$image_path.'editfield.gif" border="0" alt="Edit" title="Edit"/></a>&nbsp;|&nbsp;<a rhef="javascript:deleteCustomField('.$row["fieldid"].',\''.$fld_module.'\', \''.$row["columnname"].'\', \''.$row["uitype"].'\')"><img src="'.$image_path.'delete.gif" border="0"  alt="Delete" title="Delete"/></a>';
-
-			$cflist[] = $cf_element;
-			$count++;
-		}while($row = $adb->fetch_array($result));
-	}
-	return $cflist;
-}
-	
 ?>
