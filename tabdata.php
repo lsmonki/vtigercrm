d8d5db5924306efa16577be3d9d398ac5c570a49
mmbrich <mmbrich@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-08 05:36:30 +0000

    added webmails to tabs list

---
+++ tabdata.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This file contains the commonly used variables 
+
+$tab_info_array=array(Dashboard=>1,Potentials=>2,Home=>3,Contacts=>4,Accounts=>6,Leads=>7,Notes=>8,Activities=>9,Emails=>10,HelpDesk=>13,Products=>14,Faq=>15,Events=>16,Calendar=>17,Vendors=>18,PriceBooks=>19,Quotes=>20,PurchaseOrder=>21,SalesOrder=>22,Invoice=>23,Rss=>24,Reports=>25,Campaigns=>26,Portal=>27,Webmails=>28);
+
+$tab_seq_array=array(1=>0,2=>0,3=>0,4=>0,6=>0,7=>0,8=>0,9=>0,10=>0,13=>0,14=>0,15=>2,16=>2,17=>0,18=>0,19=>0,20=>0,21=>0,22=>0,23=>0,24=>0,25=>0,26=>0,27=>0,);
+?>
