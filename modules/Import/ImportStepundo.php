304d7da20777d1cb045f067fa5e6b900af7ef556
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-11 15:46:17 +0000

    Modified the ImportStepundo file for smartification & new ui integration of Import Module - Ahmed

--- modules/Import/ImportStepundo.php
+++ modules/Import/ImportStepundo.php
@@ -62,13 +62,26 @@ $ret_value = $last_import->undo($current_user->id);
 <br>
 
 <table width="100%" border=1>
-<tr>
-<td>
+<table align="center" cellpadding="5" cellspacing="0" width="95%" class="leadTable">
+        <tr><br><br><br>
+         <td bgcolor="#FFFFFF" height="50" valign="middle" align="left" class="genHeaderSmall"> Import <?php echo $_REQUEST['module']; ?> </td>
+        </tr>
+                <tr bgcolor="#ECECEC"><td colspan="2">&nbsp;</td></tr>
+                <tr bgcolor="#ECECEC">
+                        <td align="left"  style="padding-left:40px;" colspan="2">
+                                <span class="genHeaderGray">Step 3 of 3 : </span>&nbsp;
+                                <span class="genHeaderSmall">Mapping Results </span>
+                        </td>
+                </tr>
+          <tr bgcolor="#ECECEC">
+                <td style="padding-left:140px;">
+
+
 <br>
 <?php 
 if ($ret_value) {
 ?>
-<?php echo $mod_strings['LBL_SUCCESS'] ?><BR>
+<?php echo "<b>" . $mod_strings['LBL_SUCCESS']."</b>" ?><BR><br>
 <?php echo $mod_strings['LBL_LAST_IMPORT_UNDONE'] ?>
 <?php 
 } 
@@ -89,14 +102,16 @@ else
 <form name="Import" method="POST" action="index.php">
 <input type="hidden" name="module" value="<?php echo $_REQUEST['module']; ?>">
 <input type="hidden" name="action" value="Import">
-<input type="hidden" name="step" value="1">
+<input type="hidden" name="step" value="2">
 <input type="hidden" name="return_module" value="<?php echo $_REQUEST['RETURN_MODULE'] ?>">
 <input type="hidden" name="return_id" value="<?php echo $_REQUEST['RETURN_ID'] ?>">
 <input type="hidden" name="return_action" value="<?php echo $_REQUEST['RETURN_ACTION'] ?>">
 
 <table width="100%" cellpadding="2" cellspacing="0" border="0"><tr>
-        <td align="right"><input title="<?php echo $mod_strings['LBL_TRY_AGAIN'] ?>" accessKey="" class="button" type="submit" name="button" value="  <?php echo $mod_strings['LBL_TRY_AGAIN'] ?>  "></td>
+        <td align="right"><input title="<?php echo $mod_strings['LBL_TRY_AGAIN'] ?>" accessKey="" class="classBtn" type="submit" name="button" value="  <?php echo $mod_strings['LBL_TRY_AGAIN'] ?>  "></td>
         <td></td>
 </tr>
 </table>
         </form>
+
+</table>
