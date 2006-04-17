e89556b63462764b662c3cc1c74ae7d788586868
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-17 15:55:47 +0000

    new UI for customfield has been integrated

--- modules/Settings/SaveConvertLead.php
+++ modules/Settings/SaveConvertLead.php
@@ -47,6 +47,6 @@ require_once('include/utils/utils.php');
 
 		$adb->query($update_sql);
 	}
-	 header("Location: index.php?module=Settings&action=ListLeadCustomFieldMapping");
+	 header("Location: index.php?module=Settings&action=ListLeadCustomFieldMapping&parenttab=Settings");
 	
 ?>
