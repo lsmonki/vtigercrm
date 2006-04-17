0263f1cbfa61875301c2b975185e15f622c97204
mmbrich <mmbrich@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-17 02:09:25 +0000

    Removed webmails.js file which closes #91

--- modules/Webmails/Webmails.js
+++
@@ -1,6 +0,0 @@
-oFCKeditor = new FCKeditor( "description" ); 
-oFCKeditor.BasePath   = "include/fckeditor/"; 
-oFCKeditor.Height = 350; 
-oFCKeditor.Width = "100%"; 
-oFCKeditor.CheckBrowser = true;
-oFCKeditor.ReplaceTextarea();
