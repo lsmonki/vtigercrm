ac3bb21d9ffdc00130631cf8d75c1fb6cd0dc781
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-26 14:58:41 +0000

    Added the Button Security Check for List/Edit/Detail View - Ahmed

--- modules/Invoice/EditView.php
+++ modules/Invoice/EditView.php
@@ -274,6 +274,9 @@ $smarty->assign("VALIDATION_DATA_FIELDNAME",$data['fieldname']);
 $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
 $smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
 
+$check_button = Button_Check($module);
+$smarty->assign("CHECK", $check_button);
+
 if($focus->mode == 'edit')
 $smarty->display("salesEditView.tpl");
 else
