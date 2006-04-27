ce2ed345bca4947a7a070bd18646afe2afcaa0f7
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-04-27 08:11:34 +0000

    update info made dynamic. Fixes #245

--- modules/HelpDesk/CallRelatedList.php
+++ modules/HelpDesk/CallRelatedList.php
@@ -42,5 +42,6 @@ $smarty->assign("id",$focus->id);
 $smarty->assign("ID",$RECORD );
 $smarty->assign("MODULE",$currentmodule);
 $smarty->assign("SINGLE_MOD",$currentmodule);
+$smarty->assign("UPDATEINFO",updateInfo($focus->id));
 $smarty->display("RelatedLists.tpl");
 ?>
