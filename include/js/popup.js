ac5d16bb8a8a5f3ee110575921f0ff09512a82d8
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-22 12:06:45 +0000

    height increased

--- include/js/popup.js
+++ include/js/popup.js
@@ -1,5 +1,5 @@
 function mypopup()
 {
-      mywindow = window.open("copyright.html","mywindow", "height=5, width=575");
+      mywindow = window.open("copyright.html","mywindow", "height=115, width=575");
       mywindow.moveTo(210,620);
 }
