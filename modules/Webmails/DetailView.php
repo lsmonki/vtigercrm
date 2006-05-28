be950904312fc7c4c6461627f80e1c7ef7d4e5b5
mmbrich <mmbrich@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-28 07:58:30 +0000

    extra fields

--- modules/Webmails/DetailView.php
+++ modules/Webmails/DetailView.php
@@ -23,11 +23,13 @@ echo '<td style="background-color:#CCCC99">';
 echo '<table border="1" width="100%" cellpadding="0" cellspacing="0">';
 echo '<tr><td width="10%">From:</td><td>'.$webmail->fromname.'</td></tr>';
 echo '<tr><td width="10%">To:</td><td>'.implode(" ",$webmail->to).'</td></tr>';
+echo '<tr><td width="10%">Subject:</td><td>'.$webmail->subject.'</td></tr>';
+echo '<tr><td width="10%">Date:</td><td>'.$webmail->date.'</td></tr>';
 echo '</table>';
 
 echo '</td></tr>';
 
-echo '<tr><td align="center"><iframe src="index.php?module=Webmails&action=body&mailid='.$mailid.'&mailbox='.$mailbox.'" width="100%" height="450" frameborder="0">No Iframes supported</iframe></td></tr>';
+echo '<tr><td align="center"><iframe src="index.php?module=Webmails&action=body&mailid='.$mailid.'&mailbox='.$mailbox.'" width="100%" height="450" frameborder="0" style="border:1px solid gray">No Iframes supported</iframe></td></tr>';
 
 echo "<tr><td><p style='font-weight:bold'>Email Attachments:</p></td></tr>";
 foreach($webmail->attachments as $key=>$value) {
