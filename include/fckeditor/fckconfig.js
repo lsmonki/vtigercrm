72db7ab9c473f2e951e827e2bf0549a641fd3253
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-29 08:53:47 +0000

    icon path changes made

--- include/fckeditor/fckconfig.js
+++ include/fckeditor/fckconfig.js
@@ -31,7 +31,7 @@ FCKConfig.FullPage = false ;
 FCKConfig.Debug = false ;
 FCKConfig.AllowQueryStringDebug = true ;
 
-FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/silver/' ;
+FCKConfig.SkinPath = FCKConfig.BasePath + 'skins/default/' ;
 
 FCKConfig.PluginsPath = FCKConfig.BasePath + 'plugins/' ;
 
@@ -76,20 +76,31 @@ FCKConfig.PreserveSessionOnFileBrowser = false ;
 FCKConfig.FloatingPanelsZIndex = 10000 ;
 
 FCKConfig.ToolbarSets["Default"] = [
-	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
-	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
-	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['Bold','Italic','Underline','-'],
 	['OrderedList','UnorderedList','-','Outdent','Indent'],
 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
-	['Link','Unlink','Anchor'],
-	['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak','UniversalKey'],
-	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
-	'/',
-	['Style','FontFormat','FontName','FontSize'],
+	['Link','Image','SpellCheck'],
 	['TextColor','BGColor'],
+	'/',
+	['FontFormat','FontName','FontSize'],
+
 	['About']
 ] ;
+//FCKConfig.ToolbarSets["Default"] = [
+//	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+//	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+//	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+//	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+//	['OrderedList','UnorderedList','-','Outdent','Indent'],
+//	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+//	['Link','Unlink','Anchor'],
+//	['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak','UniversalKey'],
+//	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
+//	'/',
+//	['Style','FontFormat','FontName','FontSize'],
+//	['TextColor','BGColor'],
+//	['About']
+//] ;
 
 FCKConfig.ToolbarSets["Basic"] = [
 	['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink','-','About']
