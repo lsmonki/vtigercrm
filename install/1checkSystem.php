c7a4ae0fa8ebff6bf6f003227181bae07abb9d57
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-29 07:12:18 +0000

    added additional parameter

--- install/1checkSystem.php
+++ install/1checkSystem.php
@@ -324,6 +324,7 @@ $array = Array(
 		<tr bgcolor="#ffffff"> <td valign=top ><strong>Change the memory limit = 16M</strong></tr>
 		<tr bgcolor="#ffffff"> <td valign=top ><strong>error_reporting = E_WARNING & ~E_NOTICE</strong></tr>
 		<tr bgcolor="#ffffff"> <td valign=top ><strong>allow_call_time_reference = On</strong></tr>
+		<tr bgcolor="#ffffff"> <td valign=top ><strong>log_errors = Off</strong></tr>
 		<tr bgcolor="#ffffff"> <td valign=top ><strong>output_buffering= On</strong></tr>
 		<tr bgcolor="#ffffff">  <td valign=top ><a href="http://www.vtiger.com/products/crm/help/vtiger_CRM_Linux_Dependencies.pdf">Linux installation pre-requisites</a></tr>
 		</table>
