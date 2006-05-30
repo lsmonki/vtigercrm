31754f033bd096f58f2fdf14f1e9287a5a722bc2
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-30 16:00:35 +0000

    added word plugin

--- vtigerservice.php
+++ vtigerservice.php
@@ -27,6 +27,10 @@ if(isset($_REQUEST['service']))
 	{
 		include("soap/contactserialize.php");
 	}
+	elseif($_REQUEST['service'] == "wordplugin")
+	{
+		include("soap/contactserialize.php");
+	}
 	elseif($_REQUEST['service'] == "thunderbird")
 	{
 		include("soap/contactserialize.php");
