779540ef3c65f4be6fbf327470049f1377277b08
don <don@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-31 13:04:11 +0000

    License added

--- include/utils/GetParentGroups.php
+++ include/utils/GetParentGroups.php
@@ -1,12 +1,14 @@
 <?php
 /*********************************************************************************
- * $Header$
- * Description:  Defines the Account SugarBean Account entity with the necessary
- * methods and variables.
- * Portions created by SugarCRM are Copyright (C) SugarCRM, Inc.
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- * Contributor(s): ______________________________________..
+*
  ********************************************************************************/
+
 /** Class to retreive all the Parent Groups of the specified Group
  *
  */
