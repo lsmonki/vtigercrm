e6752d7b7229daba4710a94a45cd39a528194391
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-05-31 13:28:53 +0000

    license added

--- include/quickcreate.php
+++ include/quickcreate.php
@@ -9,7 +9,6 @@
  *
  ********************************************************************************/
 
-
 require_once("Smarty_setup.php");
 require_once("include/utils/CommonUtils.php");
 require_once("include/FormValidationUtil.php");
