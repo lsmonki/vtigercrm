c8d2ac130fadcdb2307a2d2748965b8f7a475bd0
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-05 12:24:43 +0000

    i18n for MySites module updated - ahmed

--- modules/Portal/Popup.php
+++ modules/Portal/Popup.php
@@ -26,25 +26,25 @@ $portal_inputs='';
 $portal_inputs.='<div style="display:block;position:relative;" id="orgLay">
 		<table border="0" cellpadding="5" cellspacing="0" width="100%">
 		<tbody><tr>
-			<td class="genHeaderSmall" align="left" width="30%">Add Bookmark</td>
+			<td class="genHeaderSmall" align="left" width="30%">' .$mod_strings['LBL_ADD'] .' '.$mod_strings['LBL_BOOKMARK'].'</td>
 			<td align="right" width="80%"><a href="javascript:fninvsh(\'orgLay\');"><img src="../themes/blue/images/close.gif" align="absmiddle" border="0"></a></td>
 		</tr>
 		<tr><td colspan="2"><hr></td></tr>
 		<tr>
 
-			<td align="right"><b>Bookmark URL :</b></td>
+			<td align="right"><b>'.$mod_strings['LBL_BOOKMARK'].' ' .$mod_strings['LBL_URL'] .' </b></td>
 			<td align="left"><input name="portalurl" id="portalurl" class="txtBox" value="'.$portalurl.'" type="text"></td>
 		</tr>
 		<tr>
-			<td align="right"><b>Bookmark Name : </b></td>
+			<td align="right"><b>'.$mod_strings['LBL_BOOKMARK'].' ' .$mod_strings['LBL_NAME'] .' </b></td>
 			<td align="left"><input name="portalname" id="portalname" value="'.$portalname.'" class="txtBox" type="text"></td>
 		</tr>
 		<tr><td style="border-bottom: 1px dashed rgb(204, 204, 204);" colspan="2">&nbsp;</td></tr>
 
 		<tr>
 			<td colspan="2" align="center">
-					<input name="save" value=" &nbsp;Save&nbsp; " class="classBtn" onClick="SaveSite(\''.$portalid.'\')" type="button">&nbsp;&nbsp;
-					<input name="cancel" value=" Cancel " class="classBtn" onclick="fninvsh(\'orgLay\');" type="button">
+					<input name="save" value=" &nbsp;{$APP.LBL_SAVE_BUTTON_LABEL}&nbsp; " class="classBtn" onClick="SaveSite(\''.$portalid.'\')" type="button">&nbsp;&nbsp;
+					<input name="cancel" value=" {$APP.LBL_CANCEL_BUTTON_LABEL}  " class="classBtn" onclick="fninvsh(\'orgLay\');" type="button">
 			</td>
 		</tr>
 		<tr><td colspan="2" style="border-top: 1px dashed rgb(204, 204, 204);">&nbsp;</td></tr>
