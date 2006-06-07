39236d4284d5693e478fd41a7bd07d0301886127
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-07 14:03:35 +0000

    ajax functionality has been modified

--- modules/Emails/Email.js
+++ modules/Emails/Email.js
@@ -10,82 +10,27 @@
 
 var gFolderid = 1;
 var gselectedrowid = 0;
-function massDelete()
-{
-		var delete_selected_row = false;
-        x = document.massdelete.selected_id.length;
-        idstring = "";
-        if ( x == undefined)
-        {
-
-                if (document.massdelete.selected_id.checked)
-                {
-					if(document.massdelete.selected_id.value == gselectedrowid)
-					{
-						gselectedrowid = 0;
-						delete_selected_row = true;						
-					}
-                        idstring = document.massdelete.selected_id.value;
-						xx = 1;
-                }
-                else
-                {
-                        alert("Please select atleast one entity");
-                        return false;
-                }
-        }
-        else
-        {
-                xx = 0;
-                for(i = 0; i < x ; i++)
-                {
-                        if(document.massdelete.selected_id[i].checked)
-						{
-							if(document.massdelete.selected_id[i].value == gselectedrowid)
-							{
-								gselectedrowid = 0;
-								delete_selected_row = true;						
-							}
-							idstring = document.massdelete.selected_id[i].value +";"+idstring
-							xx++
-						}
-                }
-                if (xx != 0)
-                {
-                        document.massdelete.idlist.value=idstring;
-                }
-                else
-                {
-                        alert("Please select atleast one entity");
-                        return false;
-                }
-        }
-		if(confirm("Are you sure you want to delete the selected "+xx+" records ?"))
-		{	
-			getObj('search_text').value = '';
-			show("status");
-			if(!delete_selected_row)
-				var ajaxObj = new VtigerAjax(ajaxSaveResponse);
-			else	
-				var ajaxObj = new VtigerAjax(ajaxDelResponse);
-			var urlstring ="module=Users&action=massdelete&folderid="+gFolderid+"&return_module=Emails&idlist="+idstring;
-		    ajaxObj.process("index.php?",urlstring);
-		}
-		else
-		{
-			return false;
-		}
-}
 function DeleteEmail(id)
 {
 	if(confirm("Are you sure you want to delete ?"))
 	{	
 		getObj('search_text').value = '';
 		gselectedrowid = 0;
-		show("status");
-		var ajaxObj = new VtigerAjax(ajaxDelResponse);
-		var urlstring ="module=Users&action=massdelete&return_module=Emails&folderid="+gFolderid+"&idlist="+id;
-	   	ajaxObj.process("index.php?",urlstring);
+		$("status").style.display="inline";
+                new Ajax.Request(
+                        'index.php',
+                        {ldelim}queue: {ldelim}position: 'end', scope: 'command'{rdelim},
+                                method: 'post',
+                                postBody: "module=Users&action=massdelete&return_module=Emails&folderid="+gFolderid+"&idlist="+id,
+                                onComplete: function(response) {ldelim}
+                                                $("status").style.display="none";
+                                                $('EmailDetails').innerHTML = '<table valign="top" border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td class="forwardBg"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td colspan="2">&nbsp;</td></tr></tbody></table></td></tr><tr><td style="padding-top: 10px;" bgcolor="#ffffff" height="300" valign="top"></td></tr></tbody></table>';
+                                                $("subjectsetter").innerHTML='';
+                                                $("email_con").innerHTML=response.responseText;
+                                                execJS($('email_con'));
+                                {rdelim}
+                        {rdelim}
+                );
 	}
 	else
 	{
@@ -98,8 +43,19 @@ function Searchfn()
 	var osearch_field = document.getElementById('search_field');
 	var search_field = osearch_field.options[osearch_field.options.selectedIndex].value;
 	var search_text = document.getElementById('search_text').value;
-	var ajaxObj = new VtigerAjax(ajaxDelResponse);
-	var urlstring ="module=Emails&action=EmailsAjax&ajax=true&file=ListView&folderid="+gFolderid+"&search=true&search_field="+search_field+"&search_text="+search_text;
-    ajaxObj.process("index.php?",urlstring);
+	new Ajax.Request(
+                'index.php',
+                {ldelim}queue: {ldelim}position: 'end', scope: 'command'{rdelim},
+                        method: 'post',
+                        postBody: "module=Emails&action=EmailsAjax&ajax=true&file=ListView&folderid="+gFolderid+"&search=true&search_field="+search_field+"&search_text="+search_text,
+                        onComplete: function(response) {ldelim}
+                                        $("status").style.display="none";
+                                        $('EmailDetails').innerHTML = '<table valign="top" border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td class="forwardBg"><table border="0" cellpadding="0" cellspacing="0" width="100%"><tbody><tr><td colspan="2">&nbsp;</td></tr></tbody></table></td></tr><tr><td style="padding-top: 10px;" bgcolor="#ffffff" height="300" valign="top"></td></tr></tbody></table>';
+                                        $("subjectsetter").innerHTML='';
+                                        $("email_con").innerHTML=response.responseText;
+                                        execJS($('email_con'));
+                        {rdelim}
+                {rdelim}
+        );
 }
 
