0a00828e7638cce804d4330375aae830a015271b
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-07 14:08:02 +0000

    unwanted js functions have been removed

--- modules/Faq/Faq.js
+++ modules/Faq/Faq.js
@@ -8,60 +8,6 @@
  *
  ********************************************************************************/
 
-
-function massDelete()
-{
-	x = document.massdelete.selected_id.length;
-	idstring = "";
-
-	if ( x == undefined)
-	{
-	
-		if (document.massdelete.selected_id.checked)
-		{
-			document.massdelete.idlist.value=document.massdelete.selected_id.value;
-		}
-		else 
-		{
-			alert("Please select atleast one entity");
-			return false;
-		}
-	}
-	else
-	{
-		xx = 0;
-		for(i = 0; i < x ; i++)
-		{
-			if(document.massdelete.selected_id[i].checked)
-			{
-				idstring = document.massdelete.selected_id[i].value +";"+idstring
-			xx++	
-			}
-		}
-		if (xx != 0)
-		{
-			document.massdelete.idlist.value=idstring;
-		}
-		else
-		{
-			alert("Please select atleast one entity");
-			return false;
-		}
-	}
-	if(confirm("Are you sure you want to delete the selected "+xx+" records ?"))
-    {
-		show("status");
-		var ajaxObj = new VtigerAjax(ajaxSaveResponse);
-		var urlstring ="module=Users&action=massdelete&return_module=Faq&idlist="+idstring;
-	    ajaxObj.process("index.php?",urlstring);
-	}
-	else
-	{
-		return false;
-	}
-}
-
-
 function clear_form(form) 
 {
 	for (j = 0; j < form.elements.length; j++) 
