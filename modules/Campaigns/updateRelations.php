515d197a8120d6d0fca439a6365568ffe0daa253
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-16 08:55:44 +0000

    changes made to update lead data in campaign RL

--- modules/Campaigns/updateRelations.php
+++ modules/Campaigns/updateRelations.php
@@ -12,7 +12,19 @@
 require_once('include/database/PearDatabase.php');
 global $adb;
 $idlist = $_REQUEST['idlist'];
-$returnmodule = $_REQUEST['return_module'];
+$update_mod = $_REQUEST['destination_module'];
+if($update_mod == 'Leads')
+{
+	$rel_table = 'vtiger_campaignleadrel';
+	$mod_table = 'vtiger_leaddetails';
+	$mod_field = 'leadid';
+}
+elseif($update_mod == 'Contacts')
+{
+	$rel_table = 'vtiger_campaigncontrel';
+	$mod_table = 'vtiger_contactdetails';
+	$mod_field = 'contactid';
+}
 if(isset($_REQUEST['idlist']) && $_REQUEST['idlist'] != '')
 {
 	//split the string and store in an array
@@ -21,9 +33,9 @@ if(isset($_REQUEST['idlist']) && $_REQUEST['idlist'] != '')
 	{
 		if($id != '')
 		{
-		    $sql = "insert into  vtiger_campaigncontrel values(".$_REQUEST["parentid"]." , ".$id.")";
+		    $sql = "insert into  ".$rel_table." values(".$_REQUEST["parentid"]." , ".$id.")";
 	            $adb->query($sql);
-		    $sql = "update vtiger_contactdetails set campaignid = ".$_REQUEST["parentid"]." where contactid = ".$id;
+		    $sql = "update ".$mod_table." set campaignid = ".$_REQUEST["parentid"]." where ".$mod_field." = ".$id;
                     $adb->query($sql);
 		}
 	}
@@ -31,9 +43,9 @@ if(isset($_REQUEST['idlist']) && $_REQUEST['idlist'] != '')
 }
 elseif(isset($_REQUEST['entityid']) && $_REQUEST['entityid'] != '')
 {	
-		$sql = "insert into vtiger_campaigncontrel values(".$_REQUEST["parid"].",".$_REQUEST["entityid"].")";
+		$sql = "insert into ".$rel_table." values(".$_REQUEST["parid"].",".$_REQUEST["entityid"].")";
 		$adb->query($sql);
-		$sql = "update vtiger_contactdetails set campaignid = ".$_REQUEST["parid"]." where contactid = ".$_REQUEST["entityid"];
+		$sql = "update ".$mod_table." set campaignid = ".$_REQUEST["parid"]." where ".$mod_field." = ".$_REQUEST["entityid"];
                 $adb->query($sql);
  		header("Location: index.php?action=CallRelatedList&module=Campaigns&record=".$_REQUEST["parid"]);
 }
