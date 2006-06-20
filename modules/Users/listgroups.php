ef60c73cec96695b7418a43047945d873ab6a9c2
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-20 06:41:06 +0000

    updated the listgroup file

--- modules/Users/listgroups.php
+++ modules/Users/listgroups.php
@@ -19,9 +19,10 @@ global $app_list_strings;
 
 $smarty = new vtigerCRM_Smarty;
 $groupInfo=getAllGroupInfo();
+
 $cnt=1;
 $output='';
-$list_header = array($mod_strings['LBL_OERATION'],$mod_strings['LBL_GROUP_NAME'],$mod_strings['LBL_DESCRIPTION']);
+$list_header = array($mod_strings['LBL_LIST_TOOLS'],$mod_strings['LBL_GROUP_NAME'],$mod_strings['LBL_DESCRIPTION']);
 $return_data = array();
 foreach($groupInfo as $groupId=>$groupInfo)
 {
@@ -41,6 +42,7 @@ $smarty->assign("IMAGE_PATH",$image_path);
 $smarty->assign("APP", $app_strings);
 $smarty->assign("CMOD", $mod_strings);
 $smarty->assign("MOD", return_module_language($current_language,'Settings'));
+$smarty->assign('GRPCNT', count($return_data));
 
 $smarty->display("ListGroup.tpl");
 ?>
