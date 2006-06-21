761c07818a15e5418ca2020569493fd28a4408dc
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-21 17:57:30 +0000

    fixed the detail view ajax edit issues

--- modules/Invoice/DetailView.php
+++ modules/Invoice/DetailView.php
@@ -87,7 +87,7 @@ $data = split_validationdataArray($validationData);
 $smarty->assign("VALIDATION_DATA_FIELDNAME",$data['fieldname']);
 $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
 $smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
-
+$smarty->assign("EDIT_PERMISSION",isPermitted($currentModule,'EditView',$_REQUEST[record]));
 $smarty->display("Inventory/InventoryDetailView.tpl");
 
 ?>
