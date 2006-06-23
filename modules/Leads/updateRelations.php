03469a4446db9855564b92c6b1bea52db75e83b2
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-23 04:50:06 +0000

    file added to update the relation between lead/contact and campaign

---
+++ modules/Leads/updateRelations.php
@@ -0,0 +1,38 @@
+<?php
+/*********************************************************************************
+** The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+*
+ ********************************************************************************/
+
+require_once('include/database/PearDatabase.php');
+global $adb;
+$idlist = $_REQUEST['idlist'];
+$update_mod = $_REQUEST['destination_module'];
+$rel_table = 'vtiger_campaignleadrel';
+if(isset($_REQUEST['idlist']) && $_REQUEST['idlist'] != '')
+{
+	//split the string and store in an array
+	$storearray = explode (";",$idlist);
+	foreach($storearray as $id)
+	{
+		if($id != '')
+		{
+		    $sql = "insert into  ".$rel_table." values(".$id.",".$_REQUEST["parentid"].")";
+	            $adb->query($sql);
+		}
+	}
+ 		header("Location: index.php?action=CallRelatedList&module=Leads&record=".$_REQUEST["parentid"]);
+}
+elseif(isset($_REQUEST['entityid']) && $_REQUEST['entityid'] != '')
+{	
+		$sql = "insert into ".$rel_table." values(".$_REQUEST["entityid"].",".$_REQUEST["parid"].")";
+		$adb->query($sql);
+ 		header("Location: index.php?action=CallRelatedList&module=Leads&record=".$_REQUEST["parid"]);
+}
+
+?>
