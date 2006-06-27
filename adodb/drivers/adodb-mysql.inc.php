36e35bab09de88dfc4c2633f6b1c1180addb1d22
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-06-27 11:02:49 +0000

    appened @ to suppress the exception when giving wrong username,password for db connection, fixes #1151

--- adodb/drivers/adodb-mysql.inc.php
+++ adodb/drivers/adodb-mysql.inc.php
@@ -351,11 +351,9 @@ class ADODB_mysql extends ADOConnection {
 		if (!empty($this->port)) $argHostname .= ":".$this->port;
 		
 		if (ADODB_PHPVER >= 0x4300)
-			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,
-												$this->forceNewConnect,$this->clientFlags);
+		$this->_connectionID = @mysql_connect($argHostname,$argUsername,$argPassword,$this->forceNewConnect,$this->clientFlags);
 		else if (ADODB_PHPVER >= 0x4200)
-			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,
-												$this->forceNewConnect);
+			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,$this->forceNewConnect);
 		else
 			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword);
 	
