20f0b87b96a975e81f05532d47e873c0baec6f84
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-07-01 08:56:32 +0000

    issue in Invoice Reports has been fixed

--- modules/Reports/ReportRun.php
+++ modules/Reports/ReportRun.php
@@ -834,6 +834,7 @@ class ReportRun extends CRMEntity
 			if($secmodule == "Accounts")
 			{
 				$query = "left join vtiger_account on vtiger_account.accountid = vtiger_invoice.accountid
+					left join vtiger_contactdetails as vtiger_contactdetailsInvoice on vtiger_contactdetailsInvoice.contactid = vtiger_invoice.contactid
 					left join vtiger_crmentity as vtiger_crmentityAccounts on vtiger_crmentityAccounts.crmid=vtiger_account.accountid
 					left join vtiger_accountbillads on vtiger_account.accountid=vtiger_accountbillads.accountaddressid
 					left join vtiger_accountshipads on vtiger_account.accountid=vtiger_accountshipads.accountaddressid
