70c3969863febef64eba7340d13b81dcb637885c
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-07-21 09:25:20 +0000

    calendar integration --Minnie

--- modules/Calendar/CalendarAjax.php
+++ modules/Calendar/CalendarAjax.php
@@ -17,11 +17,14 @@ require_once('include/utils/utils.php');
 require_once("modules/Calendar/Calendar.php");
 require_once('include/logging.php');
 $cal_log =& LoggerManager::getLogger('calendar');
+$cal_log->debug("In CalendarAjax file");
 if(isset($_REQUEST['type']) && ($_REQUEST['type'] !=''))
 {
 	$type = $_REQUEST['type'];
+	$cal_log->debug("type value is:".$type);
 	if($type == 'minical')
 	{
+		$cal_log->debug("going to get mini calendar");
 	        $temp_module = $currentModule;
         	$mod_strings = return_module_language($current_language,'Calendar');
 	        $currentModule = 'Calendar';
@@ -37,11 +40,13 @@ if(isset($_REQUEST['type']) && ($_REQUEST['type'] !=''))
 	}
 	elseif($type == 'settings')
 	{
+		$cal_log->debug("going to get calendar Settings");
 		require_once('modules/Calendar/calendar_share.php');	
 	}
 	else
 	{
-		$mysel= $_REQUEST['view'];
+		$mysel = $_REQUEST['view'];
+		$subtab = $_REQUEST['subtab']; 
 		$calendar_arr = Array();
 		$calendar_arr['IMAGE_PATH'] = $image_path;
 		if(empty($mysel))
@@ -74,40 +79,96 @@ if(isset($_REQUEST['type']) && ($_REQUEST['type'] !=''))
 			$date_data['year'] = $_REQUEST['year'];
 		}
 		$calendar_arr['calendar'] = new Calendar($mysel,$date_data);
+		
 		if ($mysel == 'day' || $mysel == 'week' || $mysel == 'month' || $mysel == 'year')
 		{
 			$calendar_arr['calendar']->add_Activities($current_user);
 		}
 		$calendar_arr['view'] = $mysel;
-		if($type == 'change_owner' || $type == 'activity_delete' || $type == 'listview' || $type == 'hourview')
+		if($calendar_arr['calendar']->view == 'day')
+			$start_date = $end_date = $calendar_arr['calendar']->date_time->get_formatted_date();
+		elseif($calendar_arr['calendar']->view == 'week')
 		{
-			if(isset($_REQUEST['viewOption']) && $_REQUEST['viewOption'] != null)
+			$start_date = $calendar_arr['calendar']->slices[0];
+			$end_date = $calendar_arr['calendar']->slices[6];
+		}
+		elseif($calendar_arr['calendar']->view == 'month')
+		{
+			$start_date = $calendar_arr['calendar']->date_time->getThismonthDaysbyIndex(0);
+			$end_date = $calendar_arr['calendar']->date_time->getThismonthDaysbyIndex($calendar_arr['calendar']->date_time->daysinmonth - 1);
+			$start_date = $start_date->get_formatted_date();
+			$end_date = $end_date->get_formatted_date();
+		}
+		elseif($calendar_arr['calendar']->view == 'year')
+		{
+			$start_date = $calendar_arr['calendar']->date_time->getThisyearMonthsbyIndex(0);
+			$end_date = $calendar_arr['calendar']->date_time->get_first_day_of_changed_year('increment');
+			$start_date = $start_date->get_formatted_date();
+			$end_date = $end_date->get_formatted_date();
+		}
+		else
+		{
+			die("view:".$calendar_arr['calendar']->view." is not defined");
+		}
+		
+		if($type == 'change_owner' || $type == 'activity_delete' || $type == 'change_status')
+		{
+			if($type == 'change_status')
+			{
+				$return_id = $_REQUEST['record'];
+				if(isset($_REQUEST['status']))
+				{
+					$status = $_REQUEST['status'];
+					$activity_type = "Task";
+				}
+				elseif(isset($_REQUEST['eventstatus']))
+				{
+					$status = $_REQUEST['eventstatus'];
+					$activity_type = "Events";
+				}
+				ChangeStatus($status,$return_id,$activity_type);
+			}
+			if(isset($_REQUEST['viewOption']) && $_REQUEST['viewOption'] != null && $subtab == 'event')
 			{
 				if($_REQUEST['viewOption'] == 'hourview')
 				{
-					getHourView($calendar_arr,'ajax');
+					$cal_log->debug("going to get calendar Event HourView");
+					if($calendar_arr['view'] == 'day')
+					{
+						echo getDayViewLayout($calendar_arr);
+					}
+					elseif($calendar_arr['view'] == 'week')
+					{
+						echo getWeekViewLayout($calendar_arr);	
+					}
+					elseif($calendar_arr['view'] == 'month')
+					{
+						echo getMonthViewLayout($calendar_arr);
+					}
+					elseif($calendar_arr['view'] == 'year')
+					{
+						echo getYearViewLayout($calendar_arr);
+					}
+					else
+					{
+						die("view:".$view['view']." is not defined");
+					}
 				}
 				elseif($_REQUEST['viewOption'] == 'listview')
 				{
-					getEventListView($calendar_arr);
+					$cal_log->debug("going to get calendar Event ListView");
+					//To get Events List
+					$activity_list = getEventList($calendar_arr, $start_date, $end_date);
+					echo constructEventListView($calendar_arr,$activity_list);
 				}
 			}
-		}
-		elseif($type == 'change_status')
-		{
-			$return_id = $_REQUEST['record'];
-			if(isset($_REQUEST['status']))
+			elseif($subtab == 'todo')
 			{
-				$status = $_REQUEST['status'];
-				$activity_type = "Task";
+				$cal_log->debug("going to get calendar Todo ListView");
+				//To get Todos List
+				$todo_list = getTodoList($calendar_arr, $start_date, $end_date);
+				echo constructTodoListView($todo_list,$calendar_arr);
 			}
-			elseif(isset($_REQUEST['eventstatus']))
-			{
-				$status = $_REQUEST['eventstatus'];
-				$activity_type = "Events";
-			}
-			ChangeStatus($status,$return_id,$activity_type);
-			getEventListView($calendar_arr);
 		}
 		else
 		{
