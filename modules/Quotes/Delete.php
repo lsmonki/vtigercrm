8555b6e3b39498e8861f6ed57eb2e61f65120216
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-07-24 18:35:03 +0000

    parenttab issue has been fixed -- Jeri

--- modules/Quotes/Delete.php
+++ modules/Quotes/Delete.php
@@ -34,5 +34,6 @@ DeleteEntity($_REQUEST['module'],$_REQUEST['return_module'],$focus,$_REQUEST['re
 //code added for returning back to the current view after delete from list view
 if($_REQUEST['return_viewname'] == '') $return_viewname='0';
 if($_REQUEST['return_viewname'] != '')$return_viewname=$_REQUEST['return_viewname'];
-header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']."&viewname=".$return_viewname);
+if(isset($_REQUEST['parenttab']) && $_REQUEST['parenttab'] != "") $parenttab = $_REQUEST['parenttab'];
+header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&parenttab=".$parenttab."&record=".$_REQUEST['return_id']."&viewname=".$return_viewname);
 ?>
