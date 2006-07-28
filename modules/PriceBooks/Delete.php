65427ef374c01360e879486bdf154ffa504cd511
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-07-28 08:10:59 +0000

    * Added delete query which will be used to delete the Pricebook from Product related list

--- modules/PriceBooks/Delete.php
+++ modules/PriceBooks/Delete.php
@@ -29,10 +29,19 @@ $focus = new PriceBook();
 if(!isset($_REQUEST['record']))
 	die($mod_strings['ERR_DELETE_RECORD']);
 
+//Added to delete the pricebook from Product related list
+if($_REQUEST['record'] != '' && $_REQUEST['return_id'] != '' && $_REQUEST['module'] == 'PriceBooks' && $_REQUEST['return_module'] == 'Products')
+{
+	$pricebookid = $_REQUEST['record'];
+	$productid = $_REQUEST['return_id'];
+	$adb->query("delete from vtiger_pricebookproductrel where pricebookid=$pricebookid and productid=$productid");
+}
 
 if($_REQUEST['module'] == $_REQUEST['return_module'])
 	$focus->mark_deleted($_REQUEST['record']);
 
 if(isset($_REQUEST['parenttab']) && $_REQUEST['parenttab'] != "") $parenttab = $_REQUEST['parenttab'];
-header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']."&parenttab=".$parenttab."&relmodule=".$_REQUEST['module']);
+
+header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']."&parenttab=".$parenttab);
+
 ?>
