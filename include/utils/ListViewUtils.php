810e48d45ada09d6436f57ed555ff68865bbb2d6
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-01 15:48:16 +0000

    modified for calendar functionality -Minnie

--- include/utils/ListViewUtils.php
+++ include/utils/ListViewUtils.php
@@ -1320,7 +1320,12 @@ function getValue($field_result, $list_result,$fieldname,$focus,$module,$entity_
 					$temp_val =$temp_val.' '.$firstname;
 
 					$focus->record_id = $_REQUEST['recordid'];
-					$value = '<a href="a" LANGUAGE=javascript onclick=\'add_data_to_relatedlist("'.$entity_id.'","'.$focus->record_id.'","'.$module.'"); window.close()\'>'.$temp_val.'</a>';
+					if($_REQUEST['return_module'] == "Calendar")
+					{
+						$value = '<a href="a" LANGUAGE=javascript onclick=\'add_data_to_relatedlist_incal("'.$entity_id.'","'.$temp_val.'"); window.close()\'>'.$temp_val.'</a>';
+					}
+					else
+						$value = '<a href="a" LANGUAGE=javascript onclick=\'add_data_to_relatedlist("'.$entity_id.'","'.$focus->record_id.'","'.$module.'"); window.close()\'>'.$temp_val.'</a>';
 				}
 				elseif($popuptype == "formname_specific")
 				{
