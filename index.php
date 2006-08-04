78247020b7d74814b477423c06f9e7a339fb5944
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-04 17:34:51 +0000

    Security action added --Jeri

--- index.php
+++ index.php
@@ -620,7 +620,21 @@ else
 
 if($display == "no")
 {
-        echo "You are not permitted to execute this Operation";
+	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
+	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 45%; position: relative; z-index: 10000000;'>
+
+		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
+		<tbody><tr>
+		<td rowspan='2' width='11%'><img src='themes/$theme/images/denied.gif' ></td>
+		<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'><span class='genHeaderSmall'>You are not permitted to execute this Operation !</span></td>
+		</tr>
+		<tr>
+		<td class='small' align='right' nowrap='nowrap'>			   	
+		<a href='javascript:window.history.back();'>Go Back</a><br>								   						     </td>
+		</tr>
+		</tbody></table> 
+		</div>";
+	echo "</td></tr></table>";
 }
 else
 {
