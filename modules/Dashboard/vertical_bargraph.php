b50503dade10fefd1e32773dad96dc643e5d4714
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-04 10:10:21 +0000

    home page dashboard internationalised --Jeri

--- modules/Dashboard/vertical_bargraph.php
+++ modules/Dashboard/vertical_bargraph.php
@@ -25,7 +25,7 @@ include_once ('Image/Canvas.php');
 function vertical_graph($referdata,$refer_code,$width,$height,$left,$right,$top,$bottom,$title,$target_val,$cache_file_name,$html_image_name)
 {
 
-	global $log,$root_directory,$lang_crm,$theme;
+	global $log,$root_directory,$lang_crm,$theme,$app_strings;
 //We'll be getting the values in the form of a string separated by commas
 	$datay=explode("::",$referdata); // The datay values  
 	$datax=explode("::",$refer_code); // The datax values  
@@ -38,12 +38,15 @@ function vertical_graph($referdata,$refer_code,$width,$height,$left,$right,$top,
 	$temp=array();
 	for($i=0;$i<count($datax);$i++)
 	{
-		$name=$datax[$i];
+		if($app_strings[$datax[$i]] != '') //HomePage Dashboard Strings i18nized - ahmed
+			$name=$app_strings[$datax[$i]];
+		else
+			$name=$datax[$i];
 		$pos = substr_count($name," ");
 		$alts[]=$name."=%d";
 //If the daatx value of a string is greater, adding '\n' to it so that it'll cme inh 2nd line
-		 if(strlen($name)>=14)
-                        $name=substr($name, 0, 14);
+		 if(strlen($name)>=15)
+                        $name=substr($name, 0, 15);
 		if($pos>=2)
 		{
 			$val=explode(" ",$name);
