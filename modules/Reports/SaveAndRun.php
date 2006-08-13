7d8c9735a6ca500d9a65409d60c20d15bf2851e5
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-13 17:48:13 +0000

    Header added for Permission denined page of reports--Jeri

--- modules/Reports/SaveAndRun.php
+++ modules/Reports/SaveAndRun.php
@@ -89,7 +89,25 @@ if(isPermitted($primarymodule,'index') == "yes" && (isPermitted($secondarymodule
 }
 else
 {
-	echo $mod_strings['LBL_NO_PERMISSION']." ".$primarymodule." ".$secondarymodule;
+	if($_REQUEST['mode'] != 'ajax')
+	{
+		include('themes/'.$theme.'/header.php');
+	}	
+	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
+	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
+
+		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
+		<tbody><tr>
+		<td rowspan='2' width='11%'><img src='themes/$theme/images/denied.gif' ></td>
+		<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'><span class='genHeaderSmall'>".$mod_strings['LBL_NO_PERMISSION']." ".$primarymodule." ".$secondarymodule."</span></td>
+		</tr>
+		<tr>
+		<td class='small' align='right' nowrap='nowrap'>			   	
+		<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br>								   		     </td>
+		</tr>
+		</tbody></table> 
+		</div>";
+	echo "</td></tr></table>";
 }
 
 	/** Function to get the StdfilterHTML strings for the given  primary module 
