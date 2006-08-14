03780bedb36c0278b51879d390c9325212f4ed3e
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-14 08:20:14 +0000

    css applied for undo last import --Jeri

--- modules/Import/ImportStepundo.php
+++ modules/Import/ImportStepundo.php
@@ -60,57 +60,52 @@ $ret_value = $last_import->undo($current_user->id);
 
 <br>
 
-<table width="100%" border=1>
-<table align="center" cellpadding="5" cellspacing="0" width="95%" class="leadTable">
-        <tr><br><br><br>
-         <td bgcolor="#FFFFFF" height="50" valign="middle" align="left" class="genHeaderSmall"> Import <?php echo $_REQUEST['module']; ?> </td>
+
+<table align="center" cellpadding="5" cellspacing="0" width="95%" class="mailClient importLeadUI small">
+        <tr>
+         <td bgcolor="#FFFFFF" height="50" valign="middle" align="left" class="mailClientBg genHeaderSmall"> Import <?php echo $_REQUEST['module']; ?> </td>
         </tr>
-                <tr bgcolor="#ECECEC"><td colspan="2">&nbsp;</td></tr>
-                <tr bgcolor="#ECECEC">
-                        <td align="left"  style="padding-left:40px;" colspan="2">
-                                <span class="genHeaderGray">Step 3 of 3 : </span>&nbsp;
-                                <span class="genHeaderSmall">Mapping Results </span>
-                        </td>
-                </tr>
-          <tr bgcolor="#ECECEC">
+		<tr><td>&nbsp;</td></tr>
+		<tr>
+				<td align="left"  style="padding-left:40px;">
+						<span class="genHeaderGray">Step 3 of 3 : </span>&nbsp;
+						<span class="genHeaderSmall">Mapping Results </span>
+				</td>
+		</tr>
+          <tr>
                 <td style="padding-left:140px;">
+					<br>
+					<?php 
+					if ($ret_value) {
+					?>
+					<?php echo "<b>" . $mod_strings['LBL_SUCCESS']."</b>" ?><BR><br>
+					<?php echo $mod_strings['LBL_LAST_IMPORT_UNDONE'] ?>
+					<?php 
+					} 
+					else 
+					{
+					?>
+					<?php echo $mod_strings['LBL_FAIL'] ?><br>
+					<?php echo $mod_strings['LBL_NO_IMPORT_TO_UNDO'] ?>
+					<?php
+					} 
+					?>
+					<br>
+					<br>
+				</td>
+			</tr>
+			<tr>
+				<td align="right" class="reportCreateBottom" >
+					<form name="Import" method="POST" action="index.php">
+					<input type="hidden" name="module" value="<?php echo $_REQUEST['module']; ?>">
+					<input type="hidden" name="action" value="Import">
+					<input type="hidden" name="step" value="1">
+					<input type="hidden" name="return_module" value="<?php echo $_REQUEST['RETURN_MODULE'] ?>">
+					<input type="hidden" name="return_id" value="<?php echo $_REQUEST['RETURN_ID'] ?>">
+					<input type="hidden" name="return_action" value="<?php echo $_REQUEST['RETURN_ACTION'] ?>">	
+					<input title="<?php echo $mod_strings['LBL_TRY_AGAIN'] ?>" accessKey="" class="crmbutton small save" type="submit" name="button" value="  <?php echo $mod_strings['LBL_TRY_AGAIN'] ?>  ">
+					</form>
+				</td>
+			</tr>
+		</table>
 
-
-<br>
-<?php 
-if ($ret_value) {
-?>
-<?php echo "<b>" . $mod_strings['LBL_SUCCESS']."</b>" ?><BR><br>
-<?php echo $mod_strings['LBL_LAST_IMPORT_UNDONE'] ?>
-<?php 
-} 
-else 
-{
-?>
-<?php echo $mod_strings['LBL_FAIL'] ?><br>
-<?php echo $mod_strings['LBL_NO_IMPORT_TO_UNDO'] ?>
-<?php
-} 
-?>
-<br>
-<br>
-</td>
-</tr>
-</table>
-<br>
-<form name="Import" method="POST" action="index.php">
-<input type="hidden" name="module" value="<?php echo $_REQUEST['module']; ?>">
-<input type="hidden" name="action" value="Import">
-<input type="hidden" name="step" value="1">
-<input type="hidden" name="return_module" value="<?php echo $_REQUEST['RETURN_MODULE'] ?>">
-<input type="hidden" name="return_id" value="<?php echo $_REQUEST['RETURN_ID'] ?>">
-<input type="hidden" name="return_action" value="<?php echo $_REQUEST['RETURN_ACTION'] ?>">
-
-<table width="100%" cellpadding="2" cellspacing="0" border="0"><tr>
-        <td align="right"><input title="<?php echo $mod_strings['LBL_TRY_AGAIN'] ?>" accessKey="" class="classBtn" type="submit" name="button" value="  <?php echo $mod_strings['LBL_TRY_AGAIN'] ?>  "></td>
-        <td></td>
-</tr>
-</table>
-        </form>
-
-</table>
