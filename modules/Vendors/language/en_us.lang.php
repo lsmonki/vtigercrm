45483fe7d43126f44ecef1acd48933e95d779c63
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-17 15:29:07 +0000

    Language strings updated for Vendors picklists -- Ahmed

--- modules/Vendors/language/en_us.lang.php
+++ modules/Vendors/language/en_us.lang.php
@@ -35,6 +35,18 @@ $mod_strings = array(
 'Po Box'=>'Po Box',
 'Vendor'=>'Vendor',
 
+//Added for existing Picklist entries
+
+'300-Sales-Software'=>'300-Sales-Software',
+'301-Sales-Hardware'=>'301-Sales-Hardware',
+'302-Rental-Income'=>'302-Rental-Income',
+'303-Interest-Income'=>'303-Interest-Income',
+'304-Sales-Software-Support'=>'304-Sales-Software-Support',
+'305-Sales Other'=>'305-Sales Other',
+'306-Internet Sales'=>'306-Internet Sales',
+'307-Service-Hardware Labor'=>'307-Service-Hardware Labor',
+'308-Sales-Books'=>'308-Sales-Books',
+
 
 );
 ?>
