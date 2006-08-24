a566ea5f069f93ca9539576698fd1989f1fa3153
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-24 05:34:33 +0000

    Updated the single pane view feature for detailview in PurchaseOrders Module -- ahmed

--- modules/PurchaseOrder/DetailView.php
+++ modules/PurchaseOrder/DetailView.php
@@ -26,7 +26,8 @@ require_once('modules/PurchaseOrder/PurchaseOrder.php');
 require_once('include/CustomFieldUtil.php');
 require_once('include/database/PearDatabase.php');
 require_once('include/utils/utils.php');
-global $mod_strings,$app_strings,$theme,$currentModule;
+require_once('user_privileges/default_module_view.php');
+global $mod_strings,$app_strings,$theme,$currentModule,$singlepane_view;
 
 $focus = new Order();
 
@@ -90,6 +91,15 @@ $smarty->assign("ASSOCIATED_PRODUCTS",getDetailAssociatedProducts('PurchaseOrder
 $check_button = Button_Check($module);
 $smarty->assign("CHECK", $check_button);
 $smarty->assign("EDIT_PERMISSION",isPermitted($currentModule,'EditView',$_REQUEST[record]));
+
+if($singlepane_view == 'true')
+{
+	$related_array = getRelatedLists($currentModule,$focus);
+	$smarty->assign("RELATEDLISTS", $related_array);
+}
+
+$smarty->assign("SinglePane_View", $singlepane_view);
+
 $smarty->display("Inventory/InventoryDetailView.tpl");
 
 
