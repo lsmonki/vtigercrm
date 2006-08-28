c33d4b70ebba70137abbc6ca1b18db61c9308b22
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-08-28 19:14:32 +0000

    Support for PostGres DB done

--- modules/SalesOrder/ListView.php
+++ modules/SalesOrder/ListView.php
@@ -14,7 +14,8 @@ require_once('modules/SalesOrder/SalesOrder.php');
 require_once('include/ListView/ListView.php');
 require_once('include/utils/utils.php');
 require_once('modules/CustomView/CustomView.php');
-
+require_once('include/database/Postgres8.php');
+require_once('include/DatabaseUtil.php');
 
 global $app_strings,$mod_strings,$list_max_entries_per_page,$currentModule,$theme;
 $theme_path="themes/".$theme."/";
@@ -114,20 +115,23 @@ if(isset($order_by) && $order_by != '')
 {
 	if($order_by == 'smownerid')
         {
+		if( $adb->dbType == "pgsql")
+ 		    $list_query .= ' GROUP BY user_name';
                 $list_query .= ' ORDER BY user_name '.$sorder;
         }
         else
         {
 		$tablename = getTableNameForField('SalesOrder',$order_by);
 		$tablename = (($tablename != '')?($tablename."."):'');
-
+		if( $adb->dbType == "pgsql")
+ 		    $list_query .= ' GROUP BY '.$tablename.$order_by;
                 $list_query .= ' ORDER BY '.$tablename.$order_by.' '.$sorder;
         }
 }
 
 
 //Retreiving the no of rows
-$count_result = $adb->query("select count(*) count ".substr($list_query, strpos($list_query,'FROM'),strlen($list_query)));
+$count_result = $adb->query( mkCountQuery( $list_query));
 $noofrows = $adb->query_result($count_result,0,"count");
 
 //Storing Listview session object
@@ -140,6 +144,11 @@ $start = $_SESSION['lvs'][$currentModule]['start'];
 
 //Retreive the Navigation array
 $navigation_array = getNavigationValues($start, $noofrows, $list_max_entries_per_page);
+ //Postgres 8 fixes
+ if( $adb->dbType == "pgsql")
+     $list_query = fixPostgresQuery( $list_query, $log, 0);
+
+
 
 // Setting the record count string
 //modified by rdhital
@@ -152,8 +161,13 @@ if ($start_rec ==0)
 	$limit_start_rec = 0;
 else
 	$limit_start_rec = $start_rec -1;
-	
-$list_result = $adb->query($list_query. " limit ".$limit_start_rec.",".$list_max_entries_per_page);
+
+if( $adb->dbType == "pgsql")
+     $list_result = $adb->query($list_query. " OFFSET ".$limit_start_rec." LIMIT ".$list_max_entries_per_page);
+else
+     $list_result = $adb->query($list_query. " LIMIT ".$limit_start_rec.",".$list_max_entries_per_page);
+
+
 
 $record_string= $app_strings[LBL_SHOWING]." " .$start_rec." - ".$end_rec." " .$app_strings[LBL_LIST_OF] ." ".$noofrows;
 
