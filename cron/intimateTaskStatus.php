8aafb112b5204a216279715bdc84203cf07f64cd
mmbrich <mmbrich@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-09-09 18:47:03 +0000

    changed this whole file to use ADODB instead of MySQL.  Also added some query change to give better ticket status results, prior to this is was returning false positives.

--- cron/intimateTaskStatus.php
+++ cron/intimateTaskStatus.php
@@ -1,46 +1,39 @@
 <?
+ini_set("include_path", "../:.");
+
 require('send_mail.php');
-require_once('../config.php');
+require_once('config.php');
+require_once('include/utils/utils.php');
 
-$dbhost = $dbconfig['db_hostname'];
-$dbuser =$dbconfig['db_username']; 
-$dbpass = $dbconfig['db_password'];
-$db = mysql_connect($dbhost, $dbuser, $dbpass) or die ('Error connecting to mysql');
-if (!$db) {
-  die('Could not connect: ' . mysql_error());
-}
-else
-{
-  echo 'Successfully connected to database '.$dbconfig['db_name'];
-}
-$selecteddb = $dbconfig['db_name'];
-mysql_select_db($selecteddb,$db);
-
-$emailresult = mysql_query("SELECT email1 from vtiger_users",$db);
-$emailid = mysql_fetch_row($emailresult);
+// Email Setup
+$emailresult = $adb->query("SELECT email1 from vtiger_users");
+$emailid = $adb->fetch_array($emailresult);
 $emailaddress = $emailid[0];
-$mailserveresult = mysql_query("SELECT server,server_username,server_password FROM vtiger_systems",$db);
-$mailrow = mysql_fetch_row($mailserveresult);
+$mailserveresult = $adb->query("SELECT server,server_username,server_password FROM vtiger_systems");
+$mailrow = $adb->fetch_array($mailserveresult);
 $mailserver = $mailrow[0];
 
 $mailuname = $mailrow[1];
 $mailpwd = $mailrow[1];
+// End Email Setup
+
 
 //query the vtiger_notificationscheduler vtiger_table and get data for those notifications which are active
-
-
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=1";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
+
 if($activevalue[0] == 1)
 {
 //Delayed Tasks Notification
 
 //get all those activities where the status is not completed even after the passing of 24 hours
 $today = date("Ymd"); 
-$result = mysql_query("select (vtiger_activity.date_start +1) from vtiger_activity where vtiger_activity.status !='Completed' and ".$today." > (vtiger_activity.date_start+1)",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("select (vtiger_activity.date_start +1) from vtiger_activity where vtiger_activity.status <> 'Completed' and ".$today." > (vtiger_activity.date_start+1)",$db);
+
+" > (vtiger_activity.date_start+1)";
+while ($myrow = $adb->fetch_array($result))
 {
   $status=$myrow[0];
   if($status != 'Completed')
@@ -52,13 +45,13 @@ while ($myrow = mysql_fetch_row($result))
 
 //Big Deal Alert
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=2";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
 if($activevalue[0] == 1)
 {
-$result = mysql_query("SELECT sales_stage,amount FROM vtiger_potential",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("SELECT sales_stage,amount FROM vtiger_potential",$db);
+while ($myrow = $adb->fetch_array($result))
 {
   $amount=$myrow[1];
   $stage = $myrow[0];
@@ -73,19 +66,19 @@ while ($myrow = mysql_fetch_row($result))
 
 //Pending tickets
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=3";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
 if($activevalue[0] == 1)
 {
-$result = mysql_query("SELECT status,ticketid FROM vtiger_troubletickets",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("SELECT vtiger_troubletickets.status,ticketid FROM vtiger_troubletickets INNER JOIN vtiger_crmentity ON vtiger_crmentity.crmid=vtiger_troubletickets.ticketid WHERE vtiger_crmentity.deleted='0' AND vtiger_troubletickets.status <> 'Completed' AND vtiger_troubletickets.status <> 'Closed' ",$db);
+
+while ($myrow = $adb->fetch_array($result))
 {
   $status=$myrow[0];
   $ticketid = $myrow[1];
-  if($status != 'Completed')
+  if($status != "Completed" || $status != "Closed")
   {
-
     sendmail($emailaddress,$emailaddress,"Pending Ticket notification","Dear Admin,<br> This is to bring to your kind attention that ticket number ".$ticketid ." is yet to be closed<br> Thank You,<br> HelpDesk Team<br>",$mailserver,$mailuname,$mailpwd,"");	
   }
 }
@@ -95,14 +88,14 @@ while ($myrow = mysql_fetch_row($result))
 
 //Too many tickets related to a particular vtiger_account/company causing concern
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=4";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
 if($activevalue[0] == 1)
 {
 
-$result = mysql_query("SELECT status,vtiger_troubletickets.ticketid FROM vtiger_troubletickets where status!='Completed'",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("SELECT status,vtiger_troubletickets.ticketid FROM vtiger_troubletickets where status <> 'Completed' AND status <> 'Closed'",$db);
+while ($myrow = $adb->fetch_array($result))
 {
   $status=$myrow[0];
   $ticketid = $myrow[1];
@@ -113,13 +106,13 @@ while ($myrow = mysql_fetch_row($result))
 
 //Support Starting
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=5";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
 if($activevalue[0] == 1)
 {
-$result = mysql_query("SELECT productname FROM vtiger_products where start_date like '".date('Y-m-d')."%'",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("SELECT productname FROM vtiger_products where start_date like '".date('Y-m-d')."%'",$db);
+while ($myrow = $adb->fetch_array($result))
 {
   $productname=$myrow[0];
   sendmail($emailaddress,$emailaddress,"Support starting","Hello! Support Starts for ".$productname ."\n Congratulations! Your support starts from today",$mailserver,$mailuname,$mailpwd,"");	
@@ -129,20 +122,18 @@ while ($myrow = mysql_fetch_row($result))
 
 //Support ending
 $sql = "select active from vtiger_notificationscheduler where schedulednotificationid=6";
-$result = mysql_query($sql);
+$result = $adb->query($sql);
 
-$activevalue = mysql_fetch_row($result);
+$activevalue = $adb->fetch_array($result);
 if($activevalue[0] == 1)
 {
 
-$result = mysql_query("SELECT productname from vtiger_products where expiry_date like '".date('Y-m-d')."%'",$db);
-while ($myrow = mysql_fetch_row($result))
+$result = $adb->query("SELECT productname from vtiger_products where expiry_date like '".date('Y-m-d')."%'",$db);
+while ($myrow = $adb->fetch_array($result))
 {
   $productname=$myrow[0];
   sendmail($emailaddress,$emailaddress,"Support Ending","Dear Admin,<br> This is to inform you that the support for ".$productname ."\n ends shortly. Kindly renew your support please<br>Regards,<br>HelpDesk Team<br>",$mailserver,$mailuname,$mailpwd,"");	
 }
 
 }
-
-
 ?>
