15e63f80f89ce7d92daab4b8eba6815c29f9e753
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-10-17 11:03:35 +0000

    * Modified the Activity history list display in account relatedlist, Fixes #2310

--- modules/Accounts/Accounts.php
+++ modules/Accounts/Accounts.php
@@ -294,7 +294,7 @@ class Accounts extends CRMEntity {
                 $log->debug("Entering get_history(".$id.") method ...");
 		$query = "SELECT vtiger_activity.activityid, vtiger_activity.subject,
 			vtiger_activity.status, vtiger_activity.eventstatus,
-			vtiger_activity.activitytype,
+			vtiger_activity.activitytype, vtiger_activity.date_start, vtiger_activity.due_date,
 			vtiger_crmentity.modifiedtime, vtiger_crmentity.createdtime,
 			vtiger_crmentity.description,
 			vtiger_users.user_name
