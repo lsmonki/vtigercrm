63ece59e1cb6156100d53a0638d72dc9dcab2be7
allanbush <allanbush@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-10-26 23:48:48 +0000

    Fixed calendar display issues with IE and select boxes not being hidden properly. Closes #35.

--- include/submenu.js
+++ include/submenu.js
@@ -6,8 +6,8 @@ function showSubMenu() {
 	getObj("subMenuBg").style.width=getObj("subMenu").offsetWidth
 	getObj("subMenuBg").style.height=getObj("subMenu").offsetHeight
 	
-	/*for (i=0;i<selTags.length;i++)
-		selTags[i].style.visibility="hidden"*/
+	for (i=0;i<selTags.length;i++)
+		selTags[i].style.visibility="hidden";
 }
 function hideSubMenu(ev) {
 	if (!ev) var obj = window.event.srcElement;
@@ -17,10 +17,10 @@ function hideSubMenu(ev) {
 		if (getObj("subMenu").style.display=="block") {
 			getObj("subMenuBg").style.display="none"
 			getObj("subMenu").style.display="none"
+			for (i=0;i<selTags.length;i++)
+				selTags[i].style.visibility="visible"
 		}
 	}
 	
-	for (i=0;i<selTags.length;i++)
-		selTags[i].style.visibility="visible"
 }
 document.onclick=hideSubMenu;
