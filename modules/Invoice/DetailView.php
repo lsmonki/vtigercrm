02fec13ddc47e90e9018149ea687d8fe527bb81b
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-11-08 07:43:37 +0000

    Hard Coded check for releated lists removed

--- modules/Invoice/DetailView.php
+++ modules/Invoice/DetailView.php
@@ -90,6 +90,7 @@ $smarty->assign("VALIDATION_DATA_FIELDDATATYPE",$data['datatype']);
 $smarty->assign("VALIDATION_DATA_FIELDLABEL",$data['fieldlabel']);
 $smarty->assign("EDIT_PERMISSION",isPermitted($currentModule,'EditView',$_REQUEST[record]));
 
+$smarty->assign("IS_REL_LIST",isPresentRelatedLists($currentModule));
 if($singlepane_view == 'true')
 {
 	$related_array = getRelatedLists($currentModule,$focus);
