5de37d88ac79a197af2f802e02e4d3cf900ff282
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2006-12-20 13:18:13 +0000

    * Modified to get the special character from from_html function, # Fixes 2759

--- include/utils/EditViewUtils.php
+++ include/utils/EditViewUtils.php
@@ -1449,7 +1449,7 @@ function getAssociatedProducts($module,$focus,$seid='')
 		}
 
 		$product_Detail[$i]['hdnProductId'.$i] = $hdnProductId;
-		$product_Detail[$i]['productName'.$i]= $productname;
+		$product_Detail[$i]['productName'.$i]= from_html($productname);
 		$product_Detail[$i]['productDescription'.$i]= $productdescription;
 		$product_Detail[$i]['comment'.$i]= $comment;
 
