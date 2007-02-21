fb60df6d4ad1dba8d97b2720a61366d2c6c129dd
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-02-21 10:25:52 +0000

    html tags added

--- include/utils/utils.php
+++ include/utils/utils.php
@@ -867,7 +867,7 @@ function to_html($string, $encode=true){
         global $toHtml;
         if($encode && is_string($string)){//$string = htmlentities($string, ENT_QUOTES);
 		if (is_array($toHtml))
-			$string =strip_tags($string, '<span><br /><div><a><br><b><u><i><table><td><tr><style><p><command>');
+			$string =strip_tags($string, '<span><br /><div><a><br><b><u><i><table><td><tr><style><p><command><h1><h2><h3><h4><h5><h6><li><ol><ul><th><tbody><font><center><big><hr><format> <strong><html><small>');
         }
 	$log->debug("Exiting to_html method ...");
         return $string;
