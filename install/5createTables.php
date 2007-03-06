fa1b9c06096618c5bce4b9881bab16f94bc9defd
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-03-06 09:37:46 +0000

    added links to forums and blogs

--- install/5createTables.php
+++ install/5createTables.php
@@ -130,6 +130,8 @@ if(!rename("install/", $renamefile."install/"))
 			<li>Your install.php file has been renamed to <?php echo $renamefile;?>install.php.txt.
 			<li>Your install folder too has been renamed to <?php echo $renamefile;?>install/.  
 			<li>Please log in using the "admin" user name and the password you entered in step 2.
+			<li>Talk to us at http://forums.vtiger.com
+			<li>Discuss with us at http://blogs.vtiger.com
 			</ul>
 			</div>
 
