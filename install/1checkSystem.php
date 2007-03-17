cea5c4613329e5b04277e816883b3961b439d9a0
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-03-17 09:04:23 +0000

    display of product name changed

--- install/1checkSystem.php
+++ install/1checkSystem.php
@@ -334,7 +334,7 @@ $array = Array(
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>Max Execution Time 600</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>output_buffering= On</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>Change the memory limit = 32M</strong></tr>
-				<tr bgcolor="#ffffff"> <td valign=top ><strong>error_reporting = E_WARNING & ~E_NOTICE</strong></tr>
+				<tr bgcolor="#ffffff"> <td valign=top ><strong>error_reporting = E_ALL & ~E_NOTICE</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>allow_call_time_reference = On</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>log_errors = Off</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>short_open_tag= On</strong></tr>
