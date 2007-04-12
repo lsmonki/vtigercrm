d2f8d43c3e7cdeaf16f4d28b01a29a0f1e62ec56
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-04-12 15:33:19 +0000

    * Removed the navigation and customview options from import listview because all entities are shown in a single shot. So no need for navigation and customview, Fixed #3354#3371

--- modules/Import/ImportSteplast.php
+++ modules/Import/ImportSteplast.php
@@ -220,8 +220,8 @@ foreach($import_modules_array as $module_name => $object_name)
 		//Retreive the List View Header and Entries
 		$listview_header = getListViewHeader($object,$module_name);
 		$listview_entries = getListViewEntries($object,$module_name,$list_result,$navigation_array,"","","EditView","Delete","");
-
-		$smarty->assign("NAVIGATION", $navigationOutput);
+		//commented to remove navigation buttons from import list view
+		//$smarty->assign("NAVIGATION", $navigationOutput);
 		$smarty->assign("HIDE_CUSTOM_LINKS", 1);//Added to hide the CustomView links in imported records ListView
 		$smarty->assign("LISTHEADER", $listview_header);
 		$smarty->assign("LISTENTITY", $listview_entries);
