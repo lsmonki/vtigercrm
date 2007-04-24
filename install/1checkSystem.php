408eefd29156eef773f329d9c9c3de4312309d85
saraj <saraj@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-04-24 15:21:07 +0000

    php ini values changed to recomended settings. Fixes #3473

--- install/1checkSystem.php
+++ install/1checkSystem.php
@@ -335,7 +335,7 @@ $array = Array(
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>output_buffering= On</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>Change the memory limit = 32M</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>error_reporting = E_ALL & ~E_NOTICE</strong></tr>
-				<tr bgcolor="#ffffff"> <td valign=top ><strong>allow_call_time_reference = On</strong></tr>
+				<tr bgcolor="#ffffff"> <td valign=top ><strong>allow_call_time_pass_reference = On</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>log_errors = Off</strong></tr>
 				<tr bgcolor="#ffffff"> <td valign=top ><strong>short_open_tag= On</strong></tr>
 				<tr bgcolor="#ffffff">  <td valign=top ><a href="http://www.vtiger.com/products/crm/help/vtiger_CRM_Linux_Dependencies.pdf" target="_blank">Linux installation pre-requisites</a></tr>
