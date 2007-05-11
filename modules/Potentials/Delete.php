43ee26c44eab210c95b35c3007d04d381ce12dbc
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-05-11 13:16:27 +0000

    * Fixed the issue in selecting the parenttab when we delete potential from account relatedlist

--- modules/Potentials/Delete.php
+++ modules/Potentials/Delete.php
@@ -29,5 +29,5 @@ if(!isset($_REQUEST['record']))
 
 DeleteEntity($_REQUEST['module'],$_REQUEST['return_module'],$focus,$_REQUEST['record'],$_REQUEST['return_id']);
 
-header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']."&relmodule=".$_REQUEST['module']);
+header("Location: index.php?module=".$_REQUEST['return_module']."&action=".$_REQUEST['return_action']."&record=".$_REQUEST['return_id']."&relmodule=".$_REQUEST['module']."&parenttab=".$_REQUEST['parenttab']);
 ?>
