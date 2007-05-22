a23494fd3d4a511bd9c0ced07139b721cd93bb86
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-05-22 21:23:59 +0000

    * Added order by in the query which cause problem after migration, ie., after done the migration Accounts comes as default first module in the array but the fields loaded are potentials, Fixed #3913

--- modules/Settings/PickList.php
+++ modules/Settings/PickList.php
@@ -129,7 +129,7 @@ function getPickListValues($tablename)
 function getPickListModules()
 {
 	global $adb;
-	$query = 'select distinct vtiger_field.fieldname,vtiger_field.tabid,tablabel,uitype from vtiger_field inner join vtiger_tab on vtiger_tab.tabid=vtiger_field.tabid where uitype IN (15,16, 111,33) and vtiger_field.tabid != 29';
+	$query = 'select distinct vtiger_field.fieldname,vtiger_field.tabid,tablabel,uitype from vtiger_field inner join vtiger_tab on vtiger_tab.tabid=vtiger_field.tabid where uitype IN (15,16, 111,33) and vtiger_field.tabid != 29 order by vtiger_field.tabid ASC';
 	$result = $adb->query($query);
 	while($row = $adb->fetch_array($result))
 	{
