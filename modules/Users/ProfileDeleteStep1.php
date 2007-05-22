5e8d3ad1f2aae550fd6dfd70e5f2b3b1fa26fad3
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-05-22 12:31:39 +0000

    fix for misplaced labels in Delete Profile Layer. Fixes #3855 --minnie

--- modules/Users/ProfileDeleteStep1.php
+++ modules/Users/ProfileDeleteStep1.php
@@ -40,11 +40,11 @@ $output ='<div id="DeleteLay" class="layerPopup">
 	<td class="small">
 	<table border=0 celspacing=0 cellpadding=5 width=100% align=center bgcolor=white>
 	<tr>
-		<td width="50%" class="cellLabel small"><b>'.$mod_strings["LBL_TRANSFER_ROLES_TO_PROFILE"].'</b></td>
+		<td width="50%" class="cellLabel small"><b>'.$mod_strings["LBL_PROFILE_TO_BE_DELETED"].'</b></td>
 		<td width="50%" class="cellText small"><b>'.$delete_prof_name.'</b></td>
 	</tr>
 	<tr>
-		<td align="left" class="cellLabel small" nowrap><b>'.$mod_strings["LBL_PROFILE_TO_BE_DELETED"].'</b></td>
+		<td align="left" class="cellLabel small" nowrap><b>'.$mod_strings["LBL_TRANSFER_ROLES_TO_PROFILE"].'</b></td>
 		<td align="left" class="cellText small">';
 		$output.='<select class="select" name="transfer_prof_id">';
 		global $adb;	
