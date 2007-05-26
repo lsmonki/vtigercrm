c6fef5ddcb43160cb1fc25d949c822c3c1e07662
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-05-26 15:56:39 +0000

    fixed issue - hyper link collapsed in email content view in email module for the mails qualified from Webmail module -Sri

--- include/utils/DetailViewUtils.php
+++ include/utils/DetailViewUtils.php
@@ -160,7 +160,7 @@ function getDetailViewOutputHtml($uitype, $fieldname, $fieldlabel, $col_fields,$
 	}
 	elseif($uitype == 19)
 	{
-		$col_fields[$fieldname]= make_clickable(str_replace("&lt;br /&gt;","<br>",$col_fields[$fieldname]));
+		$col_fields[$fieldname]= str_replace("&lt;br /&gt;","<br>",$col_fields[$fieldname]);
 		$label_fld[] = $mod_strings[$fieldlabel];
 		$label_fld[] = $col_fields[$fieldname];
 	}
