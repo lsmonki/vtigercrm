0e349eac709f1e714b78326ec2a4af6cdb0020cd
richie <richie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-05-27 18:52:06 +0000

    fixed - In Account related list added a new contact.The particular contact when we upload a contact image(.zip file) doesn't show any messages. -Sri

--- include/js/en_us.lang.js
+++ include/js/en_us.lang.js
@@ -129,6 +129,7 @@
 				IN_USERNAME :' in Username ',
 				LBL_ENTER_VALID_NO: 'Please enter valid number',
 				LBL_PROVIDE_YES_NO: ' Invalid value.\n Please Provide Yes or No',
-				LBL_SELECT_CRITERIA: ' Invalid criteria.\n Please select criteria'
+				LBL_SELECT_CRITERIA: ' Invalid criteria.\n Please select criteria',
+				LBL_WRONG_IMAGE_TYPE: 'Allowed file types for Contacts - jpeg, png, jpg, pjpeg, x-png or gif'
 
                        };
