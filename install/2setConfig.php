689997a3d83d3497555f1b313897680138d39f7f
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-09-17 13:35:09 +0000

    changes for UTF-8 support

--- install/2setConfig.php
+++ install/2setConfig.php
@@ -174,7 +174,7 @@ if (is_file("config.php") && is_file("config.inc.php")) {
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>vtiger CRM 5 - Configuration Wizard - System Configuration</title>
 	<link href="include/install/install.css" rel="stylesheet" type="text/css">
 </head>
