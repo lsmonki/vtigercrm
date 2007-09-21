1ff9670f4f22c9f07ef5e4eb5e123c6d62774ba2
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-09-21 12:22:26 +0000

    version name changed from 5.0.3 to 5.0.4

--- pkg/bin/startvTiger.sh
+++ pkg/bin/startvTiger.sh
@@ -111,14 +111,14 @@ if [ $exit_status -ne 0 ]; then
 		
 fi
 echo ""
-echo "Checking if the vtigercrm503 database already exists"
+echo "Checking if the vtigercrm504 database already exists"
 echo ""
-echo "select 1" | ./bin/mysql --user=$mysql_username --password=$mysql_password  --port=$mysql_port --socket=$mysql_socket -D vtigercrm503 >/dev/null
+echo "select 1" | ./bin/mysql --user=$mysql_username --password=$mysql_password  --port=$mysql_port --socket=$mysql_socket -D vtigercrm504 >/dev/null
 if [ $? -ne 0 ]; then
 	echo ""
-	echo "Database vtigercrm503 does not exist. Creating database vtigercrm503"
+	echo "Database vtigercrm504 does not exist. Creating database vtigercrm504"
 	echo ""
-	./bin/mysql --user=$mysql_username --password=$mysql_password  --port=$mysql_port --socket=$mysql_socket -e "create database if not exists vtigercrm503"
+	./bin/mysql --user=$mysql_username --password=$mysql_password  --port=$mysql_port --socket=$mysql_socket -e "create database if not exists vtigercrm504"
 fi
 
 host=`hostname`
