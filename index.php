226797e9bd5e9f0831bf0481e59f9baa4d378280
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2007-12-22 12:14:38 +0000

    Added to fix the pop up theme which is not applied when a module has no permission.

--- index.php
+++ index.php
@@ -589,6 +589,7 @@ else
 
 if($display == "no")
 {
+	echo "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";	
 	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
 	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
 
