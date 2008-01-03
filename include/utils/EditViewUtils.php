eb5c4be1ea3748d8f4d9a6a8d4ec38e048e06e5f
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-01-03 11:53:08 +0000

    Fix for quotes issue in Quotes/SO/PO/Invoice associated products

--- include/utils/EditViewUtils.php
+++ include/utils/EditViewUtils.php
@@ -1618,7 +1618,8 @@ function getAssociatedProducts($module,$focus,$seid='')
 		$product_Detail[$i]['hdnProductId'.$i] = $hdnProductId;
 		$product_Detail[$i]['productName'.$i]= from_html($productname);
 		/* Added to fix the issue Product Pop-up name display*/
-		$product_Detail[$i]['productName'.$i]= htmlspecialchars($product_Detail[$i]['productName'.$i]);
+		if($_REQUEST['action'] == 'CreateSOPDF' || $_REQUEST['action'] == 'CreatePDF' || $_REQUEST['action'] == 'SendPDFMail')
+			$product_Detail[$i]['productName'.$i]= htmlspecialchars($product_Detail[$i]['productName'.$i]);
 		$product_Detail[$i]['hdnProductcode'.$i] = $hdnProductcode;
 		$product_Detail[$i]['productDescription'.$i]= from_html($productdescription);
 		$product_Detail[$i]['comment'.$i]= $comment;
