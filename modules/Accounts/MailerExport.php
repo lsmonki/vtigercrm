b47850dd7367a4789ab8e651dca5b3dfb746dceb
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-01-22 08:14:19 +0000

    Fixed the issue - #4880 - Mailer Export not working with custom fields criteria -srini

--- modules/Accounts/MailerExport.php
+++ modules/Accounts/MailerExport.php
@@ -140,18 +140,19 @@ else
 {
   require_once('../../config.php');
   chdir ($root_directory);
+  require_once("include/utils/CommonUtils.php");
   require_once('include/database/PearDatabase.php');
   require_once('include/logging.php');
   $exquery = Array();
   $fields = explode(",",$_POST['fieldlist']);  
   $types = explode(",",$_POST['typelist']);  
   $escapxportWhere = mysql_real_escape_string($exportWhere);
-  if (($export_type == "email") || ($export_type == "emailplus"))
+  if (($export_type == "email") || ($export_type == "emailplus") )
   {
 	  
      $where = "";
 
-     foreach ($fields as $myField)
+     /*foreach ($fields as $myField)
      {
        $myType = each($types);
        if (strlen($_POST[$myField]) > 0)
@@ -173,8 +174,9 @@ else
           else
            $where .= " AND contactdetails.".$myField.$equals.$_POST[$myField].$postfix;
         }     
-     }
-	
+     }*/
+     	 if(count($fields) > 0)
+		 $where .= getExpWhereClause($fields,$types);
 	 $exquery[0] = "SELECT crmentity.crmid, contactdetails.contactid,
 	   contactdetails.salutation, contactdetails.firstname,
 	   contactdetails.lastname, contactdetails.email  FROM vtiger_account
@@ -203,22 +205,25 @@ else
 		    WHERE crmentity.deleted=0
 		    AND contactdetails.email = '' AND vtiger_account.email1 != '' ".$where;
 
-     if (strlen ($exportWhere))
-	        $exquery[1] .= " AND ".$exportWhere;
-	  } 
-	}
+     		if (strlen ($exportWhere))
+	        	$exquery[1] .= " AND ".$exportWhere;
+   } 
+  }
 	else if ($export_type == "full")
 	{
+		$where = '';
+		if(count($fields) > 0)
+		  $where .= getExpWhereClause($fields,$types);
 	  $exquery[0] = "select crmentity.crmid, contactdetails.contactid,
 	    contactdetails.salutation, contactdetails.firstname,
 	    contactdetails.lastname, contactdetails.email, vtiger_account.accountname,"
-	      ." vtiger_account.phone, vtiger_account.website, vtiger_accountshipads.ship_street AS shipstreet,"
-	      ." vtiger_accountshipads.ship_code AS shipcode,"
-	      ." vtiger_accountshipads.ship_city AS shipcity, vtiger_accountshipads.ship_state AS shipstate,"
-	      ." vtiger_accountshipads.ship_country AS shipcountry,"
-	      ." vtiger_accountbillads.bill_street AS billstreet, vtiger_accountbillads.bill_code AS billcode,"
-	      ." vtiger_accountbillads.bill_city AS billcity, vtiger_accountbillads.bill_state AS billstate,"
-	      ." vtiger_accountbillads.bill_country AS billcountry"
+	      ." vtiger_account.phone, vtiger_account.website, vtiger_accountshipads.ship_street,"
+	      ." vtiger_accountshipads.ship_code,"
+	      ." vtiger_accountshipads.ship_city, vtiger_accountshipads.ship_state,"
+	      ." vtiger_accountshipads.ship_country,"
+	      ." vtiger_accountbillads.bill_street, vtiger_accountbillads.bill_code,"
+	      ." vtiger_accountbillads.bill_city, vtiger_accountbillads.bill_state,"
+	      ." vtiger_accountbillads.bill_country"
 	      ." FROM vtiger_account INNER JOIN vtiger_crmentity crmentity ON crmentity.crmid=vtiger_account.accountid"
 	      ." LEFT JOIN vtiger_accountbillads ON vtiger_account.accountid=vtiger_accountbillads.accountaddressid"
 	      ." LEFT JOIN vtiger_accountshipads ON vtiger_account.accountid=vtiger_accountshipads.accountaddressid"
@@ -230,6 +235,26 @@ else
 	  if (strlen ($exportWhere))
 	      $exquery[0] .= " AND ".$exportWhere;
 	}
+  // Added for getting field labels of the fields selected in the above export queries. If any new fields added in the select clause then the below array need to be updated with the corresponding lablels
+  $fld_label_arr = Array("salutation"=>getTranslatedString('Salutation','Contacts'),
+	  		"lastname"=>getTranslatedString('Last Name','Contacts'),
+			"firstname"=>getTranslatedString('First Name','Contacts'),
+			"email"=>getTranslatedString('Email','Contacts'),
+			"accountname"=>getTranslatedString('Account Name','Accounts'),
+			"phone"=>getTranslatedString('Phone','Accounts'),
+			"website"=>getTranslatedString('Website','Accounts'),
+			"ship_street"=>getTranslatedString('Shipping Address','Accounts'),
+			"ship_code"=>getTranslatedString('Shipping Code','Accounts'),
+			"ship_city"=>getTranslatedString('Shipping City','Accounts'),
+			"ship_state"=>getTranslatedString('Shipping State','Accounts'),
+			"ship_country"=>getTranslatedString('Shipping Country','Accounts'),
+			"bill_street"=>getTranslatedString('Billing Address','Accounts'),
+			"bill_code"=>getTranslatedString('Billing Code','Accounts'),
+			"bill_city"=>getTranslatedString('Billing City','Accounts'),
+			"bill_state"=>getTranslatedString('Billing State','Accounts'),
+			"bill_country"=>getTranslatedString('Billing Country','Accounts')
+		);
+  
   for ($temp = 0; $temp < count($exquery); $temp++)
   {
 	  $result = $adb->query ($exquery[$temp],true,"Error exporting: "."<BR>$query");	  
@@ -246,6 +271,8 @@ else
 	        $fields_array[$arraywalk] = $name[$fields_array[$arraywalk]];
 	        // echo "Changing to: ".$fields_array[$arraywalk];
 	      }
+	      else
+		      $fields_array[$arraywalk] = ($fld_label_arr[$fields_array[$arraywalk]]!='')?$fld_label_arr[$fields_array[$arraywalk]]:$fields_array[$arraywalk];
 	    }
 
 		
@@ -286,5 +313,36 @@ else
 	print $content;
 	exit ();
 	
+}
+
+function getExpWhereClause($fields,$types)
+{
+	$where_cond = "";
+
+	foreach ($fields as $myField)
+	{
+		$myType = each($types);
+		if (strlen($_POST[$myField]) > 0)
+		{
+			// type 1 should use a LIKE search
+			if ($myType['value'] == 1)
+			{
+				$equals = " LIKE '";
+				$postfix = "%'";
+			}
+			else
+			{
+				$equals = " = '";
+				$postfix = "'";
+			}
+			// is customer field
+			if (substr($myField,0,3) == 'cf_')
+				$where_cond .= " AND contactscf.".$myField.$equals.$_POST[$myField].$postfix;
+			else
+				$where_cond .= " AND contactdetails.".$myField.$equals.$_POST[$myField].$postfix;
+		}
+	}
+	return $where_cond;
+
 }
 ?>
