1794e425b0b06c3bde9a3ff9e83a94fa9193dcf5
mangai <mangai@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-01-22 14:14:40 +0000

    Additoanl fix for the ticket #4866 -srini

--- modules/Reports/SaveAndRun.php
+++ modules/Reports/SaveAndRun.php
@@ -59,7 +59,7 @@ if($numOfRows > 0)
 		$BLOCK1 .= getSecondaryStdFilterHTML($ogReport->secmodule,$ogReport->stdselectedcolumn);
 		// Check if selectedcolumn is found in the filters (Fix for ticket #4866)
 		$selectedcolumnvalue = '"'. $ogReport->stdselectedcolumn . '"';
-		if (strpos($BLOCK1, $selectedcolumnvalue) === false) {
+		if (isset($ogReport->stdselectedcolumn) && strpos($BLOCK1, $selectedcolumnvalue) === false) {
 			$BLOCK1 .= "<option selected value=''>".$app_strings['LBL_NOT_ACCESSIBLE']."</option>";
 		}
 		$list_report_form->assign("BLOCK1",$BLOCK1);
