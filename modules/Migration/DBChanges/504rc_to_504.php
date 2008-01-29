424218255226ce75bdca0e9a53cbc8bbc91245ee
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-01-29 07:50:08 +0000

    Added migration changes for reports and customview tables for some calendar and campaign module fields

--- modules/Migration/DBChanges/504rc_to_504.php
+++ modules/Migration/DBChanges/504rc_to_504.php
@@ -49,12 +49,56 @@ ExecuteQuery("update vtiger_selectcolumn set columnname='vtiger_crmentity:descri
 ExecuteQuery("update vtiger_relcriteria set columnname='vtiger_crmentityHelpDesk:description:HelpDesk_Description:description:V' where columnname='vtiger_troubletickets:description:HelpDesk_Description:description:V'");
 ExecuteQuery("update vtiger_reportsortcol set columnname='vtiger_crmentityHelpDesk:description:HelpDesk_Description:description:V' where columnname='vtiger_troubletickets:description:HelpDesk_Description:description:V'");
 
-//Changed column recurringtype from recurringevents to activity table - cvcolumnlist, cvadvfilter && selectcolumn, relcriteria, reportsortcol - #4969
-ExecuteQuery("update vtiger_cvcolumnlist set columnname='vtiger_activity:recurringtype:recurringtype:Calendar_Recurrence:O' where columnname='vtiger_recurringevents:recurringtype:recurringtype:Calendar_Recurrence:V'");
-ExecuteQuery("update vtiger_cvadvfilter set columnname='vtiger_activity:recurringtype:recurringtype:Calendar_Recurrence:O' where columnname='vtiger_recurringevents:recurringtype:recurringtype:Calendar_Recurrence:V'");
-ExecuteQuery("update vtiger_selectcolumn set columnname='vtiger_activity:recurringtype:Calendar_Recurrence:recurringtype:O' where columnname='vtiger_recurringevents:recurringtype:Calendar_Recurrence:recurringtype:O'");
-ExecuteQuery("update vtiger_relcriteria set columnname='vtiger_activity:recurringtype:Calendar_Recurrence:recurringtype:O' where columnname='vtiger_recurringevents:recurringtype:Calendar_Recurrence:recurringtype:O'");
-ExecuteQuery("update vtiger_reportsortcol set columnname='vtiger_activity:recurringtype:Calendar_Recurrence:recurringtype:O' where columnname='vtiger_recurringevents:recurringtype:Calendar_Recurrence:recurringtype:O'");
+//Some fields in customview related tables are changed in latest version but not handled in migration
+//Array format is -- oldvalue => newvalue - old values will be updated with new values
+//customview related tables to be changed - cvcolumnlist, cvadvfilter
+$cv_fields_list = Array(
+			//campaigns module
+			"vtiger_campaign:product_id:product_id:Campaigns_Product:V"=>"vtiger_products:productname:productname:Campaigns_Product:V",
+			"vtiger_campaign:targetsize:targetsize:Campaigns_TargetSize:N"=>"vtiger_campaign:targetsize:targetsize:Campaigns_TargetSize:I",
+			"vtiger_campaign:budgetcost:budgetcost:Campaigns_Budget_Cost:I"=>"vtiger_campaign:budgetcost:budgetcost:Campaigns_Budget_Cost:N",
+			"vtiger_campaign:actualcost:actualcost:Campaigns_Actual_Cost:I"=>"vtiger_campaign:actualcost:actualcost:Campaigns_Actual_Cost:N",
+			"vtiger_campaign:expectedrevenue:expectedrevenue:Campaigns_Expected_Revenue:I"=>"vtiger_campaign:expectedrevenue:expectedrevenue:Campaigns_Expected_Revenue:N",
+			"vtiger_campaign:expectedsalescount:expectedsalescount:Campaigns_Expected_Sales_Count:N"=>"vtiger_campaign:expectedsalescount:expectedsalescount:Campaigns_Expected_Sales_Count:I",
+			"vtiger_campaign:actualsalescount:actualsalescount:Campaigns_Actual_Sales_Count:N"=>"vtiger_campaign:actualsalescount:actualsalescount:Campaigns_Actual_Sales_Count:I",
+			//calendar module
+			"vtiger_recurringevents:recurringtype:recurringtype:Calendar_Recurrence:V"=>"vtiger_activity:recurringtype:recurringtype:Calendar_Recurrence:O",
+			"vtiger_activity:time_start::Calendar_Start_Time:V"=>"vtiger_activity:time_start::Calendar_Start_Time:I",
+			"vtiger_activity:time_end:time_end:Calendar_End_Time:V"=>"vtiger_activity:time_end:time_end:Calendar_End_Time:T",
+		  );
+
+foreach($cv_fields_list as $oldval => $newval)
+{
+	ExecuteQuery("update vtiger_cvcolumnlist set columnname='$newval' where columnname = '$oldval'");
+	ExecuteQuery("update vtiger_cvadvfilter set columnname='$newval' where columnname = '$oldval'");
+}
+
+//Some fields in report related tables are changed in latest version but not handled in migration
+//Report related tables to be changed - selectcolumn, relcriteria, reportsortcol
+//Array format is -- oldvalue => newvalue - old values will be updated with new values
+$report_fields_list = Array(
+			//Calendar module
+			"vtiger_recurringevents:recurringtype:Calendar_Recurrence:recurringtype:O"=>"vtiger_activity:recurringtype:Calendar_Recurrence:recurringtype:O",
+			//Campaign module
+			"vtiger_campaign:targetsize:Campaigns_TargetSize:targetsize:N"=>"vtiger_campaign:targetsize:Campaigns_TargetSize:targetsize:I",
+			"vtiger_campaign:budgetcost:Campaigns_Budget_Cost:budgetcost:I"=>"vtiger_campaign:budgetcost:Campaigns_Budget_Cost:budgetcost:N",
+			"vtiger_campaign:actualcost:Campaigns_Actual_Cost:actualcost:I"=>"vtiger_campaign:actualcost:Campaigns_Actual_Cost:actualcost:N",
+			"vtiger_campaign:expectedrevenue:Campaigns_Expected_Revenue:expectedrevenue:I"=>"vtiger_campaign:expectedrevenue:Campaigns_Expected_Revenue:expectedrevenue:N",
+			"vtiger_campaign:expectedsalescount:Campaigns_Expected_Sales_Count:expectedsalescount:N"=>"vtiger_campaign:expectedsalescount:Campaigns_Expected_Sales_Count:expectedsalescount:I",
+			"vtiger_campaign:actualsalescount:Campaigns_Actual_Sales_Count:actualsalescount:N"=>"vtiger_campaign:actualsalescount:Campaigns_Actual_Sales_Count:actualsalescount:I",
+			"vtiger_campaign:expectedresponsecount:Campaigns_Expected_Response_Count:expectedresponsecount:N"=>"vtiger_campaign:expectedresponsecount:Campaigns_Expected_Response_Count:expectedresponsecount:I",
+			"vtiger_campaign:actualresponsecount:Campaigns_Actual_Response_Count:actualresponsecount:N"=>"vtiger_campaign:actualresponsecount:Campaigns_Actual_Response_Count:actualresponsecount:I",
+			"vtiger_crmentityRelCalendar:setype:Calendar_Related_To:parent_id:I"=>"vtiger_crmentityRelCalendar:setype:Calendar_Related_To:parent_id:V",
+			"vtiger_contactdetailsCalendar:lastname:Calendar_Contact_Name:contact_id:I"=>"vtiger_contactdetailsCalendar:lastname:Calendar_Contact_Name:contact_id:V",
+			   );
+
+foreach($report_fields_list as $oldval => $newval)
+{
+	ExecuteQuery("update vtiger_selectcolumn set columnname='$newval' where columnname='$oldval'");
+	ExecuteQuery("update vtiger_relcriteria set columnname='$newval' where columnname='$oldval'");
+	ExecuteQuery("update vtiger_reportsortcol set columnname='$newval' where columnname='$oldval'");
+}
+
 
 //we have removed the Team field in quotes and added a new custom field for Team. So we can remove that field from reports (we have changed this field name in customview related tables in 503 - 504rc migration)
 ExecuteQuery("delete from vtiger_selectcolumn where columnname='vtiger_quotes:team:team:Quotes_Team:V'");
@@ -81,6 +125,9 @@ function update_webmail_password()
 ExecuteQuery("alter table vtiger_systems change  column server_username server_username varchar(100)");
 ExecuteQuery("alter table vtiger_systems change  column server server varchar(100)");
 ExecuteQuery("alter table vtiger_systems change  column server_password server_password varchar(100)");
+
+
+
 
 $migrationlog->debug("\n\nDB Changes from 5.0.4rc to 5.0.4 -------- Ends \n\n");
 
