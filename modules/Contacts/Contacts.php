c735c1f2fbf5b240c0bb6507a87163f4617ce4ef
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-02-08 11:48:44 +0000

    Fixed the issue in repeated fields for non admin users in office plugin - Fixed #5107

--- modules/Contacts/Contacts.php
+++ modules/Contacts/Contacts.php
@@ -730,10 +730,10 @@ function getColumnNames()
 	}else
 	{
 	 $profileList = getCurrentUserProfileList();
-	 $sql1 = "select fieldlabel from vtiger_field inner join vtiger_profile2field on vtiger_profile2field.fieldid=vtiger_field.fieldid inner join vtiger_def_org_field on vtiger_def_org_field.fieldid=vtiger_field.fieldid where vtiger_field.tabid=4 and vtiger_field.block <> 75 and vtiger_field.displaytype in (1,2,4,3) and vtiger_profile2field.visible=0 and vtiger_def_org_field.visible=0";
+	 $sql1 = "select vtiger_field.fieldid,fieldlabel from vtiger_field inner join vtiger_profile2field on vtiger_profile2field.fieldid=vtiger_field.fieldid inner join vtiger_def_org_field on vtiger_def_org_field.fieldid=vtiger_field.fieldid where vtiger_field.tabid=4 and vtiger_field.block <> 75 and vtiger_field.displaytype in (1,2,4,3) and vtiger_profile2field.visible=0 and vtiger_def_org_field.visible=0";
 	 $params1 = array();
 	 if (count($profileList) > 0) {
-	 	$sql1 .= " and vtiger_profile2field.profileid in (". generateQuestionMarks($profileList) .")";
+	 	$sql1 .= " and vtiger_profile2field.profileid in (". generateQuestionMarks($profileList) .") group by fieldid";
   	 	array_push($params1, $profileList);
 	 }
   }
