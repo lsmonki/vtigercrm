f762efd01baed8456e2b66c83e5c434c93322c9d
mickie <mickie@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-03-06 15:24:43 +0000

    ticket #5204 - security issue in calendar -srini

--- modules/Calendar/Save.php
+++ modules/Calendar/Save.php
@@ -60,7 +60,29 @@ if((isset($_REQUEST['change_status']) && $_REQUEST['change_status']) && ($_REQUE
 		$status = $_REQUEST['eventstatus'];	
 		$activity_type = "Events";	
 	}
-	ChangeStatus($status,$return_id,$activity_type);
+	if(isPermitted("Calendar","EditView",$_REQUEST['record']) == 'yes')
+	{
+		ChangeStatus($status,$return_id,$activity_type);
+	}
+	else
+	{
+		echo "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";	
+		echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
+		echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
+
+			<table border='0' cellpadding='5' cellspacing='0' width='98%'>
+			<tbody><tr>
+			<td rowspan='2' width='11%'><img src='themes/$theme/images/denied.gif' ></td>
+			<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'><span class='genHeaderSmall'>$app_strings[LBL_PERMISSION]</span></td>
+			</tr>
+			<tr>
+			<td class='small' align='right' nowrap='nowrap'>			   	
+			<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br>								   						     </td>
+			</tr>
+			</tbody></table> 
+		</div>";
+		echo "</td></tr></table>";die;
+	}
 	$mail_data = getActivityMailInfo($return_id,$status,$activity_type);
 	if($mail_data['sendnotification'] == 1)
 	{
