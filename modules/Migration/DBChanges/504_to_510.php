2b874e0b86d53fce5dea4cb327092186e5c39dae
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2008-11-04 08:34:19 +0000

    #5392 - Bug fixes for the migration script of Adding Custom Events

--- modules/Migration/DBChanges/504_to_510.php
+++ modules/Migration/DBChanges/504_to_510.php
@@ -670,15 +670,13 @@ webserviceMigration();
 ExecuteQuery("create table vtiger_eventhandlers (eventhandler_id int, event_name varchar(100), handler_path varchar(400), handler_class varchar(100), primary key(eventhandler_id))");
 
 /* Adding Custom Events Migration */
-ExecuteQuery("UPDATE table vtiger_field SET uitype=111,typeofdata='V~M' WHERE tabid=16 and columnname='activitytype'");
+ExecuteQuery("UPDATE vtiger_field SET uitype=111,typeofdata='V~M' WHERE tabid=16 and columnname='activitytype'");
 ExecuteQuery("alter table vtiger_activitytype drop column sortorderid");
-ExecuteQuery("alter table vtiger_activitytype add column picklist_valueid");
+ExecuteQuery("alter table vtiger_activitytype add column picklist_valueid int(19) NOT NULL default '0'");
 
 $query = $adb->pquery("SELECT * from vtiger_activitytype",array());
 for($i=0;$i<$adb->num_rows($query);$i++){
-	$adb->pquery("UPDATE vtiger_picklistvalues_seq SET id = id + 1");
-	$picklist_valueid_query = $adb->pquery("SELECT id from vtiger_picklistvalues_seq");
-	$picklist_valueid = $adb->query_result($picklist_valueid,0,'id');
+	$picklist_valueid = $adb->getUniqueID('vtiger_picklistvalues');
 	$activitytypeid = $adb->query_result($query,$i,'activitytypeid');
 	$adb->pquery("UPDATE vtiger_activitytype SET picklist_valueid=? , presence=0 WHERE activitytypeid = ? ",array($picklist_valueid,$activitytypeid));
 }
