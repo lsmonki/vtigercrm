4d908c50c0b92f6f1825f89905b3ea7b9219b6a3
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-01-13 15:13:50 +0000

    Fixed an issue with Workflow - Sidharth

--- modules/com_vtiger_workflow/resources/vtigerwebservices.js
+++ modules/com_vtiger_workflow/resources/vtigerwebservices.js
@@ -3,7 +3,7 @@ function vtigerwebservicesproto(){
 	function md5(str){
 		return hex_md5(str);
 	}
-	
+
 	function mergeObjects(obj1, obj2){
 		var res = {};
 		for(var k in obj1){
@@ -14,24 +14,24 @@ function vtigerwebservicesproto(){
 		}
 		return res;
 	}
-	
+
 	function doGet(params, callback){
 		$.get(this.serviceUrl, params, function(result){
 			var parsed = JSON.parse(result);
 			callback(parsed);
 		});
 	}
-	
+
 	function doPost(params, callback){
 		$.post(this.serviceUrl, params, function(result){
 			var parsed = JSON.parse(result);
 			callback(parsed);
 		});
 	}
-	
-	
+
+
 	function get(operation, parameters, callback){
-		response = this.doGet(mergeObjects(parameters, 
+		response = this.doGet(mergeObjects(parameters,
 			{'operation':operation, 'sessionName':this.sessionId}), function(response){
 			if(response['success']==true){
 				callback(true,response['result']);
@@ -40,9 +40,9 @@ function vtigerwebservicesproto(){
 			}
 		});
 	}
-	
+
 	function post(operation, parameters, callback){
-		response = this.doPost(mergeObjects(parameters, 
+		response = this.doPost(mergeObjects(parameters,
 			{'operation':operation, 'sessionName':this.sessionId}), function(response){
 			if(response['success']==true){
 				callback(true,response['result']);
@@ -51,8 +51,8 @@ function vtigerwebservicesproto(){
 			}
 		});
 	}
-	
-	
+
+
 	function login(callback){
 		var self = this;
 		response = this.doGet({operation:'getchallenge', username:this.username}, function(response){
@@ -61,7 +61,7 @@ function vtigerwebservicesproto(){
 				var encodedKey = md5(token+self.accessKey);
 				self.doPost({operation:'login', username: self.username, accessKey: encodedKey}, function (response){
 					if(response['success']==true){
-						self.sessionId = response['result']['sessionId'];
+						self.sessionId = response['result']['sessionName'];
 						self.userId = response['result']['userId'];
 						callback(true);
 					}else{
@@ -73,12 +73,12 @@ function vtigerwebservicesproto(){
 			}
 		});
 	}
-	
-	
+
+
 	function logout(callback){
 		this.post('logout', {}, callback);
 	}
-	
+
 	function listTypes(callback){
 		this.get('listtypes', {}, function (status, result){
 			if(status){
@@ -88,11 +88,11 @@ function vtigerwebservicesproto(){
 			}
 		});
 	}
-	
+
 	function describeObject(name, callback){
-		this.get('describeobject', {'elementType':name}, callback);
+		this.get('describe', {'elementType':name}, callback);
 	}
-	
+
 	function create(object, objectType, callback){
 		if(object['assigned_user_id']==null){
 			object['assigned_user_id'] = this.userId;
@@ -101,28 +101,28 @@ function vtigerwebservicesproto(){
 		this.post('create', {'elementType':objectType,
 			'element':objectJson}, callback);
 	}
-	
+
 	function retrieve(id, callback){
 		this.get('retrieve', {'id':id}, callback);
 	}
-	
+
 	function update(object, callback){
 		objectJson = JSON.encode(object);
 		this.post('update', {'element':objectJson}, callback);
 	}
-	
-	
+
+
 	function deleteObject(id, callback){
 		this.post('delete', {'id':id}, callback);
 	}
-	
+
 	function query(query, callback){
 		this.get('query', {'query':query}, callback);
 	}
-	
+
 	function extendSession(callback){
 		var self = this;
-		this.doGet({operation: 'extendsession'}, function(response){
+		this.doPost({operation: 'extendsession'}, function(response){
 			var status = response['success'];
 			var result = response['result'];
 			if(status==true){
@@ -135,7 +135,7 @@ function vtigerwebservicesproto(){
 
 		});
 	}
-	
+
 	return {
 		doPost:doPost, doGet:doGet,
 		get:get, post:post,
