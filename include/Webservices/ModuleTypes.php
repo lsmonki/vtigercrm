372caff2c15dc25586837f88db4c7b8d9b6415f6
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-01-20 12:29:10 +0000

    Workflow issues fixed - Sidharth =================================== 1. Fixed issue with Multi-select combo field. 2. Fixed issue with creating a Task of type Todo.

--- include/Webservices/ModuleTypes.php
+++ include/Webservices/ModuleTypes.php
@@ -6,10 +6,6 @@
 			$allModuleNames = getPermittedModuleNames();
 			//get All the CRM entity names.
 			$crmEntityNames = vtws_getModuleNameList();
-			unset($crmEntityNames[array_search('Reports',$crmEntityNames)]);
-			if(strcasecmp('on',$user->is_admin)===0){
-				array_push($crmEntityNames,"Users");
-			}
 		}catch(Exception $exception){
 			return new WebServiceError(WebServiceErrorCode::$DATABASEQUERYERROR,
 											"An Database error occured while performing the operation");
