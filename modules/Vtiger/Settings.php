d039b5101af46293a7a2b3fe8cdc662f537379ea
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-03-19 14:10:51 +0000

    vtlib module security issue fix - Prasad =========================================
    1. Admin access check was added in modules/Vtiger/Settings.php.
    
    2. Generic OperationNotPermitted smarty template was added to modules/Vtiger for reuse purpose

--- modules/Vtiger/Settings.php
+++ modules/Vtiger/Settings.php
@@ -14,53 +14,59 @@ require_once("include/utils/utils.php");
 
 global $mod_strings, $app_strings, $theme, $adb;
 $smarty = new vtigerCRM_Smarty;
-
-$module = $_REQUEST['formodule'];
-
-$menu_array = Array();
-
-//if(layout editor is permitted)
-$menu_array['LayoutEditor']['location'] = 'index.php?module=Settings&action=LayoutBlockList&parenttab=Settings&formodule='.$module;
-$menu_array['LayoutEditor']['image_src'] = 'themes/images/orgshar.gif';
-$menu_array['LayoutEditor']['desc'] = getTranslatedString('LBL_LAYOUT_EDITOR_DESCRIPTION');
-$menu_array['LayoutEditor']['label'] = getTranslatedString('LBL_LAYOUT_EDITOR');
-
-if(vtlib_isModuleActive('FieldFormulas')) {
-	$modules = com_vtGetModules($adb);
-	if(in_array(getTranslatedString($module),$modules)) {
-		$sql_result = $adb->pquery("select * from vtiger_settings_field where name = ?",array('Field Formulas'));
-		if($adb->num_rows($sql_result) > 0) {
-			$menu_array['FieldFormulas']['location'] = $adb->query_result($sql_result, 0, 'linkto').'&formodule='.$module;
-			$menu_array['FieldFormulas']['image_src'] = $adb->query_result($sql_result, 0, 'iconpath');
-			$menu_array['FieldFormulas']['desc'] = $adb->query_result($sql_result, 0, 'description');
-			$menu_array['FieldFormulas']['label'] = $adb->query_result($sql_result, 0, 'name');
-		}
-	}
-}
-
-$sql_result = $adb->pquery("select * from vtiger_settings_field where name = ?",array('LBL_TOOLTIP_MANAGEMENT'));
-if($adb->num_rows($sql_result) > 0) {
-	$menu_array['Tooltip']['location'] = $adb->query_result($sql_result, 0, 'linkto').'&formodule='.$module;
-	$menu_array['Tooltip']['image_src'] = vtiger_imageurl($adb->query_result($sql_result, 0, 'iconpath'), $theme);
-	$menu_array['Tooltip']['desc'] = $mod_strings[$adb->query_result($sql_result, 0, 'description')];
-	$menu_array['Tooltip']['label'] = $mod_strings[$adb->query_result($sql_result, 0, 'name')];
-}
-
-//add blanks for 3-column layout
-$count = count($menu_array)%3;
-if($count>0) {
-	for($i=0;$i<3-$count;$i++) {
-		$menu_array[] = array();
-	}
-}
-
 $smarty->assign("MOD",$mod_strings);
 $smarty->assign("APP",$app_strings);
+$smarty->assign("THEME", "$theme");
 $smarty->assign("IMAGE_PATH", "themes/$theme/images/");
-$smarty->assign('MODULE',$module);
-$smarty->assign('MODULE_LBL',getTranslatedString($module));
-$smarty->assign('MENU_ARRAY', $menu_array);
 
-$smarty->display(vtlib_getModuleTemplate('Vtiger','Settings.tpl'));
+// Operation to be restricted for non-admin users.
+global $current_user;
+if(!is_admin($current_user)) {	
+	$smarty->display(vtlib_getModuleTemplate('Vtiger','OperationNotPermitted.tpl'));	
+} else {
+	$module = $_REQUEST['formodule'];
 
+	$menu_array = Array();
+	
+	//if(layout editor is permitted)
+	$menu_array['LayoutEditor']['location'] = 'index.php?module=Settings&action=LayoutBlockList&parenttab=Settings&formodule='.$module;
+	$menu_array['LayoutEditor']['image_src'] = 'themes/images/orgshar.gif';
+	$menu_array['LayoutEditor']['desc'] = getTranslatedString('LBL_LAYOUT_EDITOR_DESCRIPTION');
+	$menu_array['LayoutEditor']['label'] = getTranslatedString('LBL_LAYOUT_EDITOR');
+	
+	if(vtlib_isModuleActive('FieldFormulas')) {
+		$modules = com_vtGetModules($adb);
+		if(in_array(getTranslatedString($module),$modules)) {
+			$sql_result = $adb->pquery("select * from vtiger_settings_field where name = ? and active=0",array('Field Formulas'));
+			if($adb->num_rows($sql_result) > 0) {
+				$menu_array['FieldFormulas']['location'] = $adb->query_result($sql_result, 0, 'linkto').'&formodule='.$module;
+				$menu_array['FieldFormulas']['image_src'] = $adb->query_result($sql_result, 0, 'iconpath');
+				$menu_array['FieldFormulas']['desc'] = $adb->query_result($sql_result, 0, 'description');
+				$menu_array['FieldFormulas']['label'] = $adb->query_result($sql_result, 0, 'name');
+			}
+		}
+	}
+	
+	$sql_result = $adb->pquery("select * from vtiger_settings_field where name = ? and active=0",array('LBL_TOOLTIP_MANAGEMENT'));
+	if($adb->num_rows($sql_result) > 0) {
+		$menu_array['Tooltip']['location'] = $adb->query_result($sql_result, 0, 'linkto').'&formodule='.$module;
+		$menu_array['Tooltip']['image_src'] = vtiger_imageurl($adb->query_result($sql_result, 0, 'iconpath'), $theme);
+		$menu_array['Tooltip']['desc'] = $mod_strings[$adb->query_result($sql_result, 0, 'description')];
+		$menu_array['Tooltip']['label'] = $mod_strings[$adb->query_result($sql_result, 0, 'name')];
+	}
+	
+	//add blanks for 3-column layout
+	$count = count($menu_array)%3;
+	if($count>0) {
+		for($i=0;$i<3-$count;$i++) {
+			$menu_array[] = array();
+		}
+	}
+	
+	$smarty->assign('MODULE',$module);
+	$smarty->assign('MODULE_LBL',getTranslatedString($module));
+	$smarty->assign('MENU_ARRAY', $menu_array);
+
+	$smarty->display(vtlib_getModuleTemplate('Vtiger','Settings.tpl'));
+}
 ?>
