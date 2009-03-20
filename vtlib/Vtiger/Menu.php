4a3c974e83da9005715b7f10bc7c54f832db1ec9
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-03-20 15:57:14 +0000

    Vtlib Changes - Prasad =======================
    1. module disabling option should be given from menu.

--- vtlib/Vtiger/Menu.php
+++ vtlib/Vtiger/Menu.php
@@ -66,6 +66,26 @@ class Vtiger_Menu {
 		}
 		self::syncfile();
 	}
+	
+	/**
+	 * Remove module from this menu instance.
+	 * @param Vtiger_Module Instance of the module
+	 */
+	function removeModule($moduleInstance) {
+		if(empty($moduleInstance) || empty($moduleInstance)) {
+			self::log("Module instance is not set!");
+			return;
+		}
+		if($this->id) {
+			global $adb;
+			$adb->pquery("DELETE FROM vtiger_parenttabrel WHERE parenttabid = ? AND tabid = ?",
+					Array($this->id, $moduleInstance->id));
+			self::log("Removed $moduleInstance->name from menu $this->label ... DONE");
+		} else {
+			self::log("Menu could not be found!");
+		}
+		self::syncfile();
+	}
 
 	/**
 	 * Detach module from menu
