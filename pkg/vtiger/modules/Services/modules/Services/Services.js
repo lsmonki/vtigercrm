74d70f0c5a7be7c807c9f9e0235048b28c64858c
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-04-09 14:57:57 +0000

    #5799 : Line Item Ording Bug Fix - Prasad ==========================================
    1. Current row id reference was wrong after swapping rows.

--- pkg/vtiger/modules/Services/modules/Services/Services.js
+++ pkg/vtiger/modules/Services/modules/Services/Services.js
@@ -308,7 +308,15 @@ function fnAddServiceRow(module,image_path){
 
 function servicePickList(currObj,module, row_no) {
 	var trObj=currObj.parentNode.parentNode
-	var rowId=row_no;//parseInt(trObj.id.substr(trObj.id.indexOf("w")+1,trObj.id.length))
+	
+	var rowId = row_no;
+	var currentRowId = parseInt(currObj.id.match(/([0-9]+)$/)[1]);
+	
+	// If we have mismatching rowId and currentRowId, it is due swapping of rows
+	if(rowId != currentRowId) {
+		rowId = currentRowId;
+	}
+
 	var currencyid = document.getElementById("inventory_currency").value;
 
 	popuptype = 'inventory_service';
