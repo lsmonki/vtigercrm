435e9d485fe1539e43268d1db764d9908b5cea7f
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2009-05-26 06:49:31 +0000

    #5705: Popup window message fixed when no records are imported - Prasad

--- modules/Import/lastImport.php
+++ modules/Import/lastImport.php
@@ -121,8 +121,23 @@ foreach($import_modules_array as $module_name => $object_name)
 	//Retreiving the no of rows
 	$noofrows = $adb->num_rows($list_result);
 
-	if($noofrows>=1) 
-	{
+	if($noofrows < 1) {
+		if($module_name == $_REQUEST['req_mod']) {
+			echo "<link rel='stylesheet' type='text/css' href='themes/$theme/style.css'>";
+			echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
+			echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
+		
+				<table border='0' cellpadding='5' cellspacing='0' width='98%'>
+				<tbody><tr>
+				<td rowspan='2' width='11%'><img src='". vtiger_imageurl('empty.jpg', $theme) ."' ></td>
+				<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'>
+					<span class='genHeaderSmall'>$app_strings[LBL_NO] $mod_strings[LBL_LAST_IMPORTED] $app_strings[$module_name]</span></td>
+				</tr>
+				</tbody></table>
+				</div>";
+			echo "</td></tr></table>";
+		}
+	} else  {
 		if($module_name != 'Accounts')
 		{
 			$implict_account=true;
