f37fa8e682fdf3c9f89241eeef6c7e972c147218
musavir <musavir@83ef7b00-670c-0410-b3e3-e45103aa6900>
2010-06-03 15:48:25 +0000

    #6719 Outlook Sync Bug Fix - Praneeth ===============================
    1. fixed an issue where, contacts sync with outlook was not working.

--- modules/Contacts/Contacts.php
+++ modules/Contacts/Contacts.php
@@ -1109,6 +1109,8 @@ function get_contactsforol($user_name)
 						left join vtiger_account on vtiger_account.accountid=vtiger_contactdetails.accountid 
 						left join vtiger_contactaddress on vtiger_contactaddress.contactaddressid=vtiger_contactdetails.contactid 
 						left join vtiger_contactsubdetails on vtiger_contactsubdetails.contactsubscriptionid = vtiger_contactdetails.contactid
+                        left join vtiger_campaigncontrel on vtiger_contactdetails.contactid = vtiger_campaigncontrel.contactid
+                        left join vtiger_campaignrelstatus on vtiger_campaignrelstatus.campaignrelstatusid = vtiger_campaigncontrel.campaignrelstatusid
 			      LEFT JOIN vtiger_groups ON vtiger_groups.groupid = vtiger_crmentity.smownerid 
 						where vtiger_crmentity.deleted=0 and vtiger_users.user_name='".$user_name."'";
   $log->debug("Exiting get_contactsforol method ...");
