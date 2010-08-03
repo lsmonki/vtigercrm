20631e8b550906ebe95270bb787bae655792eb49
musavir <musavir@83ef7b00-670c-0410-b3e3-e45103aa6900>
2010-08-03 10:13:31 +0000

    VtigerBackup Change - MAK ==================
    1. changed module type from customized to standard.

--- pkg/vtiger/modules/VtigerBackup/modules/VtigerBackup/VtigerBackup.php
+++ pkg/vtiger/modules/VtigerBackup/modules/VtigerBackup/VtigerBackup.php
@@ -149,5 +149,28 @@ class VtigerBackup {
 		return $enable_ftp_backup == 'true';
 	}
 
+	 /**
+     * Invoked when special actions are performed on the module.
+     * @param String Module name
+     * @param String Event Type (module.postinstall, module.disabled, module.enabled, module.preuninstall)
+     */
+    function vtlib_handler($modulename, $event_type) {
+        if($event_type == 'module.postinstall') {
+			global $adb;
+			// Mark the module as Standard module
+			$adb->pquery('UPDATE vtiger_tab SET customized=0 WHERE name=?', array($modulename));
+        } else if($event_type == 'module.disabled') {
+            // TODO Handle actions when this module is disabled.
+        } else if($event_type == 'module.enabled') {
+            // TODO Handle actions when this module is enabled.
+        } else if($event_type == 'module.preuninstall') {
+            // TODO Handle actions when this module is about to be deleted.
+        } else if($event_type == 'module.preupdate') {
+            // TODO Handle actions before this module is updated.
+        } else if($event_type == 'module.postupdate') {
+            // TODO Handle actions after this module is updated.
+        }
+    }
+
 }
 ?>
