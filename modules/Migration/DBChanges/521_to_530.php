239c48c09940248ae1b7c9e635d85697abc69a9e
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2011-07-28 09:20:58 +0000

    Search Bug Fix - MAK =====================
    1. disabled behaviour where access for part of name could be disabled. for example access to firstname of contact could be disabled, handled field access as well.

--- modules/Migration/DBChanges/521_to_530.php
+++ modules/Migration/DBChanges/521_to_530.php
@@ -17,6 +17,27 @@ $conn = $_SESSION['adodb_current_object'];
 
 $migrationlog->debug("\n\nDB Changes from 5.2.1 to 5.3.0 -------- Starts \n\n");
 
+// Take away the ability to disable entity name fields
+$sql = "SELECT modulename, fieldname, tablename FROM vtiger_entityname;";
+$params = array();
+$result = $adb->pquery($sql, $params);
+$it = new SqlResultIterator($adb, $result);
+foreach ($it as $row) {
+	$tabId = getTabid($row->modulename);
+	$column = $row->fieldname;
+	$columnArray = explode(',', $column);
+	$tableName = $row->tablename;
+	$sql = "UPDATE vtiger_field,vtiger_def_org_field
+					SET presence=0,
+						vtiger_def_org_field.visible=0
+					WHERE vtiger_field.tabid=? and columnname in "."(".generateQuestionMarks($columnArray).")
+						AND tablename=? AND vtiger_field.fieldid=vtiger_def_org_field.fieldid";
+	$params = array($tabId, $columnArray, $tableName);
+	$adb->pquery($sql, $params);
+}
+
+
+
 $migrationlog->debug("\n\nDB Changes from 5.2.1 to 5.3.0  -------- Ends \n\n");
 
 ?>
