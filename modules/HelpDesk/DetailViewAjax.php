82acbe59e0a17ff4a8e29c0ad573e790be832900
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2011-10-13 15:55:47 +0000

    #6595 : Blank email is sent to assigned user of a Ticket when a ticket is updated - Raghu

--- modules/HelpDesk/DetailViewAjax.php
+++ modules/HelpDesk/DetailViewAjax.php
@@ -68,23 +68,23 @@ if($ajaxaction == "DETAILVIEW")
 					$sql = "select * from vtiger_portalinfo where user_name=?";
 					$isactive = $adb->query_result($adb->pquery($sql, array($contact_mailid)),0,'isactive');
 				}
-				
-				if($isactive == 1){
+			}
+			if($isactive == 1){
 					$url = "<a href='".$PORTAL_URL."/index.php?module=HelpDesk&action=index&ticketid=".$modObj->id."&fun=detail'>Ticket Details</a>";
 					$email_body = $subject.'<br><br>'.getPortalInfo_Ticket($modObj->id,$sub,$contactname,$url,"edit");
-				}else{
-					$data['sub']=$modObj->column_fields['ticket_title'];
-					$data['parent_name']=$parentname;
-					$data['status']=$modObj->column_fields['ticketstatus'];
-					$data['category']=$modObj->column_fields['ticketcategories'];
-					$data['severity'] = $modObj->column_fields['ticketseverities'];
-					$data['priority']=$modObj->column_fields['ticketpriorities'];
-					$data['description']=$modObj->column_fields['description'];
-					$data['solution'] = $modObj->column_fields['solution'];
-					$data['mode']= 'edit';
-					$email_body = getTicketDetails($modObj->id,$data);
-				}
+		   } else {
+				$data['sub'] = $modObj->column_fields['ticket_title'];
+				$data['parent_name'] = $parentname;
+				$data['status'] = $modObj->column_fields['ticketstatus'];
+				$data['category'] = $modObj->column_fields['ticketcategories'];
+				$data['severity'] = $modObj->column_fields['ticketseverities'];
+				$data['priority'] = $modObj->column_fields['ticketpriorities'];
+				$data['description'] = $modObj->column_fields['description'];
+				$data['solution'] = $modObj->column_fields['solution'];
+				$data['mode'] = 'edit';
+				$email_body = getTicketDetails($modObj->id, $data);
 			}
+			
 			if(PerformancePrefs::getBoolean('NOTIFY_OWNER_EMAILS', true) === true){
 				if($user_emailid != ''){
 					$mail_status = send_mail('HelpDesk',$user_emailid,$HELPDESK_SUPPORT_NAME,$HELPDESK_SUPPORT_EMAIL_ID,$subject,$email_body);
