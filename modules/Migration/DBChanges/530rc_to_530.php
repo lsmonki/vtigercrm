6dc6c827c696b73c20a377d33ec141d8b24c05dd
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2011-11-08 17:13:08 +0000

    vtiger logo name update for users migration still with vtiger logo in Company details - Asha

--- modules/Migration/DBChanges/530rc_to_530.php
+++ modules/Migration/DBChanges/530rc_to_530.php
@@ -17,14 +17,16 @@ $conn = $_SESSION['adodb_current_object'];
 
 $migrationlog->debug("\n\nDB Changes from 5.3.0RC to 5.3.0 -------- Starts \n\n");
 
-updateVtlibModule('MailManager', "packages/vtiger/mandatory/MailManager.zip");
-
 ExecuteQuery("UPDATE vtiger_field SET quickcreate=0 WHERE fieldname='time_start' AND tabid=". getTabid('Calendar'));
 
 ExecuteQuery("ALTER TABLE vtiger_links ADD COLUMN handler_path VARCHAR(128) DEFAULT null");
 ExecuteQuery("ALTER TABLE vtiger_links ADD COLUMN handler_class VARCHAR(32) DEFAULT null");
 ExecuteQuery("ALTER TABLE vtiger_links ADD COLUMN handler VARCHAR(32) DEFAULT null");
 
+ExecuteQuery("UPDATE vtiger_organizationdetails SET logoname='vtiger-crm-logo.gif' WHERE logoname='vtiger-crm-logo.jpg'");
+ExecuteQuery("UPDATE vtiger_organizationdetails SET organizationname='vtiger Systems Pvt Ltd' WHERE organizationname='vtiger'");
+
+updateVtlibModule('MailManager', "packages/vtiger/mandatory/MailManager.zip");
 updateVtlibModule('Mobile', "packages/vtiger/mandatory/Mobile.zip");
 updateVtlibModule('ConfigEditor', "packages/vtiger/mandatory/ConfigEditor.zip");
 updateVtlibModule('ServiceContracts', "packages/vtiger/mandatory/ServiceContracts.zip");
