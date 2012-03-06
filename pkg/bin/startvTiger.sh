fbf29ad197fd82ddd89d59d148574e206bff0dcb
asha <asha@83ef7b00-670c-0410-b3e3-e45103aa6900>
2012-03-06 06:54:26 +0000

    Test commit

--- pkg/bin/startvTiger.sh
+++ pkg/bin/startvTiger.sh
@@ -111,7 +111,7 @@ if [ $exit_status -ne 0 ]; then
 
 fi
 echo ""
-echo "Checking if the vtigercrm540 database already exists"
+echo "Checking if the vtigercrm540rc database already exists"
 echo ""
 echo "select 1" | ./bin/mysql --user=$mysql_username --password=$mysql_password  --port=$mysql_port --socket=$mysql_socket -D vtigercrm540 >/dev/null
 if [ $? -ne 0 ]; then
