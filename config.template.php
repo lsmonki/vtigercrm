ea9ea2a89f3dfe93a81d5791e67e92fe88d944c4
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2012-11-02 08:44:35 +0000

    Added required error_reporting level to avoid E_NOTICE.

--- config.template.php
+++ config.template.php
@@ -15,7 +15,7 @@
 
 include('vtigerversion.php');
 
-//version_compare(phpversion(), '5.3.9') < 0? error_reporting(E_ALL & ~E_NOTICE) : error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
+version_compare(phpversion(), '5.3.0') < 0? error_reporting(E_ALL & ~E_NOTICE) : error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED);
 
 // more than 8MB memory needed for graphics
 // memory limit default value = 64M
