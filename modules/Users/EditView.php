f5f0a5129edb64d4e82672cf8ab2395736ad8d06
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2012-12-26 09:39:43 +0000

    Fixed issue which was blocking user-creation.

--- modules/Users/EditView.php
+++ modules/Users/EditView.php
@@ -48,28 +48,6 @@ if(isset($_REQUEST['record']) && isset($_REQUEST['record'])) {
 	$mode='create';
 }
 
-if(empty($_REQUEST['record']) || empty($focus->column_fields['status']) ){
-
-	echo "<table border='0' cellpadding='5' cellspacing='0' width='100%' height='450px'><tr><td align='center'>";
-	echo "<div style='border: 3px solid rgb(153, 153, 153); background-color: rgb(255, 255, 255); width: 55%; position: relative; z-index: 10000000;'>
-
-		<table border='0' cellpadding='5' cellspacing='0' width='98%'>
-		<tbody><tr>
-		<td rowspan='2' width='11%'><img src= " . vtiger_imageurl('denied.gif', $theme) . " ></td>
-		<td style='border-bottom: 1px solid rgb(204, 204, 204);' nowrap='nowrap' width='70%'>
-			<span class='genHeaderSmall'>$app_strings[LBL_PERMISSION]</span></td>
-		</tr>
-		<tr>
-		<td class='small' align='right' nowrap='nowrap'>
-		<a href='javascript:window.history.back();'>$app_strings[LBL_GO_BACK]</a><br>
-		</td>
-		</tr>
-		</tbody></table>
-		</div>";
-	echo "</td></tr></table>";
-	exit;
-}
-
 global $theme;
 $theme_path="themes/".$theme."/";
 $image_path=$theme_path."images/";
