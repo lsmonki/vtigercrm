f303791d0fe89c7376813696cba340598df61cd2
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-02-26 04:12:28 +0000

    From portal field added to fix ticket creation.

--- modules/Migration/schema/540_to_600RC.php
+++ modules/Migration/schema/540_to_600RC.php
@@ -1894,6 +1894,21 @@ if (!$usersRowHeightField) {
 	$field->setPicklistValues(array('wide', 'medium', 'narrow'));
 }
 
+$moduleInstance = Vtiger_Module::getInstance('HelpDesk');
+$block = Vtiger_Block::getInstance('LBL_TICKET_INFORMATION', $moduleInstance);
+
+$field = new Vtiger_Field();
+$field->name = 'from_portal';
+$field->label = 'From Portal';
+$field->table ='vtiger_ticketcf';
+$field->column = 'from_portal';
+$field->columntype = 'varchar(3)';
+$field->typeofdata = 'C~O';
+$field->uitype = 56;
+$field->displaytype = 3;
+$field->presence = 0;
+$block->addField($field);
+
 //Start: Customer - Feature #10254 Configuring all Email notifications including Ticket notifications
 $moduleName = 'HelpDesk';
 //Start: Moving Entity methods of Comments to Workflows
