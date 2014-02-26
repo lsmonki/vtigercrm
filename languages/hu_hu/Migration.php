f924dce7e4271dc4e3a78801bbefd09c859e389b
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-02-26 04:03:03 +0000

    Updated migration text translations.

---
+++ languages/hu_hu/Migration.php
@@ -0,0 +1,26 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+	'LBL_VTIGER_MIGRATION' => 'Üdvözöljük a Vtiger migrációs',
+	'LBL_MIGRATION_COMPLETED' => 'Az áttelepítés befejeződött',
+	'LBL_MIGRATION_WIZARD' => 'Migration Wizard',
+	'LBL_PRIDE_BEING_ASSOCIATED' => 'Büszkék vagyunk arra, hogy kapcsolatban van.',
+	'LBL_TALK_TO_US_AT_FORUMS' => 'Van kérdése? Keresés segítséget Vtiger <a href="http://forums.vtiger.com" target="_blank"> Beszélgetések</a><br>',
+	'LBL_DISCUSS_WITH_US_AT_BLOGS' => 'Többet tudni Vtiger Kísérjen minket <a href="http://blogs.vtiger.com" target="_blank">Blog</a>',
+	'LBL_CRM_DOCUMENTATION' => 'Vtiger6 Help - <a href="http://wiki.vtiger.com/vtiger6" target="_blank">Dokumentáció</a> , <a href="http://www.youtube.com/vtiger" target="_blank">Videók</a> <BR>',
+	'LBL_THANKS' => 'Köszönjük, hogy használta<b>vtiger CRM</b>',
+	'LBL_WE_AIM_TO_BE_BEST' => 'Célunk, hogy - egyszerűen a legjobb',
+	'LBL_SPACE_FOR_YOU' => 'Gyere, van hely az Ön számára is!',
+	'LBL_MIGRATION_COMPLETED_SUCCESSFULLY' => 'A vtiger6 migráció sikeresen befejeződött.',
+	'LBL_RELEASE_NOTES' => 'Tudni, hogy mi az új ebben a verzióban, kérjük, olvassa el <a href="#" target="_blank">Kiadási megjegyzések</a> <br>',
+	'LBL_WAIT' => 'Kérjük, várjon ...',
+	'LBL_INPROGRESS' => 'Migrációs folyamatban',
+	'LBL_DATABASE_CHANGE_LOG' => 'Migráció: adatbázis változásait Log',
+);
---
+++ pkg/vtiger/translations/Sweden_sv_se/modules/Migration.php
@@ -0,0 +1,26 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+	'LBL_VTIGER_MIGRATION' => 'Välkommen till vtiger Migration',
+	'LBL_MIGRATION_COMPLETED' => 'Migration Genomförd',
+	'LBL_MIGRATION_WIZARD' => 'Migration Wizard',
+	'LBL_PRIDE_BEING_ASSOCIATED' => 'Vi är stolta över att förknippas med dig.',
+	'LBL_TALK_TO_US_AT_FORUMS' => 'Har du några frågor? Sök hjälp om vtiger <a href="http://forums.vtiger.com" target="_blank"> Diskussioner </a><br>',
+	'LBL_DISCUSS_WITH_US_AT_BLOGS' => 'För att veta mer om vtiger följa oss vår <a href="http://blogs.vtiger.com" target="_blank">Blogg</a>',
+	'LBL_CRM_DOCUMENTATION' => 'Vtiger6 Hjälp - <a href="http://wiki.vtiger.com/vtiger6" target="_blank">För dokumentation</a> , <a href="http://www.youtube.com/vtiger" target="_blank">Videor</a> <BR>',
+	'LBL_THANKS' => 'Tack för att du använder <b>vtiger CRM</b>',
+	'LBL_WE_AIM_TO_BE_BEST' => 'Vi strävar efter att vara - helt enkelt bäst',
+	'LBL_SPACE_FOR_YOU' => 'Kom över, det finns plats för dig också!',
+	'LBL_MIGRATION_COMPLETED_SUCCESSFULLY' => 'Din vtiger6 migreringen slutförts.',
+	'LBL_RELEASE_NOTES' => 'Att veta vad som är nytt i den här versionen ska du läsa <a href="#" target="_blank">Release Notes</a> <br>',
+	'LBL_WAIT' => 'Var god vänta ...',
+	'LBL_INPROGRESS' => 'Migration pågår',
+	'LBL_DATABASE_CHANGE_LOG' => 'Migration: Databas Förändringar Log',
+);
