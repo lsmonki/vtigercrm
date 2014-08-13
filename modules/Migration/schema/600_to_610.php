cab2a6dd2b96dba93fdc3fc5867ea88eadba7a59
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-08-13 13:20:16 +0000

    Removed dependency on ExtensionStorePro.

--- modules/Migration/schema/600_to_610.php
+++ modules/Migration/schema/600_to_610.php
@@ -34,7 +34,6 @@ if(defined('VTIGER_UPGRADE')) {
         updateVtlibModule("Sweden_sv_se","packages/vtiger/optional/Sweden_sv_se.zip");
 	updateVtlibModule("Webforms","packages/vtiger/optional/Webforms.zip");
         installVtlibModule('ExtensionStore', 'packages/vtiger/marketplace/ExtensionStore.zip');
-        installVtlibModule('ExtensionStorePro', 'packages/vtiger/marketplace/ExtensionStorePro-protected.zip');
 }
 if(defined('INSTALLATION_MODE')) {
 		// Set of task to be taken care while specifically in installation mode.
