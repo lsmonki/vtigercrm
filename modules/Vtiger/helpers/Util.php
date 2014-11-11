2e479ca86762d406f2c09a52bb64dee80fb2d597
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2014-11-11 11:30:16 +0000

    Fixed: Reports not respecting time-format set in my-preference of user.

--- modules/Vtiger/helpers/Util.php
+++ modules/Vtiger/helpers/Util.php
@@ -451,10 +451,6 @@ class Vtiger_Util_Helper {
 		$date = $dateTimeField->getDisplayDate($userModel);
 		$time = $dateTimeField->getDisplayTime($userModel);
 
-		if($userModel->get('hour_format') == '12') {
-			$time = Vtiger_Time_UIType::getTimeValueInAMorPM($time);
-		}
-
 		return $date . ' ' .$time;
 	}
 
