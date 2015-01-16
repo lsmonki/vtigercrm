3a824a202eb627ee57532ce7fc89d3824edadb67
prasad <prasad@83ef7b00-670c-0410-b3e3-e45103aa6900>
2015-01-16 14:08:51 +0000

    Fixed: Supression of disabled module in Email Templates.

--- pkg/vtiger/modules/EmailTemplates/modules/EmailTemplates/models/Module.php
+++ pkg/vtiger/modules/EmailTemplates/modules/EmailTemplates/models/Module.php
@@ -171,11 +171,11 @@ class EmailTemplates_Module_Model extends Vtiger_Module_Model {
 	 */
 	public function getAllModuleList(){
 		$db = PearDatabase::getInstance();
-		
+		//return the modules which are only enable 
 		$query = 'SELECT DISTINCT(name) AS modulename FROM vtiger_tab 
 				  LEFT JOIN vtiger_field ON vtiger_field.tabid = vtiger_tab.tabid
-				  WHERE vtiger_field.uitype = ?';
-		$result = $db->pquery($query, array(13));
+				  WHERE (vtiger_field.uitype = ? AND vtiger_tab.presence = ?)';
+		$result = $db->pquery($query, array(13,0));
 		$num_rows = $db->num_rows($result);
 		$moduleList = array();
 		for($i=0; $i<$num_rows; $i++){
